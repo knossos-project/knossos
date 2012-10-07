0dabfee557046fcd93c92404f986b5c01c0c05ef
Mathias Wegner <mwegner3@ix.urz.uni-heidelberg.de>
2012-10-07 18:02:58 +0000

    Reset legacy string for totalNodeElements == 0

--- gui.c
+++ gui.c
@@ -191,6 +191,9 @@ void updateAGconfig() {
     state->viewerState->ag->totalTrees = state->skeletonState->treeElements;
     state->viewerState->ag->totalNodes = state->skeletonState->totalNodeElements;
     if(state->skeletonState->totalNodeElements == 0) {
+        if(!strcmp (state->skeletonState->skeletonCreatedInVersion, "pre-3.2")){
+            strcpy(state->skeletonState->skeletonCreatedInVersion, "3.2");
+        }
         AG_NumericalSetWriteable(state->viewerState->ag->actNodeIDWdgt1, FALSE);
         AG_NumericalSetWriteable(state->viewerState->ag->actNodeIDWdgt2, FALSE);
         state->viewerState->ag->activeNodeID = 0;
