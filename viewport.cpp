d64532adf634c3a189eeeba6a723e3ee70c6f065
My-Tien Nguyen <maytie@gmail.com>
2012-12-16 22:36:29 +0000

    forgot to add viewport.cpp

---
+++ viewport.cpp
@@ -0,0 +1,75 @@
+#include "viewport.h"
+
+
+Viewport::Viewport(QWidget *parent, int plane) :
+    QGLWidget(parent)
+{
+    this->plane = plane;
+    eventModel = new EventModel();
+    /* per default the widget only receives move event when at least one mouse button is pressed
+    to change this behaviour we need to track the mouse position */
+    this->setMouseTracking(true);
+    setStyleSheet("background:black");
+}
+
+void Viewport::initializeGL()
+{
+    glClearColor(0, 0, 0, 1);
+}
+
+void Viewport::resizeGL(int w, int h)
+{
+
+}
+
+void Viewport::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+}
+
+void Viewport::mouseMoveEvent(QMouseEvent *event)
+{
+
+    //qDebug() << event->x() << " " << qDebug() << event->y();
+
+}
+
+void Viewport::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton) {
+        eventModel->handleMouseButtonLeft(event, plane);
+    } else if(event->button() == Qt::MiddleButton) {
+        eventModel->handleMouseButtonMiddle(event, plane);
+    } else if(event->button() == Qt::RightButton) {
+        eventModel->handleMouseButtonRight(event, plane);
+    }
+
+}
+
+void Viewport::mouseReleaseEvent(QMouseEvent *event)
+{
+
+
+
+}
+
+void Viewport::wheelEvent(QWheelEvent *event) {
+    if(event->delta() > 0) {
+        eventModel->handleMouseWheelForward(event, plane);
+    } else {
+        eventModel->handleMouseWheelBackward(event, plane);
+    }
+}
+
+void Viewport::keyPressEvent(QKeyEvent *event)
+{
+
+    eventModel->handleKeyboard(event);
+}
+
+void Viewport::keyReleaseEvent(QKeyEvent *event)
+{
+
+}
+
