fdbd4a73b4d47226a7e88400dc9cbca76b7172bc
My-Tien Nguyen <maytie@gmail.com>
2014-01-16 17:09:45 +0000

    the annotation widget for replacing the tools widget

---
+++ widgets/annotationwidget.cpp
@@ -0,0 +1,133 @@
+#include <QLineEdit>
+#include <QDoubleSpinBox>
+#include <QSpinBox>
+#include <QCheckBox>
+#include <QSettings>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLabel>
+
+#include "knossos-global.h"
+#include "GUIConstants.h"
+#include "annotationwidget.h"
+
+extern stateInfo *state;
+
+AnnotationWidget::AnnotationWidget(QWidget *parent) :
+    QDialog(parent)
+{
+    tabs = new QTabWidget(this);
+    treeviewTab = new ToolsTreeviewTab(this);
+    commandsTab = new ToolsCommandsTab(this);
+    tabs->addTab(treeviewTab, "Tree View");
+    tabs->addTab(commandsTab, "Commands");
+
+    treeCountLabel = new QLabel("Tree Count: 0");
+    nodeCountLabel = new QLabel("Node Count: 0");
+
+    QHBoxLayout *hLayout = new QHBoxLayout();
+    hLayout->addWidget(treeCountLabel);
+    hLayout->addWidget(nodeCountLabel);
+
+    mainLayout = new QVBoxLayout(this);
+    mainLayout->addWidget(tabs);
+    mainLayout->addLayout(hLayout);
+    setLayout(mainLayout);
+
+    connect(treeviewTab, SIGNAL(updateToolsSignal()), this, SLOT(update()));
+}
+
+void AnnotationWidget::update() {
+    treeCountLabel->setText(QString("Tree Count: %1").arg(state->skeletonState->treeElements));
+    nodeCountLabel->setText(QString("Node Count: %1").arg(state->skeletonState->totalNodeElements));
+    commandsTab->update();
+}
+
+void AnnotationWidget::loadSettings() {
+    int width, height, x, y;
+    bool visible;
+
+    QSettings settings;
+    settings.beginGroup(TOOLS_WIDGET);
+
+    width = settings.value(WIDTH).toInt();
+    height = settings.value(HEIGHT).toInt();
+    x = settings.value(POS_X).toInt();
+    y = settings.value(POS_Y).toInt();
+    visible = settings.value(VISIBLE).toBool();
+
+    if(settings.value(SEARCH_FOR_TREE).isNull() == false) {
+        treeviewTab->treeCommentField->setText(settings.value(SEARCH_FOR_TREE).toString());
+    }
+    else {
+        treeviewTab->treeCommentField->setPlaceholderText("search tree");
+    }
+    if(settings.value(SEARCH_FOR_NODE).isNull() == false) {
+        treeviewTab->nodeCommentField->setText(settings.value(SEARCH_FOR_NODE).toString());
+    }
+    else {
+        treeviewTab->nodeCommentField->setPlaceholderText("search node");
+    }
+
+    if(settings.value(USE_LAST_RADIUS_AS_DEFAULT).isNull() == false) {
+        commandsTab->useLastRadiusAsDefaultCheck->setChecked(settings.value(USE_LAST_RADIUS_AS_DEFAULT).toBool());
+    }
+    else {
+        commandsTab->useLastRadiusAsDefaultCheck->setChecked(false);
+    }
+
+    if(settings.value(DEFAULT_NODE_RADIUS).isNull() == false) {
+        state->skeletonState->defaultNodeRadius = settings.value(DEFAULT_NODE_RADIUS).toDouble();
+    }
+    else {
+        state->skeletonState->defaultNodeRadius = 1.5;
+    }
+    commandsTab->defaultRadiusSpin->setValue(state->skeletonState->defaultNodeRadius);
+
+    if(settings.value(ENABLE_COMMENT_LOCKING).isNull() == false) {
+        commandsTab->commentLockingCheck->setChecked(settings.value(ENABLE_COMMENT_LOCKING).toBool());
+    }
+    else {
+        commandsTab->commentLockingCheck->setChecked(false);
+    }
+
+    if(settings.value(LOCKING_RADIUS).isNull() == false) {
+        state->skeletonState->lockRadius = settings.value(LOCKING_RADIUS).toInt();
+    }
+    else {
+        state->skeletonState->lockRadius = 10;
+    }
+    commandsTab->lockingRadiusSpin->setValue(state->skeletonState->lockRadius);
+
+    if(settings.value(LOCK_TO_NODES_WITH_COMMENT).isNull() == false) {
+        state->viewerState->gui->lockComment = settings.value(LOCK_TO_NODES_WITH_COMMENT).toString();
+    }
+    else {
+        state->viewerState->gui->lockComment = "seed";
+    }
+    commandsTab->commentLockEdit->setText(QString(state->viewerState->gui->lockComment));
+
+    settings.endGroup();
+    setGeometry(x, y, width, height);
+}
+
+void AnnotationWidget::saveSettings() {
+    QSettings settings;
+    settings.beginGroup(TOOLS_WIDGET);
+
+    settings.setValue(WIDTH, this->width());
+    settings.setValue(HEIGHT, this->height());
+    settings.setValue(POS_X, this->x());
+    settings.setValue(POS_Y, this->y());
+    settings.setValue(VISIBLE, this->isVisible());
+
+    settings.setValue(SEARCH_FOR_TREE, treeviewTab->treeCommentField->text());
+    settings.setValue(SEARCH_FOR_NODE, treeviewTab->nodeCommentField->text());
+    settings.setValue(USE_LAST_RADIUS_AS_DEFAULT, commandsTab->useLastRadiusAsDefaultCheck->isChecked());
+    settings.setValue(DEFAULT_NODE_RADIUS, commandsTab->defaultRadiusSpin->value());
+    settings.setValue(ENABLE_COMMENT_LOCKING, commandsTab->commentLockingCheck->isChecked());
+    settings.setValue(LOCKING_RADIUS, commandsTab->lockingRadiusSpin->value());
+    settings.setValue(LOCK_TO_NODES_WITH_COMMENT, commandsTab->commentLockEdit->text());
+
+    settings.endGroup();
+}
---
+++ widgets/annotationwidget.h
@@ -0,0 +1,33 @@
+#ifndef ANNOTATIONWIDGET_H
+#define ANNOTATIONWIDGET_H
+
+#include <QDialog>
+#include <QTabWidget>
+
+#include "tools/toolstreeviewtab.h"
+#include "tools/toolscommandstab.h"
+
+class QVBoxLayout;
+class QLabel;
+class AnnotationWidget : public QDialog
+{
+    Q_OBJECT
+public:
+    ToolsTreeviewTab *treeviewTab;
+    ToolsCommandsTab *commandsTab;
+    QVBoxLayout *mainLayout;
+    explicit AnnotationWidget(QWidget *parent = 0);
+protected:
+    QTabWidget *tabs;
+    QLabel *treeCountLabel;
+    QLabel *nodeCountLabel;
+
+signals:
+    
+public slots:
+    void update();
+    void saveSettings();
+    void loadSettings();
+};
+
+#endif // ANNOTATIONWIDGET_H
