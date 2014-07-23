e69b5549f6a99c1903165635c0ab58ab980270b1
Norbert Pfeiler <norbert.pfeiler@gmail.com>
2014-07-23 19:57:54 +0200

    update FindQuaZip (better cmake find output), spaces

--- cmake/Modules/FindQuaZip.cmake
+++ cmake/Modules/FindQuaZip.cmake
@@ -3,22 +3,27 @@
 # QUAZIP_LIBRARIES           - List of QuaZip libraries
 
 if(QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
-	# in cache already
-	set(QUAZIP_FOUND TRUE)
+    # in cache already
+    set(QUAZIP_FOUND TRUE)
 else(QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
-	find_package(PkgConfig)
+    find_package(PkgConfig)
     #pkg_check_modules(PC_QCA2 QUIET qca2)
-	pkg_check_modules(PC_QUAZIP quazip)
-	find_package(ZLIB REQUIRED)
+    pkg_check_modules(PC_QUAZIP quazip)
+    find_package(ZLIB REQUIRED)
 
-	set(QUAZIP_HINTS "C:/Programme/" "C:/Program Files")
-	find_library(QUAZIP_LIBRARY NAMES quazip HINTS ${QUAZIP_HINTS} PATH_SUFFIXES "QuaZip")
-	find_path(QUAZIP_INCLUDE_DIR NAMES quazip/quazip.h HINTS ${QUAZIP_HINTS})
+    set(QUAZIP_HINTS "C:/Programme/" "C:/Program Files")
+    find_library(QUAZIP_LIBRARY NAMES quazip HINTS ${QUAZIP_HINTS} PATH_SUFFIXES "QuaZip")
+    find_path(QUAZIP_INCLUDE_DIR NAMES quazip/quazip.h HINTS ${QUAZIP_HINTS})
 
 
-	set(QUAZIP_LIBRARIES ${QUAZIP_LIBRARY} ${ZLIB_LIBRARIES})
-	set(QUAZIP_INCLUDE_DIRS ${QUAZIP_INCLUDE_DIR} ${ZLIB_INCLUDE_DIR})
+    set(QUAZIP_LIBRARIES ${QUAZIP_LIBRARY} ${ZLIB_LIBRARIES})
+    set(QUAZIP_INCLUDE_DIRS ${QUAZIP_INCLUDE_DIR} ${ZLIB_INCLUDE_DIR})
 
-	include(FindPackageHandleStandardArgs)
-	find_package_handle_standard_args(QUAZIP DEFAULT_MSG QUAZIP_INCLUDE_DIRS QUAZIP_LIBRARIES)
+    set(QUAZIP_VERSION 0.6.2)
+
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(QUAZIP
+        REQUIRED_VARS QUAZIP_LIBRARIES QUAZIP_INCLUDE_DIRS
+        VERSION_VAR QUAZIP_VERSION
+    )
 endif(QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
