b39e61f7e3f821c272acf186d3a876d21efbe20a
Norbert Pfeiler <norbert.pfeiler@gmail.com>
2014-08-21 20:27:32 +0200

    the changes were missing

--- segmentationsplit.cpp
+++ segmentationsplit.cpp
@@ -1,4 +1,4 @@
-#include "segmentation_split.h"
+#include "segmentationsplit.h"
 
 #include "knossos-global.h"
 #include "segmentation.h"
@@ -9,12 +9,14 @@
 
 extern stateInfo const * state;
 
-void bucketFill(const Coordinate & seed, boost::multi_array_ref<uint64_t, 3> cube, const uint64_t objIdToSplit, const uint64_t newSubObjId, const std::unordered_set<uint64_t> & subObjectsToFill, std::unordered_set<uint64_t> & visitedSubObjects) {
+std::unordered_set<uint64_t> bucketFill(const Coordinate & seed, boost::multi_array_ref<uint64_t, 3> cube, const uint64_t objIdToSplit, const uint64_t newSubObjId, const std::unordered_set<uint64_t> & subObjectsToFill) {
     std::vector<Coordinate> work = {seed};
     std::unordered_set<Coordinate, Coordinate::Hash> visitedVoxels;
+    std::unordered_set<uint64_t> visitedSubObjects;
     while (!work.empty()) {
         auto pos = work.back();
         work.pop_back();
+
         auto subobjectId = cube[pos.z][pos.y][pos.x];
         if (subobjectId != 0 && subobjectId != newSubObjId) {
             auto & subobject = Segmentation::singleton().subobjectFromId(subobjectId);
@@ -28,7 +30,6 @@ void bucketFill(const Coordinate & seed, boost::multi_array_ref<uint64_t, 3> cub
                 }
 
                 visitedVoxels.emplace(pos.x, pos.y, pos.z);
-
                 auto walk = [&](const Coordinate & newPos){
                     if (visitedVoxels.find(newPos) == std::end(visitedVoxels)) {
                         if (pos.x >= 0 && pos.y >= 0 && pos.z >= 0 && pos.z < cube.size() && pos.y < cube[0].size() && pos.x < cube[0][0].size()) {
@@ -45,23 +46,41 @@ void bucketFill(const Coordinate & seed, boost::multi_array_ref<uint64_t, 3> cub
             }
         }
     }
+    return visitedSubObjects;
 }
 
-void verticalSplittingPlane(const Coordinate & pos, boost::multi_array_ref<uint64_t, 3> cube, const uint64_t objIdToSplit, const uint64_t newSubObjId, std::unordered_set<uint64_t> & visitedSubObjects) {
-    auto subobjectId = cube[pos.z][pos.y][pos.x];
-    if (subobjectId != 0 && subobjectId != newSubObjId) {
-        auto & subobject = Segmentation::singleton().subobjectFromId(subobjectId);
-        auto objId = Segmentation::singleton().largestObjectContainingSubobject(subobject);
-        if (objId == objIdToSplit) {
-            cube[pos.z][pos.y][pos.x] = newSubObjId;//write to cube
-            visitedSubObjects.emplace(subobject.id);//accumulate visited subobjects
+std::unordered_set<uint64_t> verticalSplittingPlane(const Coordinate & pos, boost::multi_array_ref<uint64_t, 3> cube, const uint64_t objIdToSplit, const uint64_t newSubObjId) {
+    std::vector<Coordinate> work = {pos};
+    std::unordered_set<Coordinate, Coordinate::Hash> visitedVoxels;
+    std::unordered_set<uint64_t> visitedSubObjects;
+    while (!work.empty()) {
+        auto pos = work.back();
+        work.pop_back();
 
-            verticalSplittingPlane({pos.x, pos.y + 1, pos.z}, cube, objIdToSplit, newSubObjId, visitedSubObjects);
-            verticalSplittingPlane({pos.x, pos.y - 1, pos.z}, cube, objIdToSplit, newSubObjId, visitedSubObjects);
-            verticalSplittingPlane({pos.x, pos.y, pos.z + 1}, cube, objIdToSplit, newSubObjId, visitedSubObjects);
-            verticalSplittingPlane({pos.x, pos.y, pos.z - 1}, cube, objIdToSplit, newSubObjId, visitedSubObjects);
+        auto subobjectId = cube[pos.z][pos.y][pos.x];
+        if (subobjectId != 0 && subobjectId != newSubObjId) {
+            auto & subobject = Segmentation::singleton().subobjectFromId(subobjectId);
+            auto objId = Segmentation::singleton().largestObjectContainingSubobject(subobject);
+            if (objId == objIdToSplit) {
+                cube[pos.z][pos.y][pos.x] = newSubObjId;//write to cube
+                visitedSubObjects.emplace(subobject.id);//accumulate visited subobjects
+
+                visitedVoxels.emplace(pos.x, pos.y, pos.z);
+                auto walk = [&](const Coordinate & newPos){
+                    if (visitedVoxels.find(newPos) == std::end(visitedVoxels)) {
+                        if (pos.z < cube.size() && pos.y < cube[0].size() && pos.x < cube[0][0].size()) {
+                            work.emplace_back(newPos);
+                        }
+                    }
+                };
+                walk({pos.x, pos.y + 1, pos.z});
+                walk({pos.x, pos.y - 1, pos.z});
+                walk({pos.x, pos.y, pos.z + 1});
+                walk({pos.x, pos.y, pos.z - 1});
+            }
         }
     }
+    return visitedSubObjects;
 }
 
 void verticalSplittingPlane(const Coordinate & seed) {
@@ -70,6 +89,7 @@ void verticalSplittingPlane(const Coordinate & seed) {
     state->protectCube2Pointer->lock();
     auto rawcube = Hashtable::ht_get(state->Oc2Pointer[int_log(state->magnification)], seedDc);
     state->protectCube2Pointer->unlock();
+
     if (rawcube != HT_FAILURE) {
         auto dims = boost::extents[state->cubeEdgeLength][state->cubeEdgeLength][state->cubeEdgeLength];
         boost::multi_array_ref<uint64_t, 3> cube(reinterpret_cast<uint64_t *>(rawcube), dims);
@@ -81,14 +101,11 @@ void verticalSplittingPlane(const Coordinate & seed) {
             auto newSubObjId = Segmentation::SubObject::highestId + 1;
             auto newObjectId = Segmentation::singleton().createObject(newSubObjId).id;
 
-            std::unordered_set<uint64_t> subObjectsToFill;
+            std::unordered_set<uint64_t> subObjectsToFill = verticalSplittingPlane({pos.x, pos.y, pos.z}, cube, splitId, newSubObjId);
 
-            verticalSplittingPlane({pos.x, pos.y, pos.z}, cube, splitId, newSubObjId, subObjectsToFill);
+            std::unordered_set<uint64_t> visitedSubObjects = bucketFill({pos.x + 1, pos.y, pos.z}, cube, splitId, newSubObjId, subObjectsToFill);
 
-            std::unordered_set<uint64_t> visitedSubObjects;
-
-            bucketFill({pos.x + 1, pos.y, pos.z}, cube, splitId, newSubObjId, subObjectsToFill, visitedSubObjects);
-
+            //add the newly created subobject to all non-splitted objects
             for (auto && id : subObjectsToFill) {
                 auto & subobject = Segmentation::singleton().subobjectFromId(id);
                 for (auto && objId : subobject.objects) {
@@ -101,13 +118,15 @@ void verticalSplittingPlane(const Coordinate & seed) {
                 }
             }
 
+            //merge all traversed but unchanged supervoxel into the new object
             auto & newObject = Segmentation::singleton().objects[newObjectId];
             for (auto && id : visitedSubObjects) {
                 auto & subobject = Segmentation::singleton().subobjectFromId(id);
                 newObject.addExistingSubObject(subobject);
-                std::sort(std::begin(newObject.subobjects), std::end(newObject.subobjects));
             }
+            std::sort(std::begin(newObject.subobjects), std::end(newObject.subobjects));
 
+            //remove the newly created area from the object to split
             Segmentation::singleton().selectObject(splitId);
             Segmentation::singleton().selectObject(newObjectId);
             Segmentation::singleton().unmergeSelectedObjects();
