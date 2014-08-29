f6b3d222205f99133fa4f2bc78e48bb0074c3c61
Norbert Pfeiler <norbert.pfeiler@gmail.com>
2014-08-29 14:53:25 +0200

    remove version from FindQuaZip

--- cmake/Modules/FindQuaZip.cmake
+++ cmake/Modules/FindQuaZip.cmake
@@ -5,17 +5,12 @@ find_package(ZLIB REQUIRED)
 find_library(QUAZIP_LIBRARY quazip PATH_SUFFIXES "QuaZip")
 find_path(QUAZIP_INCLUDE_DIR quazip/quazip.h)
 
-set(QUAZIP_VERSION 0.7)
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(QUAZIP
     REQUIRED_VARS QUAZIP_LIBRARY QUAZIP_INCLUDE_DIR
-    VERSION_VAR QUAZIP_VERSION
 )
 
 if(QUAZIP_FOUND)
-    message(STATUS "QuaZip version: ${QUAZIP_VERSION}")
-
     add_library(QuaZip::QuaZip STATIC IMPORTED)
     set_target_properties(QuaZip::QuaZip PROPERTIES
         IMPORTED_LOCATION ${QUAZIP_LIBRARY}
