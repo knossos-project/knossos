8a82c922b9cdc1614a4947dec745be2588544f7b
Norbert Pfeiler <norbert.pfeiler@gmail.com>
2014-10-23 02:09:26 +0200

    link against the current pythonqt library outsource some of the dependencies

--- CustomLibraryLocations.cmake
+++ CustomLibraryLocations.cmake
@@ -2,9 +2,4 @@
 #cmake searches for headers and libraries both directly and inside include/lib folders below
 set(CMAKE_PREFIX_PATH
     "${CMAKE_PREFIX_PATH}"#append
-    #"E:/dev/qt/Qt-5.2.1-x86_64"#not needed if qmake is in path or building inside qtcreator
-    "E:/dev/curl-7.36.0/_install"
-    "E:/dev/freeglut-2.8.1/_install"
-    "C:/libjpeg-turbo-gcc"
-    "C:/libjpeg-turbo-gcc64"
 )
