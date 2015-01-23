18b5ade63a9cbdff79d565eecd760c9fa9b8b7ce
Eroica <Leye@REXPLORER.local>
2015-01-23 18:39:51 +0100

    add Homebrew paths to CustomLibraryLocations

--- CustomLibraryLocations.cmake
+++ CustomLibraryLocations.cmake
@@ -2,4 +2,8 @@
 #cmake searches for headers and libraries both directly and inside include/lib folders below
 set(CMAKE_PREFIX_PATH
     "${CMAKE_PREFIX_PATH}"#append
+
+# For Mac development: required to find packages from Homebrew
+    /usr/local/opt/curl/bin
+    /usr/local/opt/qt5/bin
 )
