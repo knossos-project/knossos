9f9572310c81f268f4bbe4617ad315e0a57729d3
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2015-01-28 18:42:20 +0100

    static build fixes

--- CustomLibraryLocations.cmake
+++ CustomLibraryLocations.cmake
@@ -4,6 +4,13 @@ set(CMAKE_PREFIX_PATH
     "${CMAKE_PREFIX_PATH}"#append
 
 # For Mac development: required to find packages from Homebrew
-    /usr/local/opt/curl/bin
-    /usr/local/opt/qt5/bin
+    "/usr/local/opt/curl/bin"
+    "/usr/local/opt/qt5/bin"
 )
+
+if(WIN32 AND NOT BUILD_SHARED_LIBS)
+    set(CMAKE_PREFIX_PATH
+        "${CMAKE_PREFIX_PATH}"
+        "C:/msys64/mingw64/qt5-static"
+    )
+endif()
