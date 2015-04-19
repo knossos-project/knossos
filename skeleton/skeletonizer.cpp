knossos-project/knossos

commit 42d656d04dcc6f0c8ab596f6fea12a27b2de9c4c

fix #144

skeleton/skeletonizer.cpp
@@ -1107,6 +1107,12 @@ bool Skeletonizer::delTree(int treeID) {
         }
     }
 
+    //remove if active tree
+    if (treeToDel == state->skeletonState->activeTree) {
+        state->skeletonState->activeTree = nullptr;
+    }
+
+    //no references to tree left
     emit treeRemovedSignal(treeID);
 
     state->viewer->skeletonizer->setActiveNode(state->skeletonState->activeNode, 0);//set active tree through node

commit f2154411a39b2558c177075b84a526e7d0b72546

A regression introduced in 42d656d invalidated the check if the deleted tree was the active tree resulting in an unset active tree which the renderer didn’t like. (segfault) fixes #169 If the deleted tree contained nodes, the active tree is set through the active node (while deleting active nodes) which – in the end – is the nearest node to the lastly removed active node. If the deleted tree is empty, the first tree (lastly created) is set active for now.

skeleton/skeletonizer.cpp
@@ -1108,23 +1108,18 @@ bool Skeletonizer::delTree(int treeID) {
     if (treeToDel == state->skeletonState->activeTree) {
         state->skeletonState->activeTree = nullptr;
     }
-
-    //no references to tree left
-    emit treeRemovedSignal(treeID);
-
-    state->viewer->skeletonizer->setActiveNode(state->skeletonState->activeNode, 0);//set active tree through node
-    if (treeToDel == state->skeletonState->activeTree && state->skeletonState->firstTree != nullptr) {
-        setActiveTreeByID(state->skeletonState->firstTree->treeID);//if there is no node but a tree
-    }
-    if (state->skeletonState->firstTree == nullptr) {
-        state->skeletonState->activeTree = nullptr;//no trees
-    }
     free(treeToDel);
-
     state->skeletonState->treeElements--;
 
-    state->skeletonState->unsavedChanges = true;
+    //no references to tree left
+    emit treeRemovedSignal(treeID);//updates tools
 
+    //if the active tree was not set through the active node but another tree exists
+    if (state->skeletonState->activeTree == nullptr && state->skeletonState->firstTree != nullptr) {
+        setActiveTreeByID(state->skeletonState->firstTree->treeID);//updates tools too
+    }
+
+    state->skeletonState->unsavedChanges = true;
     return true;
 }
 