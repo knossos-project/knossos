knossos-project/knossos

commit d5bb7699357b4225e79e8807e614baaa5e813e8f

Replace nodeID with node reference as parameter in most skeleton functions

skeleton/segmentationSkeletonBridge.cpp
@@ -23,8 +23,7 @@ void Skeletonizer::selectObjectForNode(const nodeListElement & node) {
     });
 }
 
-void Skeletonizer::setSubobject(const quint64 nodeId, const quint64 subobjectId) {
-    auto & node = *Skeletonizer::singleton().findNodeByNodeID(nodeId);
+void Skeletonizer::setSubobject(nodeListElement & node, const quint64 subobjectId) {
     node.properties.insert(subobjectPropertyKey, subobjectId);
     ++node.correspondingTree->subobjectCount[subobjectId];
 }
@@ -38,12 +37,8 @@ void setSubobjectSelectAndMerge(nodeListElement & node, const quint64 subobjectI
     Segmentation::singleton().mergeSelectedObjects();
 }
 
-void Skeletonizer::setSubobjectSelectAndMergeWithPrevious(const quint64 nodeId, const quint64 subobjectId, const quint64 previousActiveNodeId) {
-    auto & node = *Skeletonizer::singleton().findNodeByNodeID(nodeId);
-    if (previousActiveNodeId != 0) {
-        const auto & node2 = *Skeletonizer::singleton().findNodeByNodeID(previousActiveNodeId);
-        selectObjectForNode(node2);//reselect the previous active node as it got unselected during new node creation
-    }
+void Skeletonizer::setSubobjectSelectAndMergeWithPrevious(nodeListElement & node, const quint64 subobjectId, nodeListElement & previousNode) {
+    selectObjectForNode(previousNode);//reselect the previous active node as it got unselected during new node creation
     setSubobjectSelectAndMerge(node, subobjectId);
 }
 

skeleton/skeletonizer.h
@@ -184,7 +184,7 @@ public slots:
     static treeListElement *getTreeWithPrevID(treeListElement *currentTree);
     static treeListElement *getTreeWithNextID(treeListElement *currentTree);
     uint64_t findAvailableNodeID();
-    boost::optional<uint64_t> addNode(uint64_t nodeID, const float radius, const int treeID, const Coordinate & position, const ViewportType VPtype, const int inMag, boost::optional<uint64_t> time, const bool respectLocks, const QHash<QString, QVariant> & properties = {});
+    boost::optional<nodeListElement &> addNode(uint64_t nodeID, const float radius, const int treeID, const Coordinate & position, const ViewportType VPtype, const int inMag, boost::optional<uint64_t> time, const bool respectLocks, const QHash<QString, QVariant> & properties = {});
 
     static segmentListElement* addSegmentListElement(segmentListElement **currentSegment, nodeListElement *sourceNode, nodeListElement *targetNode);
 
@@ -196,23 +196,23 @@ public slots:
 
     bool delTree(int treeID);
     void clearSkeleton();
-    uint64_t UI_addSkeletonNode(const Coordinate & clickedCoordinate, ViewportType VPtype, const uint64_t nodeId = 0);
-    bool setActiveNode(nodeListElement *node, uint nodeID);
+    boost::optional<nodeListElement &> UI_addSkeletonNode(const Coordinate & clickedCoordinate, ViewportType VPtype);
+    bool setActiveNode(nodeListElement *node);
     bool addTreeCommentToSelectedTrees(QString comment);
     bool addTreeComment(int treeID, QString comment);
     static bool unlockPosition();
     static bool lockPosition(Coordinate lockCoordinate);
     commentListElement *nextComment(QString searchString);
     commentListElement *previousComment(QString searchString);
-    static bool delSegment(uint sourceNodeID, uint targetNodeID, segmentListElement *segToDel);
+    static bool delSegment(segmentListElement *segToDel);
     bool editNode(uint nodeID, nodeListElement *node, float newRadius, const Coordinate & newPos, int inMag);
     bool delNode(uint nodeID, nodeListElement *nodeToDel);
-    bool addComment(QString content, nodeListElement *node, uint nodeID);
+    bool addComment(QString content, nodeListElement &node);
     bool editComment(commentListElement *currentComment, uint nodeID, QString newContent, nodeListElement *newNode, uint newNodeID);
     bool setComment(QString newContent, nodeListElement *commentNode, uint commentNodeID);
     bool delComment(commentListElement *currentComment, uint commentNodeID);
-    void setSubobject(const quint64 nodeId, const quint64 subobjectId);
-    void setSubobjectSelectAndMergeWithPrevious(const quint64 nodeId, const quint64 subobjectId, const quint64 previousActiveNodeId);
+    void setSubobject(nodeListElement & node, const quint64 subobjectId);
+    void setSubobjectSelectAndMergeWithPrevious(nodeListElement & node, const quint64 subobjectId, nodeListElement & previousNode);
     void updateSubobjectCountFromProperty(nodeListElement & node);
     void unsetSubobjectOfHybridNode(nodeListElement & node);
     void movedHybridNode(nodeListElement & node, const quint64 newSubobjectId, const Coordinate & oldPos);
@@ -225,7 +225,7 @@ public slots:
 
     nodeListElement *popBranchNodeAfterConfirmation(QWidget * const parent);
     nodeListElement *popBranchNode();
-    bool pushBranchNode(int setBranchNodeFlag, int checkDoubleBranchpoint, nodeListElement *branchNode, uint branchNodeID);
+    bool pushBranchNode(int setBranchNodeFlag, int checkDoubleBranchpoint, nodeListElement & branchNode);
     bool moveToNextTree();
     bool moveToPrevTree();
     bool moveToPrevNode();
@@ -235,7 +235,7 @@ public slots:
     static QList<treeListElement *> findTrees(const QString & comment);
     static nodeListElement *findNodeByNodeID(uint nodeID);
     static QList<nodeListElement *> findNodesInTree(const treeListElement & tree, const QString & comment);
-    static bool addSegment(uint sourceNodeID, uint targetNodeID);
+    static bool addSegment(nodeListElement &sourceNodeID, nodeListElement &targetNodeID);
     void restoreDefaultTreeColor(treeListElement & tree);
 
     bool extractConnectedComponent(int nodeID);
@@ -245,15 +245,14 @@ public slots:
     void loadTreeLUT(const QString & path = ":/resources/color_palette/default.json");
     void updateTreeColors();
     static nodeListElement *findNodeInRadius(Coordinate searchPosition);
-    static segmentListElement *findSegmentByNodeIDs(uint sourceNodeID, uint targetNodeID);
-    uint64_t addSkeletonNodeAndLinkWithActive(const Coordinate & clickedCoordinate, ViewportType VPtype, int makeNodeActive);
+    static segmentListElement *findSegmentBetween(const nodeListElement & sourceNode, const nodeListElement & targetNode);
+    boost::optional<nodeListElement &> addSkeletonNodeAndLinkWithActive(const Coordinate & clickedCoordinate, ViewportType VPtype, int makeNodeActive);
 
     bool searchInComment(char *searchString, commentListElement *comment);
     static bool updateCircRadius(nodeListElement *node);
 
 public:
     bool areConnected(const nodeListElement & v,const nodeListElement & w) const; // true if a path between the two nodes can be found.
-
     void setColorFromNode(nodeListElement *node, color4F *color) const;
     float radius(const nodeListElement &node) const;
     float segmentSizeAt(const nodeListElement &node) const;

eventmodel.cpp
@@ -131,20 +131,21 @@ void EventModel::handleMouseButtonLeft(QMouseEvent *event, int VPfound) {
         }
         //Set Connection between Active Node and Clicked Node
         if (QApplication::keyboardModifiers() == Qt::ALT) {
-            auto clickedNodeId = state->viewer->renderer->retrieveVisibleObjectBeneathSquare(VPfound, event->x(), event->y(), 10) != 0;
+            auto clickedNode = state->viewer->renderer->retrieveVisibleObjectBeneathSquare(VPfound, event->x(), event->y(), 10);
             auto * activeNode = state->skeletonState->activeNode;
-            if (clickedNodeId != 0 && activeNode != nullptr) {
+            if (clickedNode && activeNode != nullptr) {
                 auto & skel = Skeletonizer::singleton();
-                if (skel.findSegmentByNodeIDs(activeNode->nodeID, clickedNodeId)) {
-                    emit delSegmentSignal(activeNode->nodeID, clickedNodeId, NULL);
-                } else if (skel.findSegmentByNodeIDs(clickedNodeId, activeNode->nodeID)) {
-                    emit delSegmentSignal(clickedNodeId, activeNode->nodeID, NULL);
+                auto * segment = skel.findSegmentBetween(*activeNode, clickedNode.get());
+                if (segment) {
+                    emit delSegmentSignal(segment);
+                } else if ((segment = skel.findSegmentBetween(clickedNode.get(), *activeNode))) {
+                    emit delSegmentSignal(segment);
                 } else{
-                    if(!Session::singleton().annotationMode.testFlag(AnnotationMode::SkeletonCycles) && Skeletonizer::singleton().areConnected(*activeNode, *Skeletonizer::findNodeByNodeID(clickedNodeId))) {
+                    if(!Session::singleton().annotationMode.testFlag(AnnotationMode::SkeletonCycles) && Skeletonizer::singleton().areConnected(*activeNode, clickedNode.get())) {
                         QMessageBox::information(nullptr, "Cycle detected!", "If you want to allow cycles, please select 'Advanced Tracing' in the dropdown menu in the toolbar.");
                         return;
                     }
-                    emit addSegmentSignal(activeNode->nodeID, clickedNodeId);
+                    emit addSegmentSignal(*activeNode, clickedNode.get());
                 }
             }
         }
@@ -153,27 +154,28 @@ void EventModel::handleMouseButtonLeft(QMouseEvent *event, int VPfound) {
 
 void EventModel::handleMouseButtonMiddle(QMouseEvent *event, int VPfound) {
     if (Session::singleton().annotationMode.testFlag(AnnotationMode::NodeEditing)) {
-        auto clickedNodeId = state->viewer->renderer->retrieveVisibleObjectBeneathSquare(VPfound, event->x(), event->y(), 10);
+        auto clickedNode = state->viewer->renderer->retrieveVisibleObjectBeneathSquare(VPfound, event->x(), event->y(), 10);
 
-        if (clickedNodeId != 0) {
+        if (clickedNode) {
             auto & skel = Skeletonizer::singleton();
             auto activeNode = state->skeletonState->activeNode;
             Qt::KeyboardModifiers keyMod = event->modifiers();
             if (keyMod.testFlag(Qt::ShiftModifier) && activeNode != nullptr) {
                 // Toggle segment between clicked and active node
-                if (skel.findSegmentByNodeIDs(activeNode->nodeID, clickedNodeId)) {
-                    emit delSegmentSignal(activeNode->nodeID, clickedNodeId, 0);
-                } else if (skel.findSegmentByNodeIDs(clickedNodeId, activeNode->nodeID)) {
-                    emit delSegmentSignal(clickedNodeId, activeNode->nodeID, 0);
+                auto * segment = skel.findSegmentBetween(*activeNode, clickedNode.get());
+                if (segment) {
+                    emit delSegmentSignal(segment);
+                } else if ((segment = skel.findSegmentBetween(clickedNode.get(), *activeNode))) {
+                    emit delSegmentSignal(segment);
                 } else {
-                    if(!Session::singleton().annotationMode.testFlag(AnnotationMode::SkeletonCycles) && Skeletonizer::singleton().areConnected(*activeNode, *Skeletonizer::findNodeByNodeID(clickedNodeId))) {
+                    if(!Session::singleton().annotationMode.testFlag(AnnotationMode::SkeletonCycles) && Skeletonizer::singleton().areConnected(*activeNode, clickedNode.get())) {
                         QMessageBox::information(nullptr, "Cycle detected!", "If you want to allow cycles, please select 'Advanced Tracing' in the dropdown menu in the toolbar.");
                         return;
                     }
-                    emit addSegmentSignal(activeNode->nodeID, clickedNodeId);
+                    emit addSegmentSignal(*activeNode, clickedNode.get());
                 }
             } else if (keyMod.testFlag(Qt::NoModifier)) {
-                state->viewerState->vpConfigs[VPfound].draggedNode = Skeletonizer::findNodeByNodeID(clickedNodeId);
+                state->viewerState->vpConfigs[VPfound].draggedNode = &clickedNode.get();
             }
         }
     }
@@ -198,33 +200,33 @@ void EventModel::handleMouseButtonRight(QMouseEvent *event, int VPfound) {
         return;
     }
 
-    uint64_t oldNodeId = state->skeletonState->activeNode != nullptr ? state->skeletonState->activeNode->nodeID : 0;
-    uint64_t newNodeId = 0;
+    nodeListElement * oldNode = state->skeletonState->activeNode;
+    boost::optional<nodeListElement &> newNode;
 
     if (annotationMode.testFlag(AnnotationMode::UnlinkedNodes)) {
-        newNodeId = Skeletonizer::singleton().UI_addSkeletonNode(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type);
+        newNode = Skeletonizer::singleton().UI_addSkeletonNode(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type);
     } else if (annotationMode.testFlag(AnnotationMode::LinkedNodes)) {
-        if (state->skeletonState->activeNode == nullptr || state->skeletonState->activeTree->firstNode == nullptr) {
+        if (oldNode == nullptr || state->skeletonState->activeTree->firstNode == nullptr) {
             //no node to link with or no empty tree
-            newNodeId = Skeletonizer::singleton().UI_addSkeletonNode(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type);
+            newNode = Skeletonizer::singleton().UI_addSkeletonNode(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type);
         } else if (event->modifiers().testFlag(Qt::ControlModifier)) {
             if (Session::singleton().annotationMode.testFlag(AnnotationMode::Mode_MergeTracing)) {
-                const auto splitNodeId = Skeletonizer::singleton().UI_addSkeletonNode(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type);
-                if (splitNodeId != 0) {
+                const auto splitNode = Skeletonizer::singleton().UI_addSkeletonNode(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type);
+                if (splitNode) {
                     const auto comment = background ? "ecs" : "split";
-                    Skeletonizer::singleton().setSubobject(splitNodeId, subobjectId);
-                    Skeletonizer::singleton().addComment(comment, nullptr, splitNodeId);
-                    Skeletonizer::singleton().setActiveNode(nullptr, oldNodeId);
+                    Skeletonizer::singleton().setSubobject(splitNode.get(), subobjectId);
+                    Skeletonizer::singleton().addComment(comment, splitNode.get());
+                    Skeletonizer::singleton().setActiveNode(oldNode);
                 }
-            } else if (const auto stumpNodeId = Skeletonizer::singleton().addSkeletonNodeAndLinkWithActive(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type, false)) {
+            } else if (auto stumpNode = Skeletonizer::singleton().addSkeletonNodeAndLinkWithActive(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type, false)) {
                 //Add a "stump", a branch node to which we don't automatically move.
-                Skeletonizer::singleton().pushBranchNode(true, true, NULL, stumpNodeId);
-                Skeletonizer::singleton().setActiveNode(nullptr, oldNodeId);
+                Skeletonizer::singleton().pushBranchNode(true, true, stumpNode.get());
+                Skeletonizer::singleton().setActiveNode(oldNode);
             }
         } else {
             const auto lastPos = state->skeletonState->activeNode->position;
-            newNodeId = Skeletonizer::singleton().addSkeletonNodeAndLinkWithActive(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type, true);
-            if (newNodeId == 0) {
+            newNode = Skeletonizer::singleton().addSkeletonNodeAndLinkWithActive(clickedCoordinate, state->viewerState->vpConfigs[VPfound].type, true);
+            if (!newNode) {
                 return;
             }
             const auto movement = clickedCoordinate - lastPos;
@@ -265,9 +267,9 @@ void EventModel::handleMouseButtonRight(QMouseEvent *event, int VPfound) {
         }
     }
 
-    if (newNodeId != 0) {
+    if (newNode) {
         if (Session::singleton().annotationMode.testFlag(AnnotationMode::Mode_MergeTracing)) {
-            Skeletonizer::singleton().setSubobjectSelectAndMergeWithPrevious(newNodeId, subobjectId, oldNodeId);
+            Skeletonizer::singleton().setSubobjectSelectAndMergeWithPrevious(newNode.get(), subobjectId, *oldNode);
         }
         // Move to the new node position
         if (state->viewerState->vpConfigs[VPfound].type == VIEWPORT_ARBITRARY) {
@@ -409,10 +411,9 @@ void EventModel::handleMouseReleaseLeft(QMouseEvent *event, int VPfound) {
     int diffY = std::abs(state->viewerState->nodeSelectionSquare.first.y - event->pos().y());
     if ((diffX < 5 && diffY < 5) || (event->pos() - mouseDown).manhattanLength() < 5) { // interpreted as click instead of drag
         // mouse released on same spot on which it was pressed down: single node selection
-        auto nodeID = state->viewer->renderer->retrieveVisibleObjectBeneathSquare(VPfound, event->pos().x(), event->pos().y(), 10);
-        nodeListElement *selectedNode = Skeletonizer::findNodeByNodeID(nodeID);
-        if (nodeID != 0 && selectedNode != nullptr) {
-            selectedNodes = {selectedNode};
+        auto selectedNode = state->viewer->renderer->retrieveVisibleObjectBeneathSquare(VPfound, event->pos().x(), event->pos().y(), 10);
+        if (selectedNode) {
+            selectedNodes = {&selectedNode.get()};
         }
     } else if (state->viewerState->nodeSelectSquareVpId != -1) {
         selectedNodes = nodeSelection(event->pos().x(), event->pos().y(), VPfound);
@@ -930,7 +931,7 @@ void EventModel::handleKeyPress(QKeyEvent *event, int VPfound) {
             prompt.addButton("No", QMessageBox::ActionRole);
             prompt.exec();
             if(prompt.clickedButton() == confirmButton) {
-                Skeletonizer::singleton().setActiveNode(state->skeletonState->activeNode, 0);
+                Skeletonizer::singleton().setActiveNode(state->skeletonState->activeNode);
             }
         }
     }

eventmodel.h
@@ -101,10 +101,9 @@ signals:
 
     void setRecenteringPositionSignal(float x, float y, float z);
     void setRecenteringPositionWithRotationSignal(float x, float y, float z, uint vp);
-    void delSegmentSignal(uint sourceNodeID, uint targetNodeID, segmentListElement *segToDel);
-    void addCommentSignal(const char *content, nodeListElement *node, uint nodeID);
+    void delSegmentSignal(segmentListElement *segToDel);
     bool editCommentSignal(commentListElement *currentComment, uint nodeID, char *newContent, nodeListElement *newNode, uint newNodeID);
-    void addSegmentSignal(uint sourceNodeID, uint targetNodeID);
+    void addSegmentSignal(nodeListElement & sourceNode, nodeListElement & targetNode);
 
     void compressionRatioToggled();
 };

renderer.cpp
@@ -1677,13 +1677,12 @@ void Renderer::renderArbitrarySlicePane(const vpConfig & vp) {
     glBindTexture (GL_TEXTURE_2D, 0);
 }
 
-uint Renderer::retrieveVisibleObjectBeneathSquare(uint currentVP, uint x, uint y, uint width) {
+boost::optional<nodeListElement &> Renderer::retrieveVisibleObjectBeneathSquare(uint currentVP, uint x, uint y, uint width) {
     const auto & nodes = retrieveAllObjectsBeneathSquare(currentVP, x, y, width, width);
     if (nodes.size() != 0) {
-        return (*std::begin(nodes))->nodeID;
-    } else {
-        return 0;//no node found
+        return *(*std::begin(nodes));
     }
+    return boost::none;
 }
 
 QSet<nodeListElement *> Renderer::retrieveAllObjectsBeneathSquare(uint currentVP, uint centerX, uint centerY, uint selectionWidth, uint selectionHeight) {

renderer.h
@@ -32,6 +32,7 @@
 #include <QSet>
 
 #include <boost/multi_array.hpp>
+#include <boost/optional.hpp>
 
 constexpr int defaultFonsSize = 10;
 
@@ -84,7 +85,7 @@ protected:
     bool updateFrustumClippingPlanes(uint viewportType);
 
 public slots:
-    uint retrieveVisibleObjectBeneathSquare(uint currentVP, uint x, uint y, uint width);
+    boost::optional<nodeListElement &> retrieveVisibleObjectBeneathSquare(uint currentVP, uint x, uint y, uint width);
     QSet<nodeListElement *> retrieveAllObjectsBeneathSquare(uint currentVP, uint centerX, uint centerY, uint width, uint height);
     bool renderOrthogonalVP(uint currentVP, const RenderOptions & options = RenderOptions());
     bool renderSkeletonVP(const RenderOptions & options = RenderOptions());

scriptengine/proxies/skeletonproxy.cpp
@@ -176,7 +176,12 @@ void SkeletonProxy::export_converter(const QString &path) {
 }
 
 segmentListElement *SkeletonProxy::find_segment(int source_id, int target_id) {
-    return Skeletonizer::findSegmentByNodeIDs(source_id, target_id);
+    auto * sourceNode = Skeletonizer::singleton().findNodeByNodeID(source_id);
+    auto * targetNode = Skeletonizer::singleton().findNodeByNodeID(target_id);
+    if(sourceNode && targetNode) {
+        return Skeletonizer::findSegmentBetween(*sourceNode, *targetNode);
+    }
+    return nullptr;
 }
 
 void SkeletonProxy::jump_to_node(nodeListElement *node) {
@@ -194,11 +199,11 @@ void SkeletonProxy::delete_skeleton() {
 }
 
 bool SkeletonProxy::delete_segment(int source_id, int target_id) {
-    if(!Skeletonizer::delSegment(source_id, target_id, NULL)) {
-        emit echo(QString("could not delete the segment with source id %1 and target id %2").arg(source_id).arg(target_id));
-        return false;
+    if(auto *segment = find_segment(source_id, target_id)) {
+        Skeletonizer::delSegment(segment);
+        return true;
     }
-    return true;
+    return false;
 }
 
 bool SkeletonProxy::delete_node(int node_id) {
@@ -210,7 +215,7 @@ bool SkeletonProxy::delete_node(int node_id) {
 }
 
 bool SkeletonProxy::set_active_node(int node_id) {
-    if (!Skeletonizer::singleton().setActiveNode(0, node_id)) {
+    if (!Skeletonizer::singleton().setActiveNode(find_node_by_id(node_id))) {
         emit echo(QString("could not set the node with id %1 to active node").arg(node_id));
         return false;
     }
@@ -283,20 +288,25 @@ bool SkeletonProxy::delete_comment(int node_id) {
 }
 
 bool SkeletonProxy::add_segment(int source_id, int target_id) {
-    if (!Skeletonizer::addSegment(source_id, target_id)) {
-        emit echo(QString("could not add a segment with source id %1 and target id %2").arg(source_id).arg(target_id));
-        return false;
+    auto * sourceNode = Skeletonizer::findNodeByNodeID(source_id);
+    auto * targetNode = Skeletonizer::findNodeByNodeID(target_id);
+    if(sourceNode != nullptr && targetNode != nullptr) {
+        if (!Skeletonizer::addSegment(*sourceNode, *targetNode)) {
+            emit echo(QString("could not add a segment with source id %1 and target id %2").arg(source_id).arg(target_id));
+            return false;
+        }
+        return true;
     }
-    return true;
+    return false;
 }
 
 bool SkeletonProxy::set_branch_node(int node_id) {
     nodeListElement *currentNode = Skeletonizer::findNodeByNodeID(node_id);
-    if(NULL == currentNode) {
+    if(nullptr == currentNode) {
         emit echo(QString("no node with id %1 found").arg(node_id));
         return false;
     }
-    if (!Skeletonizer::singleton().pushBranchNode(true, false, currentNode, 0)) {
+    if (!Skeletonizer::singleton().pushBranchNode(true, false, *currentNode)) {
         emit echo(QString("An unexpected error occured while adding a branch node"));
         return false;
     }

skeleton/node.cpp
@@ -5,6 +5,10 @@
 nodeListElement::nodeListElement(const uint64_t nodeID, const float radius, const Coordinate & position, const int inMag, const ViewportType inVP, const uint64_t ms, const QVariantHash & properties, treeListElement & tree)
         : nodeID{nodeID}, radius{radius}, position{position}, createdInMag{inMag}, createdInVp{inVP}, timestamp{ms}, properties{properties}, correspondingTree{&tree} {}
 
+bool nodeListElement::operator==(const nodeListElement & other) {
+    return nodeID == other.nodeID;
+}
+
 QList<segmentListElement *> *nodeListElement::getSegments() {
     QList<segmentListElement *> *segments = new QList<segmentListElement *>();
     segmentListElement *currentSegment = firstSegment;

commit 92764e5105fa0f6a913fcb35e129f9b933c8e5ce

check previous node existence (fix #288)
regression from d5bb7699357b4225e79e8807e614baaa5e813e8f

skeleton/segmentationSkeletonBridge.cpp
@@ -37,8 +37,10 @@ void setSubobjectSelectAndMerge(nodeListElement & node, const quint64 subobjectI
     Segmentation::singleton().mergeSelectedObjects();
 }
 
-void Skeletonizer::setSubobjectSelectAndMergeWithPrevious(nodeListElement & node, const quint64 subobjectId, nodeListElement & previousNode) {
-    selectObjectForNode(previousNode);//reselect the previous active node as it got unselected during new node creation
+void Skeletonizer::setSubobjectSelectAndMergeWithPrevious(nodeListElement & node, const quint64 subobjectId, nodeListElement * previousNode) {
+    if (previousNode != nullptr) {
+        selectObjectForNode(*previousNode);//reselect the previous active node as it got unselected during new node creation
+    }
     setSubobjectSelectAndMerge(node, subobjectId);
 }
 

skeleton/skeletonizer.h
@@ -200,7 +200,7 @@ public slots:
     bool setComment(QString newContent, nodeListElement *commentNode, uint commentNodeID);
     bool delComment(commentListElement *currentComment, uint commentNodeID);
     void setSubobject(nodeListElement & node, const quint64 subobjectId);
-    void setSubobjectSelectAndMergeWithPrevious(nodeListElement & node, const quint64 subobjectId, nodeListElement & previousNode);
+    void setSubobjectSelectAndMergeWithPrevious(nodeListElement & node, const quint64 subobjectId, nodeListElement * previousNode);
     void updateSubobjectCountFromProperty(nodeListElement & node);
     void unsetSubobjectOfHybridNode(nodeListElement & node);
     void movedHybridNode(nodeListElement & node, const quint64 newSubobjectId, const Coordinate & oldPos);

widgets/viewportevents.cpp
@@ -275,7 +275,7 @@ void ViewportBase::handleMouseButtonRight(QMouseEvent *event) {
 
     if (newNode) {
         if (Session::singleton().annotationMode.testFlag(AnnotationMode::Mode_MergeTracing)) {
-            Skeletonizer::singleton().setSubobjectSelectAndMergeWithPrevious(newNode.get(), subobjectId, *oldNode);
+            Skeletonizer::singleton().setSubobjectSelectAndMergeWithPrevious(newNode.get(), subobjectId, oldNode);
         }
         // Move to the new node position
         if (viewportType == VIEWPORT_ARBITRARY) {