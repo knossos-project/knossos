knossos-project/knossos

commit e9c6bf8934c14485f6b4ebb1a3f60b5c552d6c9a

std::list<segmentListElement>
source and target are references now, therefore no more null-checks
(what are segments with missing source or target anyway)
moved the 3 cycle checks to gui_wrapper
merged extractConnectedComponent and areConnected

remote.cpp
@@ -79,60 +79,59 @@ bool Remote::remoteJump(const Coordinate & jumpVec) {
 }
 
 std::deque<floatCoordinate> Remote::getLastNodes() {
-
     std::deque<floatCoordinate> nodelist;
     nodelist.clear();
     floatCoordinate pos;
 
-    nodeListElement *node = state->skeletonState->activeNode;
-    nodeListElement *lastnode=NULL;
+    nodeListElement const * node = state->skeletonState->activeNode;
+    nodeListElement const * previousNode=NULL;
 
-    if(node == NULL) {
+    if (node == nullptr) {
         return nodelist;
     }
-    if(node->firstSegment == NULL || node->getSegments()->size() > 1) { //we are in the middle of our skeleton or we don't have a skeleton
+    if (node->segments.size() != 1) { //we are in the middle of our skeleton or we don't have a skeleton
         return nodelist;
     }
 
-    for(int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 10; ++i) {
         qDebug() << node->nodeID;
 
-        if(node->getSegments()->size() == 1) {
-            if(node->firstSegment->source == lastnode || node->firstSegment->target == lastnode) break;
-            if(node->firstSegment->source == node) {
-                lastnode = node;
-                node = node->firstSegment->target;
-            } else {
-                lastnode = node;
-                node= node->firstSegment->source;
+        if (node->segments.size() == 1) {
+            if (node->segments.front().source == *previousNode || node->segments.front().target == *previousNode) {
+                break;
             }
-        } else if(node->getSegments()->size() == 0) {
+            if (node->segments.front().source == *node) {
+                previousNode = node;
+                node = &node->segments.front().target;
+            } else {
+                previousNode = node;
+                node = &node->segments.front().source;
+            }
+        } else if(node->segments.empty()) {
             break;
         } else {
-            if(node->firstSegment->next->source == lastnode || node->firstSegment->next->target == lastnode) {
+            auto nextIt = std::next(std::begin(node->segments));
+            if (nextIt->source == *previousNode || nextIt->target == *previousNode) {
                 //user firstSegment
-                if(node->firstSegment->source == node) {
-                    lastnode = node;
-                    node = node->firstSegment->target;
+                if (node->segments.front().source == *node) {
+                    previousNode = node;
+                    node = &node->segments.front().target;
                 } else {
-                    lastnode = node;
-                    node = node->firstSegment->source;
+                    previousNode = node;
+                    node = &node->segments.front().source;
                 }
             } else {
-                if(node->firstSegment->next->source == node) {
-                    lastnode = node;
-                    node = node->firstSegment->next->target;
+                if (nextIt->source == *node) {
+                    previousNode = node;
+                    node = &nextIt->target;
                 } else {
-                    lastnode = node;
-                    node = node->firstSegment->next->source;
+                    previousNode = node;
+                    node = &nextIt->source;
                 }
             }
         }
 
-        pos.x = node->position.x;
-        pos.y = node->position.y;
-        pos.z = node->position.z;
-
+        pos = node->position;
         nodelist.push_back(pos);
     }
 

gui_wrapper.cpp
@@ -1,5 +1,6 @@
 #include "gui_wrapper.h"
 
+#include "session.h"
 #include "skeleton/skeletonizer.h"
 
 #include <QMessageBox>
@@ -16,4 +17,16 @@ void checkedMoveNodes(QWidget * parent, int treeID) {
     if (prompt.clickedButton() == confirmButton) {
         Skeletonizer::singleton().moveSelectedNodesToTree(treeID);
     }
+}
+
+void checkedToggleNodeLink(QWidget * parent, nodeListElement & lhs, nodeListElement & rhs) {
+    if (!Session::singleton().annotationMode.testFlag(AnnotationMode::SkeletonCycles) && Skeletonizer::singleton().areConnected(lhs, rhs)) {
+        QMessageBox prompt(parent);
+        prompt.setIcon(QMessageBox::Warning);
+        prompt.setText(QObject::tr("Cycle detected!"));
+        prompt.setInformativeText(QObject::tr("If you want to allow cycles, please select 'Advanced Tracing' in the dropdown menu in the toolbar."));
+        prompt.exec();
+        return;
+    }
+    Skeletonizer::singleton().toggleLink(lhs, rhs);
 }

gui_wrapper.h
@@ -1,6 +1,9 @@
 #ifndef GUI_WRAPPER_H
 #define GUI_WRAPPER_H
 
+#include "skeleton/node.h"
+
 void checkedMoveNodes(QWidget * parent, int treeID);
+void checkedToggleNodeLink(QWidget * parent, nodeListElement & lhs, nodeListElement & rhs);
 
 #endif//GUI_WRAPPER_H

scriptengine/decorators/segmentlistdecorator.cpp
@@ -4,26 +4,20 @@
 
 SegmentListDecorator::SegmentListDecorator(QObject *parent) : QObject(parent) {}
 
-nodeListElement *SegmentListDecorator::source(segmentListElement *self) {
+nodeListElement & SegmentListDecorator::source(segmentListElement *self) {
     return self->source;
 }
 
-nodeListElement *SegmentListDecorator::target(segmentListElement *self) {
+nodeListElement & SegmentListDecorator::target(segmentListElement *self) {
     return self->target;
 }
 
 int SegmentListDecorator::source_id(segmentListElement *self) {
-    if(self->source)
-        return self->source->nodeID;
-
-    return 0;
+    return self->source.nodeID;
 }
 
 int SegmentListDecorator::target_id(segmentListElement *self) {
-    if(self->target)
-        return self->target->nodeID;
-
-    return 0;
+    return self->target.nodeID;
 }
 
 QString SegmentListDecorator::static_Segment_help() {

scriptengine/decorators/segmentlistdecorator.h
@@ -10,12 +10,12 @@ class SegmentListDecorator : public QObject
     Q_OBJECT
 public:
     explicit SegmentListDecorator(QObject *parent = 0);
-    
+
 signals:
-    
+
 public slots:
-    nodeListElement *source(segmentListElement *self);
-    nodeListElement *target(segmentListElement *self);
+    nodeListElement & source(segmentListElement *self);
+    nodeListElement & target(segmentListElement *self);
     int source_id(segmentListElement *self);
     int target_id(segmentListElement *self);
     QString static_Segment_help();

scriptengine/proxies/skeletonproxy.cpp
@@ -176,15 +176,6 @@ void SkeletonProxy::export_converter(const QString &path) {
 
 }
 
-segmentListElement *SkeletonProxy::find_segment(int source_id, int target_id) {
-    auto * sourceNode = Skeletonizer::singleton().findNodeByNodeID(source_id);
-    auto * targetNode = Skeletonizer::singleton().findNodeByNodeID(target_id);
-    if(sourceNode && targetNode) {
-        return Skeletonizer::findSegmentBetween(*sourceNode, *targetNode);
-    }
-    return nullptr;
-}
-
 void SkeletonProxy::jump_to_node(nodeListElement *node) {
     if(node) {
         Skeletonizer::singleton().jumpToNode(*node);
@@ -200,9 +191,14 @@ void SkeletonProxy::delete_skeleton() {
 }
 
 bool SkeletonProxy::delete_segment(int source_id, int target_id) {
-    if(auto *segment = find_segment(source_id, target_id)) {
-        Skeletonizer::delSegment(segment);
-        return true;
+    auto * sourceNode = Skeletonizer::singleton().findNodeByNodeID(source_id);
+    auto * targetNode = Skeletonizer::singleton().findNodeByNodeID(target_id);
+    if (sourceNode && targetNode) {
+        auto segmentIt = Skeletonizer::findSegmentBetween(*sourceNode, *targetNode);
+        if (segmentIt != std::end(sourceNode->segments)) {
+            Skeletonizer::singleton().delSegment(segmentIt);
+            return true;
+        }
     }
     return false;
 }
@@ -292,7 +288,7 @@ bool SkeletonProxy::add_segment(int source_id, int target_id) {
     auto * sourceNode = Skeletonizer::findNodeByNodeID(source_id);
     auto * targetNode = Skeletonizer::findNodeByNodeID(target_id);
     if(sourceNode != nullptr && targetNode != nullptr) {
-        if (!Skeletonizer::addSegment(*sourceNode, *targetNode)) {
+        if (!Skeletonizer::singleton().addSegment(*sourceNode, *targetNode)) {
             emit echo(QString("could not add a segment with source id %1 and target id %2").arg(source_id).arg(target_id));
             return false;
         }

scriptengine/proxies/skeletonproxy.h
@@ -73,7 +73,6 @@ public slots:
     bool set_branch_node(int node_id);
     bool add_segment(int source_id, int target_id);
     bool delete_segment(int source_id, int target_id);
-    segmentListElement *find_segment(int source_id, int target_id);
     bool delete_comment(int node_id);
     bool set_comment(int node_id, char *comment);
 

skeleton/node.cpp
@@ -5,16 +5,14 @@
 nodeListElement::nodeListElement(const uint64_t nodeID, const float radius, const Coordinate & position, const int inMag, const ViewportType inVP, const uint64_t ms, const QVariantHash &properties, treeListElement & tree)
         : nodeID{nodeID}, radius{radius}, position{position}, createdInMag{inMag}, createdInVp{inVP}, timestamp{ms}, properties{properties}, correspondingTree{&tree} {}
 
-bool nodeListElement::operator==(const nodeListElement & other) {
+bool nodeListElement::operator==(const nodeListElement & other) const {
     return nodeID == other.nodeID;
 }
 
 QList<segmentListElement *> *nodeListElement::getSegments() {
-    QList<segmentListElement *> *segments = new QList<segmentListElement *>();
-    segmentListElement *currentSegment = firstSegment;
-    while(currentSegment) {
-        segments->append(currentSegment);
-        currentSegment = currentSegment->next;
+    QList<segmentListElement *> * segmentList = new QList<segmentListElement *>();
+    for (auto & segment : segments) {
+        segmentList->append(&segment);
     }
-    return segments;
+    return segmentList;
 }

commit 230b580574d7f642a1499154775154a48ebac7da

prevent null deref during arbitrary recentering
regression from e9c6bf8934c14485f6b4ebb1a3f60b5c552d6c9a

remote.cpp
@@ -84,7 +84,7 @@ std::deque<floatCoordinate> Remote::getLastNodes() {
     floatCoordinate pos;
 
     nodeListElement const * node = state->skeletonState->activeNode;
-    nodeListElement const * previousNode=NULL;
+    nodeListElement const * previousNode = nullptr;
 
     if (node == nullptr) {
         return nodelist;
@@ -97,7 +97,7 @@ std::deque<floatCoordinate> Remote::getLastNodes() {
         qDebug() << node->nodeID;
 
         if (node->segments.size() == 1) {
-            if (node->segments.front().source == *previousNode || node->segments.front().target == *previousNode) {
+            if (previousNode != nullptr && (node->segments.front().source == *previousNode || node->segments.front().target == *previousNode)) {
                 break;
             }
             if (node->segments.front().source == *node) {
@@ -111,7 +111,7 @@ std::deque<floatCoordinate> Remote::getLastNodes() {
             break;
         } else {
             auto nextIt = std::next(std::begin(node->segments));
-            if (nextIt->source == *previousNode || nextIt->target == *previousNode) {
+            if (previousNode != nullptr && (nextIt->source == *previousNode || nextIt->target == *previousNode)) {
                 //user firstSegment
                 if (node->segments.front().source == *node) {
                     previousNode = node;