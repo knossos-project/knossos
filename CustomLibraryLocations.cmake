56916ee2e638aef1b3b41bf82984bb25772bfe22
orenshatz <shatz.oren@gmail.com>
2015-12-13 18:38:06 +0100

    undo false commit

--- CustomLibraryLocations.cmake
+++ CustomLibraryLocations.cmake
@@ -1,7 +1,6 @@
 #specify extraordinary locations for convenience here (forward slashes essential)
 #cmake searches for headers and libraries both directly and inside include/lib folders below
 set(CMAKE_PREFIX_PATH
-    "C:/msys64/mingw64"
     "${CMAKE_PREFIX_PATH}"#append
 
 # For Mac development: required to find packages from Homebrew
