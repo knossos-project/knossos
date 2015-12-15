9ac071c94231503bc54705989d31c9f3e11f86ee
orenshatz <shatz.oren@gmail.com>
2015-12-15 15:43:09 +0100

    Scripting::reloadPlugin: check if imported before reloading

--- scriptengine/scripting.cpp
+++ scriptengine/scripting.cpp
@@ -361,8 +361,10 @@ bool Scripting::reloadPlugin(const QString &pluginName, bool isQuiet) {
     if (!closePlugin(pluginName, isQuiet)) {
         return pluginActionError(actionStr, pluginName, "close failed", isQuiet);
     }
-    if (!evalScript(QString("reload(%1) <> None").arg(getImportInContainerStr(pluginName)), Py_eval_input).toBool()) {
-        return pluginActionError(actionStr, pluginName, "reload failed", isQuiet);
+    if (isPluginImported(pluginName))  {
+        if (!evalScript(QString("reload(%1) <> None").arg(getImportInContainerStr(pluginName)), Py_eval_input).toBool()) {
+            return pluginActionError(actionStr, pluginName, "reload failed", isQuiet);
+        }
     }
     if (!openPlugin(pluginName, isQuiet)) {
         return pluginActionError(actionStr, pluginName, "open failed", isQuiet);
