be521182337f51b6a6376511d49de21e663898e8
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2016-02-07 23:32:45 +0100

    disable python console in debug mode on windows because it crashes

--- widgets/pythoninterpreterwidget.cpp
+++ widgets/pythoninterpreterwidget.cpp
@@ -14,7 +14,9 @@ PythonInterpreterWidget::PythonInterpreterWidget(QWidget *parent) : QDialog(pare
 }
 
 void PythonInterpreterWidget::startConsole() {
-    console = new PythonQtScriptingConsole((QWidget*)this->parent(), PythonQt::self()->getMainModule());
+#if defined(WIN32) && defined(NDEBUG)// PythonQt bug: QWidget: Must construct a QApplication before a QWidget
+    console = new PythonQtScriptingConsole(this, PythonQt::self()->getMainModule());
+#endif
     state->scripting->addObject("knossos_global_interpreter_console", console);
     mainLayout.addWidget(console);
 }
