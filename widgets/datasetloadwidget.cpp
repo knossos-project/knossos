knossos-project/knossos

commit c6fd7ce2b06eb7340878758593596e6bbd0a3d5e

only save dataset when actually loading (fix #254)

widgets/datasetloadwidget.cpp
@@ -221,16 +221,16 @@ void DatasetLoadWidget::processButtonClicked() {
  * 2. for multires datasets: by selecting the dataset folder (the folder containing the "magX" subfolders)
  * 3. by specifying a .conf directly.
  */
-bool DatasetLoadWidget::loadDataset(const boost::optional<bool> loadOverlay, QString path,  const bool keepAnnotation) {
+bool DatasetLoadWidget::loadDataset(const boost::optional<bool> loadOverlay, QUrl path,  const bool keepAnnotation) {
     if (path.isEmpty() && datasetUrl.isEmpty()) {//no dataset available to load
         open();
         return false;
-    } else if (!path.isEmpty()) {//if empty reload previous
-        datasetUrl = {path};//remember config url
+    } else if (path.isEmpty()) {//if empty reload previous
+        path = datasetUrl;
     }
-    const auto download = Network::singleton().refresh(datasetUrl);
+    const auto download = Network::singleton().refresh(path);
     if (!download.first) {
-        QMessageBox::information(this, "Unable to load", QString("Failed to read config from %1").arg(datasetUrl.toString()));
+        QMessageBox::information(this, "Unable to load", QString("Failed to read config from %1").arg(path.toString()));
         return false;
     }
 
@@ -256,6 +256,7 @@ bool DatasetLoadWidget::loadDataset(const boost::optional<bool> loadOverlay, QSt
             return false;
         }
     }
+    datasetUrl = {path};//remember config url
     Loader::Controller::singleton().suspendLoader();//we change variables the loader uses
     info.applyToState();
     raw_compression = info.compressionRatio == 0 ? Dataset::CubeType::RAW_UNCOMPRESSED : info.compressionRatio == 1000 ? Dataset::CubeType::RAW_JPG

widgets/datasetloadwidget.h
@@ -42,7 +42,7 @@ public:
 
     explicit DatasetLoadWidget(QWidget *parent = 0);
     void changeDataset(bool isGUI);
-    bool loadDataset(const boost::optional<bool> loadOverlay, QString path = "", const bool keepAnnotation = false);
+    bool loadDataset(const boost::optional<bool> loadOverlay, QUrl path = {}, const bool keepAnnotation = false);
     void saveSettings();
     void loadSettings();
     void applyGeometrySettings();

commit b3c2ac03ba25fe3111029ebd70620702f68e81b8

use correct path, datasetUrl only is set later (regression from c6fd7ce2b06eb7340878758593596e6bbd0a3d5e)

widgets/datasetloadwidget.cpp
@@ -248,10 +248,10 @@ bool DatasetLoadWidget::loadDataset(const boost::optional<bool> loadOverlay, QUr
 
     Dataset info;
     Dataset::CubeType raw_compression;
-    if (datasetUrl.toString().contains("/ocp/ca/")) {
-        info = Dataset::parseOpenConnectomeJson(datasetUrl, download.second);
+    if (path.toString().contains("/ocp/ca/")) {
+        info = Dataset::parseOpenConnectomeJson(path, download.second);
     } else {
-        info = Dataset::fromLegacyConf(datasetUrl, download.second);
+        info = Dataset::fromLegacyConf(path, download.second);
         try {
             info.checkMagnifications();
         } catch (std::exception &) {