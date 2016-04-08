knossos-project/knossos

commit 990f37ae844dc83c56d3683aeacdac218f082b6f

delete comment if empty in setComment overload

skeleton/skeletonizer.cpp
@@ -1920,10 +1920,19 @@ bool Skeletonizer::setComment(QString newContent, nodeListElement *commentNode, 
         qDebug() << "Please provide a valid comment node to set!";
         return false;
     }
-    if(commentNode->comment) {
-        return editComment(commentNode->comment, 0, newContent, NULL, 0);
+    setComment(*commentNode, newContent);
+    return true;
+}
+
+void Skeletonizer::setComment(nodeListElement & commentNode, const QString & newContent) {
+    if (commentNode.comment) {
+        if (newContent.isEmpty()) {
+            delComment(commentNode.comment, 0);
+        } else {
+            editComment(commentNode.comment, 0, newContent, nullptr, 0);
+        }
     }
-    return addComment(newContent, commentNode, 0);
+    addComment(newContent, &commentNode, 0);
 }
 
 bool Skeletonizer::delComment(commentListElement *currentComment, uint commentNodeID) {

skeleton/skeletonizer.h
@@ -221,6 +221,7 @@ public slots:
     bool delNode(uint nodeID, nodeListElement *nodeToDel);
     bool addComment(QString content, nodeListElement *node, uint nodeID);
     bool editComment(commentListElement *currentComment, uint nodeID, QString newContent, nodeListElement *newNode, uint newNodeID);
+    void setComment(nodeListElement & commentNode, const QString & newContent);
     bool setComment(QString newContent, nodeListElement *commentNode, uint commentNodeID);
     bool delComment(commentListElement *currentComment, uint commentNodeID);
     void setSubobject(const quint64 nodeId, const quint64 subobjectId);

widgets/tools/skeletonview.cpp
@@ -342,11 +342,7 @@ SkeletonView::SkeletonView(QWidget * const parent) : QWidget(parent) {
         if (applied) {
             prevComment = comment;
             for (auto node : state->skeletonState->selectedNodes) {
-                if (!comment.isEmpty()) {
-                    Skeletonizer::singleton().setComment(comment.toUtf8().data(), node, 0);
-                } else if (node->comment != nullptr) {
-                    Skeletonizer::singleton().delComment(node->comment, node->nodeID);
-                }
+                Skeletonizer::singleton().setComment(*node,comment);
             }
         }
     });

commit 34b5538d23f52d2387cfbc79681ad3bf7ebbec55

setComment always added a new comment (regression from 990f37ae844dc83c56d3683aeacdac218f082b6f)

skeleton/skeletonizer.cpp
@@ -1423,14 +1423,15 @@ bool Skeletonizer::setComment(QString newContent, nodeListElement *commentNode, 
 }
 
 void Skeletonizer::setComment(nodeListElement & commentNode, const QString & newContent) {
-    if (commentNode.comment) {
+    if (commentNode.comment == nullptr) {
+        addComment(newContent, commentNode);
+    } else {
         if (newContent.isEmpty()) {
             delComment(commentNode.comment, 0);
         } else {
             editComment(commentNode.comment, 0, newContent, nullptr, 0);
         }
     }
-    addComment(newContent, commentNode);
 }
 
 bool Skeletonizer::delComment(commentListElement *currentComment, uint commentNodeID) {