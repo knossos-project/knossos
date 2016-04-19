1977eb309e7d0b46de660cbc4d2b465576f66f09
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2016-04-19 09:56:06 +0200

    use first proxy index

--- widgets/tools/segmentationview.cpp
+++ widgets/tools/segmentationview.cpp
@@ -483,7 +483,7 @@ void SegmentationView::updateSelection() {
     objectSelectionProtection = false;
 
     if (!selectedItems.indexes().isEmpty()) {// scroll to first selected entry
-        objectsTable.scrollTo(objectProxyModelComment.mapFromSource(objectProxyModelCategory.mapFromSource(selectedItems.indexes().front())));
+        objectsTable.scrollTo(proxySelection.indexes().front());
     }
 }
 
