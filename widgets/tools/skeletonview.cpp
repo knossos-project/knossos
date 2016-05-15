f2ecd46e42aaf5c41edaea83170a9caf00947ac9
My-Tien Nguyen <adtieni@gmail.com>
2016-05-15 16:23:43 +0200

    node table: mark active node row with an asterisk

--- widgets/tools/skeletonview.cpp
+++ widgets/tools/skeletonview.cpp
@@ -87,6 +87,9 @@ QVariant NodeModel::data(const QModelIndex &index, int role) const {
     }
     const auto & node = cache[index.row()].get();
 
+    if (&node == state->skeletonState->activeNode && index.column() == 0 && role == Qt::DecorationRole) {
+        return QPixmap(":/resources/icons/fa-play.png").scaled(QSize(8, 8), Qt::KeepAspectRatio);
+    }
     if (role == Qt::DisplayRole || role == Qt::EditRole) {
         switch (index.column()) {
         case 0: return static_cast<quint64>(node.nodeID);
