knossos-project/knossos

commit 02cf692d5b13eb5cf05f9f3fc5990c54b8b554e4

use QColor for tree (and node) color, and std::array for lines and points color
remove color4F (and its decorator)
remove mesh: replaced by small anonymous class in viewer using std::vector

widgets/renderer.cpp
@@ -23,7 +23,6 @@
  */
 #include "widgets/viewport.h"
 
-#include "mesh.h"
 #include "profiler.h"
 #include "segmentation/cubeloader.h"
 #include "segmentation/segmentation.h"
@@ -65,37 +64,6 @@ enum GLNames {
     Scalebar,
     NodeOffset
 };
-
-bool ViewportBase::initMesh(mesh & toInit, uint initialSize) {
-    toInit.vertices = (floatCoordinate *)malloc(initialSize * sizeof(floatCoordinate));
-    toInit.normals = (floatCoordinate *)malloc(initialSize * sizeof(floatCoordinate));
-    toInit.colors = (color4F *)malloc(initialSize * sizeof(color4F));
-
-    toInit.vertsBuffSize = initialSize;
-    toInit.normsBuffSize = initialSize;
-    toInit.colsBuffSize = initialSize;
-
-    toInit.vertsIndex = 0;
-    toInit.normsIndex = 0;
-    toInit.colsIndex = 0;
-    return true;
-}
-
-bool ViewportBase::doubleMeshCapacity(mesh & toDouble) {
-    return resizemeshCapacity(toDouble, 2);
-}
-
-bool ViewportBase::resizemeshCapacity(mesh & toResize, uint n) {
-    toResize.vertices = (floatCoordinate *)realloc(toResize.vertices, n * toResize.vertsBuffSize * sizeof(floatCoordinate));
-    toResize.normals = (floatCoordinate *)realloc(toResize.normals, n * toResize.normsBuffSize * sizeof(floatCoordinate));
-    toResize.colors = (color4F *)realloc(toResize.colors, n * toResize.colsBuffSize * sizeof(color4F));
-
-    toResize.vertsBuffSize = n * toResize.vertsBuffSize;
-    toResize.normsBuffSize = n * toResize.normsBuffSize;
-    toResize.colsBuffSize = n * toResize.colsBuffSize;
-
-    return true;
-}
 
 bool setRotationState(uint setTo) {
     if (setTo == ROTATIONSTATERESET){
@@ -173,24 +141,18 @@ bool setRotationState(uint setTo) {
     return true;
 }
 
-uint ViewportBase::renderCylinder(Coordinate *base, float baseRadius, Coordinate *top, float topRadius, color4F color, const RenderOptions & options) {
+uint ViewportBase::renderCylinder(Coordinate *base, float baseRadius, Coordinate *top, float topRadius, QColor color, const RenderOptions & options) {
+    decltype(state->viewerState->lineVertBuffer.colors)::value_type color4f = {static_cast<GLfloat>(color.redF()), static_cast<GLfloat>(color.greenF()), static_cast<GLfloat>(color.blueF()), static_cast<GLfloat>(color.alphaF())};
     if (options.enableSkeletonDownsampling &&
         (((screenPxXPerDataPx * baseRadius < 1.f) && (screenPxXPerDataPx * topRadius < 1.f) && (state->viewerState->cumDistRenderThres > 1.f)) || (state->viewerState->cumDistRenderThres > 19.f))) {
 
-        if(state->viewerState->lineVertBuffer.vertsBuffSize < state->viewerState->lineVertBuffer.vertsIndex + 2)
-            doubleMeshCapacity(state->viewerState->lineVertBuffer);
+        state->viewerState->lineVertBuffer.vertices.emplace_back(*base);
+        state->viewerState->lineVertBuffer.vertices.emplace_back(*top);
 
-        state->viewerState->lineVertBuffer.vertices[state->viewerState->lineVertBuffer.vertsIndex] = Coordinate{base->x, base->y, base->z};
-        state->viewerState->lineVertBuffer.vertices[state->viewerState->lineVertBuffer.vertsIndex + 1] = Coordinate{top->x, top->y, top->z};
-
-        state->viewerState->lineVertBuffer.colors[state->viewerState->lineVertBuffer.vertsIndex] = color;
-        state->viewerState->lineVertBuffer.colors[state->viewerState->lineVertBuffer.vertsIndex + 1] = color;
-
-        state->viewerState->lineVertBuffer.vertsIndex += 2;
-
+        state->viewerState->lineVertBuffer.colors.emplace_back(color4f);
+        state->viewerState->lineVertBuffer.colors.emplace_back(color4f);
     } else {
-        GLfloat a[] = {color.r, color.g, color.b, color.a};
-        glColor4fv(a);
+        glColor4fv(color4f.data());
 
         glPushMatrix();
         GLUquadricObj * gluCylObj = gluNewQuadric();
@@ -226,9 +188,10 @@ uint ViewportBase::renderCylinder(Coordinate *base, float baseRadius, Coordinate
     return true;
 }
 
-uint ViewportBase::renderSphere(const Coordinate & pos, const float & radius, const color4F & color, const RenderOptions & options) {
+uint ViewportBase::renderSphere(const Coordinate & pos, const float & radius, const QColor & color, const RenderOptions & options) {
     GLUquadricObj *gluSphereObj = NULL;
 
+    decltype(state->viewerState->lineVertBuffer.colors)::value_type color4f = {static_cast<GLfloat>(color.redF()), static_cast<GLfloat>(color.greenF()), static_cast<GLfloat>(color.blueF()), static_cast<GLfloat>(color.alphaF())};
     /* Render only a point if the sphere wouldn't be visible anyway */
     if (options.enableSkeletonDownsampling &&
         (((screenPxXPerDataPx * radius > 0.0f) && (screenPxXPerDataPx * radius < 2.0f)) || (state->viewerState->cumDistRenderThres > 19.f))) {
@@ -241,19 +204,13 @@ uint ViewportBase::renderSphere(const Coordinate & pos, const float & radius, co
                 glVertex3f((float)pos.x, (float)pos.y, (float)pos.z);
             glEnd();
             glPointSize(1.);
-        }
-        else {
-            if(state->viewerState->pointVertBuffer.vertsBuffSize < state->viewerState->pointVertBuffer.vertsIndex + 2)
-                doubleMeshCapacity(state->viewerState->pointVertBuffer);
-
-            state->viewerState->pointVertBuffer.vertices[state->viewerState->pointVertBuffer.vertsIndex] = Coordinate{pos.x, pos.y, pos.z};
-            state->viewerState->pointVertBuffer.colors[state->viewerState->pointVertBuffer.vertsIndex] = color;
-            state->viewerState->pointVertBuffer.vertsIndex++;
+        } else {
+            state->viewerState->pointVertBuffer.vertices.emplace_back(pos);
+            state->viewerState->pointVertBuffer.colors.emplace_back(color4f);
         }
     }
     else {
-        GLfloat tmp[] = {color.r, color.g, color.b, color.a};
-        glColor4fv(tmp);
+        glColor4fv(color4f.data());
         glPushMatrix();
         glTranslatef((float)pos.x, (float)pos.y, (float)pos.z);
         glScalef(1.f, 1.f, state->viewerState->voxelXYtoZRatio);
@@ -278,12 +235,12 @@ uint ViewportBase::renderSphere(const Coordinate & pos, const float & radius, co
     return true;
 }
 
-void ViewportBase::renderSegment(const segmentListElement & segment, const color4F & color, const RenderOptions & options) {
+void ViewportBase::renderSegment(const segmentListElement & segment, const QColor & color, const RenderOptions & options) {
     renderCylinder(&(segment.source.position), Skeletonizer::singleton().segmentSizeAt(segment.source) * state->viewerState->segRadiusToNodeRadius,
         &(segment.target.position), Skeletonizer::singleton().segmentSizeAt(segment.target) * state->viewerState->segRadiusToNodeRadius, color, options);
 }
 
-void ViewportOrtho::renderSegment(const segmentListElement & segment, const color4F & color, const RenderOptions & options) {
+void ViewportOrtho::renderSegment(const segmentListElement & segment, const QColor & color, const RenderOptions & options) {
     ViewportBase::renderSegment(segment, color, options);
     if (state->viewerState->showIntersections) {
         renderSegPlaneIntersection(segment);
@@ -307,13 +264,15 @@ void ViewportBase::renderNode(const nodeListElement & node, const RenderOptions 
     renderSphere(node.position, radius, color, options);
 
     if(node.selected && options.highlightSelection) { // highlight selected nodes
-        renderSphere(node.position, radius * mp, {0.f, 1.f, 0.f, 0.5f});
+        auto selectedNodeColor = QColor(Qt::green);
+        selectedNodeColor.setAlphaF(0.5f);
+        renderSphere(node.position, radius * mp, selectedNodeColor);
     }
     // Highlight active node with a halo
     if(state->skeletonState->activeNode == &node && options.highlightActiveNode) {
         // Color gets changes in case there is a comment & conditional comment highlighting
-        color4F haloColor = state->viewer->getNodeColor(node);
-        haloColor.a = 0.2f;
+        auto haloColor = state->viewer->getNodeColor(node);
+        haloColor.setAlphaF(0.2);
         renderSphere(node.position, radius * ((mp - 1.0)/2.0 + 1.0), haloColor);
     }
 }
@@ -321,8 +280,7 @@ void ViewportBase::renderNode(const nodeListElement & node, const RenderOptions 
 void ViewportOrtho::renderNode(const nodeListElement & node, const RenderOptions & options) {
     ViewportBase::renderNode(node, options);
     if(1.5 <  Skeletonizer::singleton().radius(node)) { // draw node center to make large nodes visible and clickable in ortho vps
-        color4F color = state->viewer->getNodeColor(node);
-        renderSphere(node.position, 1.5, color);
+        renderSphere(node.position, 1.5, state->viewer->getNodeColor(node));
     }
     // Render the node description
     glColor4f(0.f, 0.f, 0.f, 1.f);
@@ -2154,14 +2112,8 @@ bool Viewport3D::rotateViewport() {
  */
 
 void ViewportBase::renderSkeleton(const RenderOptions &options) {
-    state->viewerState->lineVertBuffer.vertsIndex = 0;
-    state->viewerState->lineVertBuffer.normsIndex = 0;
-    state->viewerState->lineVertBuffer.colsIndex = 0;
-
-    state->viewerState->pointVertBuffer.vertsIndex = 0;
-    state->viewerState->pointVertBuffer.normsIndex = 0;
-    state->viewerState->pointVertBuffer.colsIndex = 0;
-    color4F currentColor = {1.f, 0.f, 0.f, 1.f};
+    state->viewerState->lineVertBuffer.vertices.clear();
+    state->viewerState->lineVertBuffer.colors.clear();
 
     //tdItem: test culling under different conditions!
     //if(viewportType == VIEWPORT_SKELETON) glEnable(GL_CULL_FACE);
@@ -2253,12 +2205,10 @@ void ViewportBase::renderSkeleton(const RenderOptions &options) {
 
             if (nodeVisible) {
                 //This sets the current color for the segment rendering
+                QColor currentColor = currentTree.color;
                 if((currentTree.treeID == state->skeletonState->activeTree->treeID)
                     && (state->viewerState->highlightActiveTree)) {
-                        currentColor = {1.f, 0.f, 0.f, 1.f};
-                }
-                else {
-                    currentColor = currentTree.color;
+                    currentColor = Qt::red;
                 }
 
                 cumDistToLastRenderedNode = 0.f;
@@ -2286,8 +2236,7 @@ void ViewportBase::renderSkeleton(const RenderOptions &options) {
                                 || synapse.preSynapse == state->skeletonState->activeNode ) {
 
                             segmentListElement virtualSegment(*synapse.postSynapse, *synapse.preSynapse);
-                            color4F col(0.0, 0.0, 0.0, 1.0);
-                            renderSegment(virtualSegment, col, options);
+                            renderSegment(virtualSegment, Qt::black, options);
 
                             auto post = synapse.postSynapse->position;
                             auto pre = synapse.preSynapse->position;
@@ -2296,9 +2245,9 @@ void ViewportBase::renderSkeleton(const RenderOptions &options) {
                                                   post.y - (post.y - pre.y)/10,
                                                   post.z - (post.z - pre.z)/10};
 
-                            renderCylinder(&arrowbase, Skeletonizer::singleton().segmentSizeAt(*synapse.preSynapse) * 3.0f,
-                                &(synapse.postSynapse->position),
-                                Skeletonizer::singleton().segmentSizeAt(*synapse.postSynapse) * state->viewerState->segRadiusToNodeRadius, col, options);
+                            renderCylinder(&arrowbase, Skeletonizer::singleton().segmentSizeAt(*synapse.preSynapse) * 3.0f
+                                , &synapse.postSynapse->position
+                                , Skeletonizer::singleton().segmentSizeAt(*synapse.postSynapse) * state->viewerState->segRadiusToNodeRadius, Qt::black, options);
                         }
                     }
                 }
@@ -2319,15 +2268,15 @@ void ViewportBase::renderSkeleton(const RenderOptions &options) {
     }
 
     /* Render line geometry batch if it contains data */
-    if(state->viewerState->lineVertBuffer.vertsIndex > 0) {
+    if (!state->viewerState->lineVertBuffer.vertices.empty()) {
         glEnableClientState(GL_VERTEX_ARRAY);
         glEnableClientState(GL_COLOR_ARRAY);
 
         /* draw all segments */
-        glVertexPointer(3, GL_FLOAT, 0, state->viewerState->lineVertBuffer.vertices);
-        glColorPointer(4, GL_FLOAT, 0, state->viewerState->lineVertBuffer.colors);
+        glVertexPointer(3, GL_FLOAT, 0, state->viewerState->lineVertBuffer.vertices.data());
+        glColorPointer(4, GL_FLOAT, 0, state->viewerState->lineVertBuffer.colors.data());
 
-        glDrawArrays(GL_LINES, 0, state->viewerState->lineVertBuffer.vertsIndex);
+        glDrawArrays(GL_LINES, 0, state->viewerState->lineVertBuffer.vertices.size());
 
         glDisableClientState(GL_COLOR_ARRAY);
         glDisableClientState(GL_VERTEX_ARRAY);
@@ -2339,15 +2288,15 @@ void ViewportBase::renderSkeleton(const RenderOptions &options) {
         glPointSize(3.f);
 
     /* Render point geometry batch if it contains data */
-    if(state->viewerState->pointVertBuffer.vertsIndex > 0) {
+    if (!state->viewerState->pointVertBuffer.vertices.empty()) {
         glEnableClientState(GL_VERTEX_ARRAY);
         glEnableClientState(GL_COLOR_ARRAY);
 
         /* draw all segments */
-        glVertexPointer(3, GL_FLOAT, 0, state->viewerState->pointVertBuffer.vertices);
-        glColorPointer(4, GL_FLOAT, 0, state->viewerState->pointVertBuffer.colors);
+        glVertexPointer(3, GL_FLOAT, 0, state->viewerState->pointVertBuffer.vertices.data());
+        glColorPointer(4, GL_FLOAT, 0, state->viewerState->pointVertBuffer.colors.data());
 
-        glDrawArrays(GL_POINTS, 0, state->viewerState->pointVertBuffer.vertsIndex);
+        glDrawArrays(GL_POINTS, 0, state->viewerState->pointVertBuffer.vertices.size());
 
         glDisableClientState(GL_COLOR_ARRAY);
         glDisableClientState(GL_VERTEX_ARRAY);

color4F.cpp
@@ -1,10 +1,0 @@
-#include "color4F.h"
-
-color4F::color4F() {}
-
-color4F::color4F(float r, float g, float b, float a) {
-    this->r = r;
-    this->g = g;
-    this->b = b;
-    this->a = a;
-}

color4F.h
@@ -1,11 +1,0 @@
-#ifndef COLOR4F_H
-#define COLOR4F_H
-
-class color4F {
-public:
-    color4F();
-    color4F(float r, float g, float b, float a);
-    float r, g, b, a;
-};
-
-#endif//COLOR4F_H

commentsetting.cpp
@@ -14,7 +14,7 @@ QColor CommentSetting::getColor(const QString comment) {
             return item.color;
         }
     }
-    return QColor(255, 255, 0, 255);
+    return Qt::yellow;
 }
 
 float CommentSetting::getRadius(const QString comment) {

mesh.h
@@ -1,27 +1,0 @@
-#ifndef MESH_H
-#define MESH_H
-
-#include "color4F.h"
-#include "coordinate.h"
-
-class mesh {
-public:
-    mesh() {}
-    mesh(int mode) { this->mode = mode; } /* GL_POINTS, GL_TRIANGLES, etc. */
-    floatCoordinate *vertices;
-    floatCoordinate *normals;
-    color4F *colors;
-
-    /* useful for flexible mesh manipulations */
-    uint vertsBuffSize;
-    uint normsBuffSize;
-    uint colsBuffSize;
-    /* indicates last used element in corresponding buffer */
-    uint vertsIndex;
-    uint normsIndex;
-    uint colsIndex;
-    uint mode;
-    uint size;
-};
-
-#endif//MESH_H

scriptengine/decorators/colordecorator.cpp
@@ -1,64 +1,0 @@
-#include "colordecorator.h"
-
-#include "color4F.h"
-
-ColorDecorator::ColorDecorator(QObject *parent) : QObject(parent) {}
-
-
-color4F *ColorDecorator::new_color4F() {
-    return new color4F();
-}
-
-color4F *ColorDecorator::new_color4F(float r, float g, float b, float a) {
-    return new color4F(r, g, b, a);
-}
-
-float ColorDecorator::red(color4F *self) {
-    return self->r;
-}
-
-float ColorDecorator::green(color4F *self) {
-    return self->g;
-}
-
-float ColorDecorator::blue(color4F *self) {
-    return self->b;
-}
-
-float ColorDecorator::alpha(color4F *self) {
-    return self->a;
-}
-
-QString ColorDecorator::static_color4F_help() {
-    return QString("An instanteable class which stores rgba channels in float coordinates in range [0..1]. Access to attributes only via getter and setter." \
-                   "\n\n CONSTRUCTORS: " \
-                   "\n Color() : creates an empty color object." \
-                   "\n Color(red, green, blue, alpha) : creates a color object by specifying all color channels. Excepts whether float values in range [0..1]"
-                   "\n\n GETTER: " \
-                   "\n red() : returns the red channel of the color" \
-                   "\n green() : returns the green channel of the color" \
-                   "\n blue() : returns the blue channel of the color" \
-                   "\n alpha() : returns the alpha channel of the color" \
-                   "\n\n SETTER: " \
-                   "\n set_red(red) : sets the red channel of the color. Expects a float value in range [0..1]" \
-                   "\n set_green(green) : sets the green channel of the color. Expects a float value in range [0..1] " \
-                   "\n set_blue(blue) : sets the blue channel of the color. Expects a float value in range [0..1]" \
-                   "\n set_alpha(alpha) : sets the alpha channel of the color. Expects a floats value in range [0..1] ");
-}
-
-
-void ColorDecorator::set_red(color4F *self, float red) {
-    self->r = red;
-}
-
-void ColorDecorator::set_green(color4F *self, float green) {
-    self->g = green;
-}
-
-void ColorDecorator::set_blue(color4F *self, float blue) {
-    self->r = blue;
-}
-
-void ColorDecorator::set_alpha(color4F *self, float alpha) {
-    self->a = alpha;
-}

scriptengine/decorators/colordecorator.h
@@ -1,29 +1,0 @@
-#ifndef COLOR_DECORATOR_H
-#define COLOR_DECORATOR_H
-
-#include <QObject>
-
-class color4F;
-class ColorDecorator : public QObject {
-    Q_OBJECT
-public:
-    explicit ColorDecorator(QObject *parent = 0);
-
-public slots:
-    color4F *new_color4F();
-    color4F *new_color4F(float r, float g, float b, float a = 1.0);
-
-    float red(color4F *self);
-    float blue(color4F *self);
-    float green(color4F *self);
-    float alpha(color4F *self);
-
-    void set_red(color4F *self, float red);
-    void set_green(color4F *self, float green);
-    void set_blue(color4F *self, float blue);
-    void set_alpha(color4F *self, float alpha);
-
-    QString static_color4F_help();
-};
-
-#endif//COLOR_DECORATOR_H

scriptengine/decorators/treelistdecorator.cpp
@@ -16,7 +16,7 @@ QString TreeListDecorator::comment(treeListElement *self) {
     return self->comment;
 }
 
-color4F TreeListDecorator::color(treeListElement *self) {
+QColor TreeListDecorator::color(treeListElement *self) {
     return self->color;
 }
 

commit 257a3a0d69a6b21c2ee8bb1cf07e6be0b4af4d72

point vertices were not cleared (regression from 02cf692d5b13eb5cf05f9f3fc5990c54b8b554e4)
fix #340

widgets/renderer.cpp
@@ -2104,6 +2104,8 @@ bool Viewport3D::rotateViewport() {
 void ViewportBase::renderSkeleton(const RenderOptions &options) {
     state->viewerState->lineVertBuffer.vertices.clear();
     state->viewerState->lineVertBuffer.colors.clear();
+    state->viewerState->pointVertBuffer.vertices.clear();
+    state->viewerState->pointVertBuffer.colors.clear();
 
     //tdItem: test culling under different conditions!
     //if(viewportType == VIEWPORT_SKELETON) glEnable(GL_CULL_FACE);