7a85319a2edd5497cbbcd6773c2897ddaaa942f7
al3xst <al3xst@users.noreply.github.com>
2017-01-03 17:02:18 +0100

    Added missing Extension
    Also explained signing process for WIX bundle

--- installer/create-installer.sh
+++ installer/create-installer.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 candle knossos.wxs && light knossos.wixobj
-candle bundle.wxs -ext WixBalExtension && light bundle.wixobj -ext WixBalExtension
+candle bundle.wxs -ext WixBalExtension -ext WixUtilExtension && light bundle.wixobj -ext WixBalExtension
+
+#signing process:
+#insignia -ib bundle.exe -o engine.exe
+# ... sign engine.exe with e.g. signtool engine.exe
+#insignia -ab engine.exe bundle.exe -o bundle.exe
+# ... sign bundle.exe with e.g. signtool bundle.exe
