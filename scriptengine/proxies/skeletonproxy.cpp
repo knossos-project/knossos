76ecc61fe67d168d0ba3cc53c8d2271af4db462f
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2017-01-13 17:56:57 +0100

    (un)signed comparison warning

--- scriptengine/proxies/skeletonproxy.cpp
+++ scriptengine/proxies/skeletonproxy.cpp
@@ -98,7 +98,7 @@ void SkeletonProxy::add_tree_mesh(quint64 tree_id, QVector<float> & verts, QVect
         throw std::runtime_error(QObject::tr("SkeletonProxy::add_tree_mesh failed: number of color components not 0 or 4 × vertices (%1), got %2 color components.").arg(correctColorSize).arg(color.size()).toStdString());
     }
     QVector<std::uint8_t> uintColors(color.size());
-    for (std::size_t i = 0 ; i < color.size(); ++i) {
+    for (int i = 0 ; i < color.size(); ++i) {
         uintColors[i] = color[i] * 255;
     }
     Skeletonizer::singleton().addMeshToTree(tree_id, verts, normals, indices, uintColors, draw_mode, swap_xy);
