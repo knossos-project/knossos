1cfec7c2a479c3328b2d78495d56033dc52f0cfe
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2017-01-19 02:30:04 +0100

    remove submodules, they are annoying

--- installer/aur/aur4 ssh git.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#just let something like smartgit handle the ssh authentication
-#ssh-agent bash -c 'ssh-add id_rsa; git clone ssh://aur@aur4.archlinux.org/knossos.git'
-ssh-agent bash -c 'ssh-add ../id_rsa; git push'
