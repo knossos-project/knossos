52212574c01db3ea73907ed8fa2819c291d5884d
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2017-03-18 02:52:06 +0100

    update installer

--- installer/wix/create-installer.sh
+++ installer/wix/create-installer.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+export PATH="/c/Program Files (x86)/WiX Toolset v3.11/bin":$PATH$
 candle knossos.wxs && light knossos.wixobj
 candle bundle.wxs -ext WixBalExtension -ext WixUtilExtension && light bundle.wixobj -ext WixBalExtension
-
-#signing process:
-#insignia -ib bundle.exe -o engine.exe
-# ... sign engine.exe with e.g. signtool engine.exe
-#insignia -ab engine.exe bundle.exe -o bundle.exe
-# ... sign bundle.exe with e.g. signtool bundle.exe
+insignia.exe -ib bundle.exe -o engine.exe
+read -n1 -r -p "sign engine.exe  then press a key"
+insignia -ab engine.exe bundle.exe -o bundle.exe
+read -n1 -r -p "sign bundle.exe  then press a key"
