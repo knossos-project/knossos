e43aad30a178320910e535a5fd44fd989c061d45
n0void <mich.pron@gmail.com>
2017-06-02 16:50:35 +0200

    added most layer management gui elements

--- widgets/layerdialog.cpp
+++ widgets/layerdialog.cpp
@@ -19,19 +19,50 @@ QVariant LayerItemModel::headerData(int section, Qt::Orientation orientation, in
 }
 
 QVariant LayerItemModel::data(const QModelIndex &index, int role) const {
-    if (role == Qt::DisplayRole || role == Qt::EditRole) {
-        return QString("[empty]");
+    if(index.isValid()) {
+        if (role == Qt::DisplayRole || role == Qt::EditRole) {
+            switch(index.column()) {
+            case 1: return QString("0%");
+            case 2: return QString("unnamed");
+            case 3: return QString("untyped");
+            }
+        } else if(role == Qt::CheckStateRole) {
+            if(index.column() == 0) {
+                return Qt::Checked;
+            }
+        }
     }
     return QVariant();
+}
+
+Qt::ItemFlags LayerItemModel::flags(const QModelIndex &index) const {
+    if(index.isValid()) {
+        Qt::ItemFlags flags = Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsDragEnabled | Qt::ItemIsDropEnabled | Qt::ItemIsEditable;
+        switch(index.column()) {
+        case 0:
+            flags |= Qt::ItemIsUserCheckable;
+            break;
+        }
+        return flags;
+    }
+    return 0;
 }
 
 
 LayerDialogWidget::LayerDialogWidget(QWidget *parent) : DialogVisibilityNotify(PREFERENCES_WIDGET, parent) {
     setWindowTitle("Layers");
 
+    int row = 0;
+    optionsLayout.addWidget(&opacitySliderLabel, row, 0);
+    optionsLayout.addWidget(&opacitySlider, row, 1);
+    optionsLayout.addWidget(&linearFilteringCheckBox, row, 2);
+    optionsLayout.addWidget(&rangeDeltaSliderLabel, ++row, 0);
+    optionsLayout.addWidget(&rangeDeltaSlider, row, 1);
+    optionsLayout.addWidget(&biasSliderLabel, ++row, 0);
+    optionsLayout.addWidget(&biasSlider, row, 1);
+    optionsSpoiler.setContentLayout(optionsLayout);
+
     treeView.setModel(&itemModel);
-    treeView.setDragEnabled(true);
-    treeView.setAcceptDrops(true);
     treeView.resizeColumnToContents(0);
     treeView.resizeColumnToContents(1);
     treeView.resizeColumnToContents(2);
@@ -42,7 +73,16 @@ LayerDialogWidget::LayerDialogWidget(QWidget *parent) : DialogVisibilityNotify(P
     treeView.setUniformRowHeights(true); // for optimization
     treeView.setDragDropMode(QAbstractItemView::InternalMove);
 
+    controlButtonLayout.addWidget(&infoTextLabel);
+    controlButtonLayout.addWidget(&moveUpButton);
+    controlButtonLayout.addWidget(&moveDownButton);
+    controlButtonLayout.addWidget(&addLayerButton);
+    controlButtonLayout.addWidget(&removeLayerButton);
+
+    mainLayout.addWidget(&optionsSpoiler);
     mainLayout.addWidget(&treeView);
+    mainLayout.addLayout(&controlButtonLayout);
+
     setLayout(&mainLayout);
 
     resize(700, 600);
--- widgets/layerdialog.h
+++ widgets/layerdialog.h
@@ -2,20 +2,29 @@
 #define LAYERDIALOG_H
 
 #include "widgets/DialogVisibilityNotify.h"
+#include "widgets/Spoiler.h"
 
+#include <QGroupBox>
+#include <QToolButton>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+#include <QSlider>
+#include <QCheckBox>
 #include <QTreeView>
 #include <QAbstractItemModel>
 
 class LayerItemModel : public QAbstractListModel {
 Q_OBJECT
 protected:
-    const std::vector<QString> header{"h1", "h2", "h3"};
+    const std::vector<QString> header{"visible", "opacity", "name", "type"};
 public:
     virtual int rowCount(const QModelIndex & parent = QModelIndex()) const override;
     virtual int columnCount(const QModelIndex & parent = QModelIndex()) const override;
     virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
     virtual QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const override;
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const override;
 };
 
 class LayerDialogWidget : public DialogVisibilityNotify {
@@ -24,8 +33,27 @@ public:
     explicit LayerDialogWidget(QWidget * parent = 0);
 
     QVBoxLayout mainLayout;
+
+    Spoiler optionsSpoiler{"layer options"};
+    QGridLayout optionsLayout;
+    QLabel opacitySliderLabel{"opacity"};
+    QSlider opacitySlider{Qt::Horizontal};
+    QLabel rangeDeltaSliderLabel{"range delta"};
+    QSlider rangeDeltaSlider{Qt::Horizontal};
+    QLabel biasSliderLabel{"bias"};
+    QSlider biasSlider{Qt::Horizontal};
+    QCheckBox linearFilteringCheckBox{"linear filtering"};
+
+    QVBoxLayout layerLayout;
     LayerItemModel itemModel;
     QTreeView treeView;
+
+    QHBoxLayout controlButtonLayout;
+    QLabel infoTextLabel{"Hint: use layers to have layers."};
+    QToolButton moveUpButton;
+    QToolButton moveDownButton;
+    QToolButton addLayerButton;
+    QToolButton removeLayerButton;
 };
 
 #endif // LAYERDIALOG_H
