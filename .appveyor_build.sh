7b4b205c0f62f857137a70916ddb7acfd829742e
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2017-07-19 19:54:24 +0200

    update appveyor script a bit
    don’t reinstall already installed packages (--needed)
    install PythonQt as package (patch Qt for builds of PythonQt with current Qt)
    get PROJECTPATH from APPVEYOR_BUILD_FOLDER
    Qt5Gui plugin workaround was added to CMakeLists.txt in 4c1526b5cf6b40ef4d97a18dc27c1e5fa77a072d
    use sed regex to add nightly suffix
    use BRANCH_PREFIX variable to reduce code duplication
    extract bitness from MINGW_PREFIX
    
    only set MSYSTEM
    disable releasing only on tags (was overwritten anyway)
    update appveyor script a bit

--- .appveyor_build.sh
+++ .appveyor_build.sh
@@ -1,57 +1,32 @@
 #!/bin/bash
 
-pacman -Syu --noconfirm
-pacman --sync --noconfirm mingw-w64-x86_64-toolchain # msys64-only
-pacman -S mingw-w64-x86_64-boost --noconfirm
-pacman -S mingw-w64-x86_64-python2 --noconfirm
-pacman -S mingw-w64-x86_64-qt5-static --noconfirm
-pacman -S mingw-w64-x86_64-ninja --noconfirm
-pacman -S mingw-w64-x86_64-snappy --noconfirm
-pacman -S mingw-w64-x86_64-jasper --noconfirm #jpeg-2000
-pacman -S mingw-w64-x86_64-cmake --noconfirm #cmake
+pacman -Syuu --noconfirm
+pacman -S --needed --noconfirm $MINGW_PACKAGE_PREFIX-{boost,cmake,jasper,ninja,python2,qt5-static,snappy,toolchain}
 
-####
-# Download and unpack PythonQt
-####
-curl -L https://al3xst.de/stuff/pythonqt.tar.xz > pythonqt.tar.xz
-tar xvf pythonqt.tar.xz
+# Download and install static PythonQt and QuaZIP
+curl -L https://al3xst.de/stuff/$MINGW_PACKAGE_PREFIX-pythonqt-static.pkg.tar.xz > pythonqt.pkg.tar.xz
+curl -L https://al3xst.de/stuff/$MINGW_PACKAGE_PREFIX-quazip-static.pkg.tar.xz > quazip.pkg.tar.xz
+pacman -U --noconfirm pythonqt.pkg.tar.xz quazip.pkg.tar.xz
 
-####
-### apply *** TEMP workaround *** for PythonQt
-####
-PROJECTPATH=`ls /c/projects/`
-echo 'get_target_property(gui_static_plugins Qt5::Gui STATIC_PLUGINS)' >> /c/projects/${PROJECTPATH}/CMakeLists.txt
-echo 'if(gui_static_plugins)' >> /c/projects/${PROJECTPATH}/CMakeLists.txt
-echo 'string(REGEX REPLACE [;]*QVirtualKeyboardPlugin[;]* ";" gui_static_plugins_new "${gui_static_plugins}")' >> /c/projects/${PROJECTPATH}/CMakeLists.txt
-echo 'set_target_properties(Qt5::Gui PROPERTIES STATIC_PLUGINS "${gui_static_plugins_new}")' >> /c/projects/${PROJECTPATH}/CMakeLists.txt
-echo 'endif()' >> /c/projects/${PROJECTPATH}/CMakeLists.txt
+PROJECTPATH=$(cygpath ${APPVEYOR_BUILD_FOLDER})
 
-###
-# append "-nightly" suffix in version file
-###
-KVER=`cat /c/projects/${PROJECTPATH}/version.h | grep KVERSION | cut -d'"' -f2` #get version name aka 5.0
-sed -i "s/KVERSION \""${KVER}"\"/KVERSION \"${KVER}-nightly\"/g" /c/projects/${PROJECTPATH}/version.h #append -nightly, so version is 5.0-nightly
+# patch Qt for PythonQt
+cd /
+patch -Np0 -i $PROJECTPATH/qml_static_plugins.patch
+cd -
 
-####
-# Download and install Quazip-Static
-####
-curl -L https://al3xst.de/stuff/mingw-w64-x86_64-quazip-static-0.7-1-any.pkg.tar.xz > quazip-static.tar.xz
-pacman -U quazip-static.tar.xz --noconfirm
+# Append "-nightly" suffix in version file
+sed -i -E "/KVERSION/s/(([0-9]\.?)+)/\1-nightly/g" $PROJECTPATH/version.h
 
-####
-# Run cmake
-####
 mkdir knossos-build
 cd knossos-build
-cmake -G Ninja -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_PREFIX_PATH="/c/projects/${PROJECTPATH}/mingw64/qt5-static/" /c/projects/${PROJECTPATH}
+cmake -G Ninja -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=RELEASE $PROJECTPATH
 
-####
-# build knossos
-####
+# Build
 ninja -j2
 
-if [ $APPVEYOR_REPO_BRANCH = "master" ]; then
-	cp knossos.exe /c/projects/${PROJECTPATH}/win64.standalone.KNOSSOS.nightly.exe
-else
-	cp knossos.exe /c/projects/${PROJECTPATH}/win64.standalone.KNOSSOS.${APPVEYOR_REPO_BRANCH}-nightly.exe
+# Deploy
+if [[ $APPVEYOR_REPO_BRANCH != "master" ]]; then
+	BRANCH_PREFIX=${APPVEYOR_REPO_BRANCH}-
 fi
+cp knossos.exe $PROJECTPATH/win${MINGW_PREFIX: -2}.standalone.KNOSSOS.${BRANCH_PREFIX}nightly.exe
