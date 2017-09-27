knossos-project/knossos

commit 5985f28c1bbd579de7bb1f4025875db66a06ec7f

Bucket fill: respect vp and movement boundaries (fix #412).

coordinate.h
@@ -155,6 +155,14 @@ public:
         };
     }
 
+    static constexpr CoordinateDerived min(const CoordinateDerived & c1, const CoordinateDerived & c2) {
+        return CoordinateDerived{std::min(c1.x, c2.x), std::min(c1.y, c2.y), std::min(c1.z, c2.z)};
+    }
+
+    static constexpr CoordinateDerived max(const CoordinateDerived & c1, const CoordinateDerived & c2) {
+        return CoordinateDerived{std::max(c1.x, c2.x), std::max(c1.y, c2.y), std::max(c1.z, c2.z)};
+    }
+
     constexpr CoordinateDerived toWorldFrom(const CoordinateDerived & v1, const CoordinateDerived & v2, const CoordinateDerived & n) const {
         return x * v1 + y * v2 + z * n;
     }

segmentation/segmentationsplit.cpp
@@ -29,7 +29,7 @@
 
 #include <unordered_set>
 
-void subobjectBucketFill(const Coordinate & seed, const Coordinate & center, const uint64_t fillsoid, const brush_t & brush) {
+void subobjectBucketFill(const Coordinate & seed, const Coordinate & center, const uint64_t fillsoid, const brush_t & brush, const Coordinate & areaMin, const Coordinate & areaMax) {
     std::vector<Coordinate> work = {seed};
     std::unordered_set<Coordinate> visitedVoxels;
 
@@ -49,17 +49,20 @@ void subobjectBucketFill(const Coordinate & seed, const Coordinate & center, con
                 }
             };
 
+            Coordinate posDec = Coordinate::max(pos - 1, areaMin);
+            Coordinate posInc = Coordinate::min(pos + 1, areaMax);
+
             if (brush.view != brush_t::view_t::zy || brush.mode == brush_t::mode_t::three_dim) {
-                walk({pos.x + 1, pos.y, pos.z});
-                walk({pos.x - 1, pos.y, pos.z});
+                walk({posInc.x, pos.y, pos.z});
+                walk({posDec.x, pos.y, pos.z});
             }
             if (brush.view != brush_t::view_t::xz || brush.mode == brush_t::mode_t::three_dim) {
-                walk({pos.x, pos.y + 1, pos.z});
-                walk({pos.x, pos.y - 1, pos.z});
+                walk({pos.x, posInc.y, pos.z});
+                walk({pos.x, posDec.y, pos.z});
             }
             if (brush.view != brush_t::view_t::xy || brush.mode == brush_t::mode_t::three_dim) {
-                walk({pos.x, pos.y, pos.z + 1});
-                walk({pos.x, pos.y, pos.z - 1});
+                walk({pos.x, pos.y, posInc.z});
+                walk({pos.x, pos.y, posDec.z});
             }
         }
     }

widgets/viewports/viewportevents.cpp
@@ -429,7 +429,15 @@ void ViewportOrtho::handleMouseReleaseMiddle(const QMouseEvent *event) {
             uint64_t soid = brush_copy.inverse ? seg.getBackgroundId() : seg.subobjectIdOfFirstSelectedObject(clickedCoordinate);
             brush_copy.shape = brush_t::shape_t::angular;
             brush_copy.radius = displayedlengthInNmX / 2;//set brush to fill visible area
-            subobjectBucketFill(clickedCoordinate, state->viewerState->currentPosition, soid, brush_copy);
+
+            auto halfWidth = width()/2 * 1/screenPxXPerDataPx;
+            auto halfHeight = height()/2 * 1/screenPxYPerDataPx;
+            Coordinate areaMin = state->viewerState->currentPosition - v1.abs() * halfWidth - v2.abs() * halfHeight;
+            Coordinate areaMax = state->viewerState->currentPosition + v1.abs() * halfWidth + v2.abs() * halfHeight;
+            areaMin = Coordinate::max(areaMin, Session::singleton().movementAreaMin);
+            areaMax = Coordinate::min(areaMax, Session::singleton().movementAreaMax);
+
+            subobjectBucketFill(clickedCoordinate, state->viewerState->currentPosition, soid, brush_copy, areaMin, areaMax);
         }
     }
     //finish node drag

segmentation/segmentationsplit.h
@@ -101,7 +101,7 @@ signals:
     void shapeChanged(const shape_t);
 };
 
-void subobjectBucketFill(const Coordinate & seed, const Coordinate & center, const uint64_t fillsoid, const brush_t & brush);
+void subobjectBucketFill(const Coordinate & seed, const Coordinate & center, const uint64_t fillsoid, const brush_t & brush, const Coordinate & areaMin, const Coordinate & areaMax);
 void connectedComponent(const Coordinate & seed);
 void verticalSplittingPlane(const Coordinate & seed);
 

commit 875cf4d536382ea21eb426613f291564a76b3be0

3d bucket fill was only 2d (regression from 5985f28c1bbd579de7bb1f4025875db66a06ec7f)
remove `min` and `max` Coordinate functions, use `capped` instead

coordinate.h
@@ -155,14 +155,6 @@ public:
         };
     }
 
-    static constexpr CoordinateDerived min(const CoordinateDerived & c1, const CoordinateDerived & c2) {
-        return CoordinateDerived{std::min(c1.x, c2.x), std::min(c1.y, c2.y), std::min(c1.z, c2.z)};
-    }
-
-    static constexpr CoordinateDerived max(const CoordinateDerived & c1, const CoordinateDerived & c2) {
-        return CoordinateDerived{std::max(c1.x, c2.x), std::max(c1.y, c2.y), std::max(c1.z, c2.z)};
-    }
-
     constexpr CoordinateDerived toWorldFrom(const CoordinateDerived & v1, const CoordinateDerived & v2, const CoordinateDerived & n) const {
         return x * v1 + y * v2 + z * n;
     }

segmentation/segmentationsplit.cpp
@@ -49,8 +49,8 @@ void subobjectBucketFill(const Coordinate & seed, const Coordinate & center, con
                 }
             };
 
-            Coordinate posDec = Coordinate::max(pos - 1, areaMin);
-            Coordinate posInc = Coordinate::min(pos + 1, areaMax);
+            const auto posDec = (pos - 1).capped(areaMin, areaMax);
+            const auto posInc = (pos + 1).capped(areaMin, areaMax);
 
             if (brush.view != brush_t::view_t::zy || brush.mode == brush_t::mode_t::three_dim) {
                 walk({posInc.x, pos.y, pos.z});

widgets/viewports/viewportevents.cpp
@@ -431,12 +431,12 @@ void ViewportOrtho::handleMouseReleaseMiddle(const QMouseEvent *event) {
             brush_copy.shape = brush_t::shape_t::angular;
             brush_copy.radius = displayedlengthInNmX / 2;//set brush to fill visible area
 
-            auto halfWidth = width()/2 * 1/screenPxXPerDataPx;
-            auto halfHeight = height()/2 * 1/screenPxYPerDataPx;
-            Coordinate areaMin = state->viewerState->currentPosition - v1.abs() * halfWidth - v2.abs() * halfHeight;
-            Coordinate areaMax = state->viewerState->currentPosition + v1.abs() * halfWidth + v2.abs() * halfHeight;
-            areaMin = Coordinate::max(areaMin, Session::singleton().movementAreaMin);
-            areaMax = Coordinate::min(areaMax, Session::singleton().movementAreaMax);
+            const floatCoordinate displayedDataPx{floatCoordinate{displayedlengthInNmX, displayedlengthInNmX, displayedlengthInNmX} / Dataset::current().scale};
+            auto areaMin = state->viewerState->currentPosition - displayedDataPx / 2;
+            auto areaMax = state->viewerState->currentPosition + displayedDataPx / 2;
+
+            areaMin = areaMin.capped(Session::singleton().movementAreaMin, Session::singleton().movementAreaMax);
+            areaMax = areaMax.capped(Session::singleton().movementAreaMin, Session::singleton().movementAreaMax);
 
             subobjectBucketFill(clickedCoordinate, state->viewerState->currentPosition, soid, brush_copy, areaMin, areaMax);
         }