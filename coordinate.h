knossos-project/knossos

commit 59623d1f242ecb33275293613a20879b61e9cdf0

Generalized brush region calculation for arb vp.

segmentation/cubeloader.cpp
@@ -53,25 +53,25 @@ bool isInsideSphere(const double xi, const double yi, const double zi, const dou
     return sqdistance < radius * radius;
 }
 
-std::pair<Coordinate, Coordinate> getRegion(const Coordinate & centerPos, const brush_t & brush) {
-    const auto xsize = brush.radius / state->scale.x;
-    const auto ysize = brush.radius / state->scale.y;
-    const auto zsize = brush.radius / state->scale.z;
-    const auto brushExtents = Coordinate(xsize, ysize, zsize);
-    auto globalFirst = (centerPos - brushExtents).capped(Session::singleton().movementAreaMin, Session::singleton().movementAreaMax);
-    auto globalLast = (centerPos + brushExtents).capped(Session::singleton().movementAreaMin, Session::singleton().movementAreaMax);
-
-    if (brush.mode == brush_t::mode_t::two_dim) {//disable depth
-        if (brush.view == brush_t::view_t::xy) {
-            globalFirst.z = globalLast.z = centerPos.z;
-        } else if(brush.view == brush_t::view_t::xz) {
-            globalFirst.y = globalLast.y = centerPos.y;
-        } else if(brush.view == brush_t::view_t::zy) {
-            globalFirst.x = globalLast.x = centerPos.x;
-        }
+std::pair<Coordinate, Coordinate> getRegion(const floatCoordinate & centerPos, const brush_t & brush) { // calcs global AABB of local coordinate system's region
+    const auto posArb = centerPos.toLocal(brush.v1, brush.v2, brush.n);
+    const auto width = brush.radius / state->scale.componentMul(brush.v1).length();
+    const auto height = brush.radius / state->scale.componentMul(brush.v2).length();
+    const auto depth = (brush.mode == brush_t::mode_t::three_dim) ? brush.radius / state->scale.componentMul(brush.n).length() : 0;
+    std::vector<floatCoordinate> localPoints(8);
+    for (std::size_t i = 0; i < localPoints.size(); ++i) { // all possible combinations. Ordering like in a truth table (with - and + instead of false and true). I just didn't want to type it all out…
+        localPoints[i].x = (i < 4) ? posArb.x - width : posArb.x + width;
+        localPoints[i].y = (i < 8 && (i < 2 || i > 3)) ? posArb.y - height : posArb.y + height;
+        localPoints[i].z = (i % 2 == 0) ? posArb.z - depth : posArb.z + depth;
     }
-
-    return std::make_pair(globalFirst, globalLast);
+    auto min = floatCoordinate(1, 1, 1) * std::numeric_limits<int>::max();
+    floatCoordinate max{0, 0, 0};
+    for (const auto localCoord : localPoints) {
+        const auto worldCoord = localCoord.toWorldFrom(brush.v1, brush.v2, brush.n).abs().capped(Session::singleton().movementAreaMin, Session::singleton().movementAreaMax);
+        min = {std::min(worldCoord.x, min.x), std::min(worldCoord.y, min.y), std::min(worldCoord.z, min.z)};
+        max = {std::max(worldCoord.x, max.x), std::max(worldCoord.y, max.y), std::max(worldCoord.z, max.z)};
+    }
+    return std::make_pair(min, max);
 }
 
 void coordCubesMarkChanged(const CubeCoordSet & cubeChangeSet) {

segmentation/segmentationsplit.h
@@ -13,7 +13,7 @@ public:
         two_dim, three_dim
     };
     enum class view_t {
-        xy, xz, zy
+        xy, xz, zy, arb
     };
     enum class shape_t {
         angular, round
@@ -24,6 +24,9 @@ public:
     mode_t mode = mode_t::two_dim;
     view_t view = view_t::xy;
     shape_t shape = shape_t::round;
+    floatCoordinate v1{1, 0, 0};
+    floatCoordinate v2{0, 1, 0};
+    floatCoordinate n{0, 0, 1};
 };
 
 class brush_subject : public QObject, private brush_t {
@@ -53,8 +56,11 @@ public:
     int getRadius() const {
         return radius;
     }
-    void setView(const view_t newView) {
+    void setView(const view_t newView, const floatCoordinate & newV1, const floatCoordinate & newV2, const floatCoordinate & newN) {
         view = newView;
+        v1 = newV1;
+        v2 = newV2;
+        n = newN;
     }
     view_t getView() const {
         return view;

widgets/viewport.cpp
@@ -166,6 +166,16 @@ ViewportOrtho::ViewportOrtho(QWidget *parent, ViewportType viewportType) : Viewp
     timeFBase.start();
 }
 
+void ViewportOrtho::mouseMoveEvent(QMouseEvent *event) {
+    ViewportBase::mouseMoveEvent(event);
+    Segmentation::singleton().brush.setView(static_cast<brush_t::view_t>(viewportType), v1, v2, n);
+}
+
+void ViewportOrtho::mousePressEvent(QMouseEvent *event) {
+    Segmentation::singleton().brush.setView(static_cast<brush_t::view_t>(viewportType), v1, v2, n);
+    ViewportBase::mousePressEvent(event);
+}
+
 void ViewportOrtho::resetTexture() {
     const auto size = texture.edgeLengthPx;
     if (texture.texHandle != 0) {
@@ -473,8 +483,6 @@ void ViewportBase::mouseMoveEvent(QMouseEvent *event) {
         handleMouseMotionRightHold(event);
     }
     handleMouseHover(event);
-
-    Segmentation::singleton().brush.setView(static_cast<brush_t::view_t>(viewportType));
 
     prevMouseMove = event->pos();
 }

widgets/viewport.h
@@ -323,6 +323,10 @@ class ViewportOrtho : public ViewportBase {
 
     floatCoordinate arbNodeDragCache = {};
     class nodeListElement *draggedNode = nullptr;
+
+    virtual void mousePressEvent(QMouseEvent *event) override;
+    virtual void mouseMoveEvent(QMouseEvent *event) override;
+
     bool mouseEventAtValidDatasetPosition(const QMouseEvent *event);
     virtual void handleKeyPress(const QKeyEvent *event) override;
     virtual void handleMouseHover(const QMouseEvent *event) override;

widgets/viewportevents.cpp
@@ -40,7 +40,6 @@
 #include <QApplication>
 #include <QMessageBox>
 
-#include <boost/math/constants/constants.hpp>
 #include <boost/optional.hpp>
 
 #include <cstdlib>

commit 14424f989c92971b4c32b1ed28e7c43ce3f1df2a

Fix getRegion() that returned offsetted region when applied to position near lower dataset boundaries (regression from 59623d1f242ecb33275293613a20879b61e9cdf0).
abs() turned negative coordinate components to positive ones instead of truncating to 0, which is also already done by capped() anyway. abs() isn’t used anywhere else.

coordinate.h
@@ -126,10 +126,6 @@ public:
         return static_cast<CoordinateDerived&>(*this = *this / rhs);
     }
 
-    constexpr CoordinateDerived abs() const {
-        return {std::abs(x), std::abs(y), std::abs(z)};
-    }
-
     constexpr float length() const {
         return std::sqrt(x*x + y*y + z*z);
     }

segmentation/cubeloader.cpp
@@ -90,7 +90,7 @@ std::pair<Coordinate, Coordinate> getRegion(const floatCoordinate & centerPos, c
     auto min = floatCoordinate(1, 1, 1) * std::numeric_limits<int>::max();
     floatCoordinate max{0, 0, 0};
     for (const auto localCoord : localPoints) {
-        const auto worldCoord = localCoord.toWorldFrom(brush.v1, brush.v2, brush.n).abs().capped(Session::singleton().movementAreaMin, Session::singleton().movementAreaMax);
+        const auto worldCoord = localCoord.toWorldFrom(brush.v1, brush.v2, brush.n).capped(Session::singleton().movementAreaMin, Session::singleton().movementAreaMax);
         min = {std::min(worldCoord.x, min.x), std::min(worldCoord.y, min.y), std::min(worldCoord.z, min.z)};
         max = {std::max(worldCoord.x, max.x), std::max(worldCoord.y, max.y), std::max(worldCoord.z, max.z)};
     }