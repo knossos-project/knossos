knossos-project/knossos

commit f773c5f88208190c25479b5f532ec2924f7fbf3d

introduce allocationEnabled and loadingEnabled for dataset layers
if compression is enabled for Heidelbrain datasets assume a raw layer is present and add it as a disabled 2nd layer

move createCorrespondingOverlayLayer from loadDataset to fromLegacyConf

always add but only enable overlay layers if checkbox is on

loader.cpp
@@ -217,6 +217,9 @@ Loader::Worker::Worker(const decltype(datasets) & layers)
     // memory location back into this list.
     for (std::size_t layerId{0}; layerId < layers.size(); ++layerId) {
         state->cube2Pointer.emplace_back(std::log2(layers[layerId].highestAvailableMag)+1);
+        if (!datasets[layerId].allocationEnabled) {
+            continue;
+        }
         const auto overlayFactor = layers[layerId].isOverlay() ? OBJID_BYTES : 1;
         qDebug() << "Allocating" << state->cubeSetBytes * overlayFactor / 1024. / 1024. << "MiB for cubes.";
         for (size_t i = 0; i < state->cubeSetBytes * overlayFactor; i += state->cubeBytes * overlayFactor) {
@@ -684,8 +687,10 @@ void Loader::Worker::downloadAndLoadCubes(const unsigned int loadingNr, const Co
     for (auto globalCoord : allCubes) {
         if (loadingNr == Loader::Controller::singleton().loadingNr) {
             for (std::size_t layerId{0}; layerId < datasets.size(); ++layerId) {
-                startDownload(layerId, datasets[layerId], globalCoord, slotDownload[layerId], slotDecompression[layerId], freeSlots[layerId], state->cube2Pointer[layerId][loaderMagnification]);
-                workaroundProcessLocalImmediately();//https://bugreports.qt.io/browse/QTBUG-45925
+                if (datasets[layerId].loadingEnabled) {
+                    startDownload(layerId, datasets[layerId], globalCoord, slotDownload[layerId], slotDecompression[layerId], freeSlots[layerId], state->cube2Pointer[layerId][loaderMagnification]);
+                    workaroundProcessLocalImmediately();//https://bugreports.qt.io/browse/QTBUG-45925
+                }
             }
         }
     }

dataset.cpp
@@ -194,6 +194,9 @@ Dataset::list_t Dataset::parsePyKnossosConf(const QUrl & configUrl, QString conf
         if (info.url.isEmpty()) {
             info.url = QUrl::fromLocalFile(QFileInfo(configUrl.toLocalFile()).absoluteDir().absolutePath());
         }
+        if (&info != &infos.front()) {// disable all layers expect the first TODO multi layer
+            info.allocationEnabled = info.loadingEnabled = false;
+        }
     }
 
     return infos;
@@ -335,7 +338,14 @@ Dataset::list_t Dataset::fromLegacyConf(const QUrl & configUrl, QString config) 
     info.scale = info.scale / static_cast<float>(info.magnification);
     info.lowestAvailableMag = info.highestAvailableMag = info.magnification;
 
-    return {info};
+    if (info.type != Dataset::CubeType::RAW_UNCOMPRESSED) {
+        auto info2 = info;
+        info2.type = Dataset::CubeType::RAW_UNCOMPRESSED;
+        info2.allocationEnabled = info2.loadingEnabled = false;
+        return {info, info2, info.createCorrespondingOverlayLayer()};
+    } else {
+        return {info, info.createCorrespondingOverlayLayer()};
+    }
 }
 
 void Dataset::checkMagnifications() {

dataset.h
@@ -88,6 +88,8 @@ struct Dataset {
     QString fileextension;
     QUrl url;
     QString token;
+    bool allocationEnabled{true};
+    bool loadingEnabled{true};
 
     static Dataset::list_t datasets;
     static auto & current() {

viewer.cpp
@@ -39,6 +39,7 @@
 #include <QVector3D>
 
 #include <boost/container/static_vector.hpp>
+#include <boost/range/combine.hpp>
 
 #include <fstream>
 #include <cmath>
@@ -1233,6 +1234,9 @@ void Viewer::resizeTexEdgeLength(const int cubeEdge, const int superCubeEdge, co
             vp.resetTexture(layerCount);
         });
         recalcTextureOffsets();
+    }
+    for (auto tup : boost::combine(viewerState.layerVisibility, Dataset::datasets)) {
+        tup.get<0>() = tup.get<1>().loadingEnabled;// TODO multi layer
     }
 }
 

widgets/datasetloadwidget.cpp
@@ -283,6 +283,10 @@ bool DatasetLoadWidget::loadDataset(const boost::optional<bool> loadOverlay, QUr
     if (Dataset::isHeidelbrain(path)) {
         try {
             layers.front().checkMagnifications();
+            for (auto & layer : layers) {// apply discovered mags to all layers
+                layer.lowestAvailableMag = layers.front().lowestAvailableMag;
+                layer.highestAvailableMag = layers.front().highestAvailableMag;
+            }
         } catch (std::exception &) {
             if (!silent) {
                 QMessageBox warning{QApplication::activeWindow()};
@@ -310,23 +314,21 @@ bool DatasetLoadWidget::loadDataset(const boost::optional<bool> loadOverlay, QUr
     if (loadOverlay != boost::none) {
         segmentationOverlayCheckbox.setChecked(loadOverlay.get());
     }
-    Segmentation::singleton().enabled = false;
-    if (segmentationOverlayCheckbox.isChecked()) {
-        if (Dataset::isHeidelbrain(path)) {
-            layers.push_back(layers.front().createCorrespondingOverlayLayer());
+    Segmentation::singleton().enabled = segmentationOverlayCheckbox.isChecked();
+    bool overlayPresent{false};
+    for (std::size_t i = 0; i < layers.size(); ++i) {// determine segmentation layer
+        if (layers[i].isOverlay()) {
+            overlayPresent = true;
+            layers[i].allocationEnabled = layers[i].loadingEnabled = Segmentation::singleton().enabled;
+            Segmentation::singleton().layerId = i;// last layer gets snappy layer
         }
-        for (std::size_t i = 0; i < layers.size(); ++i) {// determine segmentation layer
-            if (layers[i].isOverlay()) {
-                Segmentation::singleton().enabled = true;
-                Segmentation::singleton().layerId = i;
-            }
-        }
-        if (!Segmentation::singleton().enabled) {// add empty overlay channel
-            Segmentation::singleton().enabled = true;
-            Segmentation::singleton().layerId = layers.size();
-            layers.push_back(layers.front());
-            layers.back().type = Dataset::CubeType::SNAPPY;
-        }
+    }
+    if (!overlayPresent) {// add empty overlay channel
+        const auto i = layers.size();
+        layers.emplace_back(layers.front());
+        layers[i].allocationEnabled = layers[i].loadingEnabled = Segmentation::singleton().enabled;
+        Segmentation::singleton().layerId = i;
+        layers.back().type = Dataset::CubeType::SNAPPY;
     }
     Dataset::datasets = layers;
 

commit 4c02ee7a2bf375bb07677c040d290b25f236aea1

dcoi for every layer individually
fixes a regression from f773c5f88208190c25479b5f532ec2924f7fbf3d
where modified overlay cubes were replaced by their snappy counterparts
when dragging across a cube boundary
before modifications were committed to the snappy cache
(not until unload, i.e. when leaving the supercube)

remove visible and cache cubes as they have no use just yet
time also wasn’t used

loader.cpp
@@ -504,8 +504,6 @@ void Loader::Worker::broadcastProgress(bool startup) {
 }
 
 void Loader::Worker::downloadAndLoadCubes(const unsigned int loadingNr, const Coordinate center, const UserMoveType userMoveType, const floatCoordinate & direction, const Dataset::list_t & changedDatasets) {
-    QTime time;
-    time.start();
     datasets = changedDatasets;
     cleanup(center);
     decltype(Dataset::magnification) magnification = datasets.front().magnification;
@@ -513,21 +511,14 @@ void Loader::Worker::downloadAndLoadCubes(const unsigned int loadingNr, const Co
     const auto cubeEdgeLen = datasets.front().cubeEdgeLength;
     const auto Dcoi = DcoiFromPos(center.cube(cubeEdgeLen, magnification), userMoveType, direction);//datacubes of interest prioritized around the current position
     //split dcoi into slice planes and rest
-    std::vector<Coordinate> allCubes;
-    std::vector<Coordinate> visibleCubes;
-    std::vector<Coordinate> cacheCubes;
+    std::vector<std::pair<std::size_t, Coordinate>> allCubes;
     for (auto && todo : Dcoi) {
         const Coordinate globalCoord = todo.cube2Global(cubeEdgeLen, magnification);
         QMutexLocker locker(&state->protectCube2Pointer);
         for (std::size_t layerId{0}; layerId < datasets.size(); ++layerId) {
             // only queue downloads which are necessary
             if (Coordinate2BytePtr_hash_get_or_fail(state->cube2Pointer, layerId, loaderMagnification, globalCoord.cube(cubeEdgeLen, magnification)) == nullptr) {
-                allCubes.emplace_back(globalCoord);
-                if (currentlyVisibleWrap(center, datasets[layerId])(globalCoord)) {
-                    visibleCubes.emplace_back(globalCoord);
-                } else {
-                    cacheCubes.emplace_back(globalCoord);
-                }
+                allCubes.emplace_back(layerId, globalCoord);
             }
         }
     }
@@ -680,15 +671,13 @@ void Loader::Worker::downloadAndLoadCubes(const unsigned int loadingNr, const Co
     };
 
     const auto workaroundProcessLocalImmediately = datasets[0].url.scheme() == "file" ? [](){QCoreApplication::processEvents();} : [](){};
-    for (auto globalCoord : allCubes) {
+    for (auto [layerId, globalCoord] : allCubes) {
         if (loadingNr == Loader::Controller::singleton().loadingNr) {
-            for (std::size_t layerId{0}; layerId < datasets.size(); ++layerId) {
-                if (datasets[layerId].loadingEnabled) {
-                    try {
-                        startDownload(layerId, datasets[layerId], globalCoord, slotDownload[layerId], slotDecompression[layerId], freeSlots[layerId], state->cube2Pointer.at(layerId).at(loaderMagnification));
-                    } catch (const std::out_of_range &) {}
-                    workaroundProcessLocalImmediately();//https://bugreports.qt.io/browse/QTBUG-45925
-                }
+            if (datasets[layerId].loadingEnabled) {
+                try {
+                    startDownload(layerId, datasets[layerId], globalCoord, slotDownload[layerId], slotDecompression[layerId], freeSlots[layerId], state->cube2Pointer.at(layerId).at(loaderMagnification));
+                } catch (const std::out_of_range &) {}
+                workaroundProcessLocalImmediately();//https://bugreports.qt.io/browse/QTBUG-45925
             }
         }
     }