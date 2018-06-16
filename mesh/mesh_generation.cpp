26564176335a9faf8f84522a8b899107bf441db3
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2018-06-16 02:23:17 +0200

    multi object mesh gen
    collect triangle cases for all objects of found subobjects separately
    iterate to be processed objects and generate vertices
    generate 1 mesh for each object

--- mesh/mesh_generation.cpp
+++ mesh/mesh_generation.cpp
@@ -20,9 +20,8 @@
 
 #include <unordered_map>
 
-template<typename T>
-void marchingCubes(std::unordered_map<floatCoordinate, int> & points, std::vector<unsigned int> & faces, std::size_t & idCounter, const std::vector<T> & data, const std::unordered_set<T> & values
-    , const std::array<double, 3> & origin, const std::array<double, 3> & dims, const std::array<double, 3> & spacing, const std::array<double, 6> & extent) {
+template<typename T, typename U>
+void marchingCubes(std::unordered_map<U, std::unordered_map<floatCoordinate, int>> & obj2points, std::unordered_map<U, std::vector<unsigned int>> & obj2faces, std::unordered_map<std::uint64_t, std::size_t> & obj2idCounter, const std::vector<T> & data, const std::unordered_map<T, U> & soid2oid, const std::array<double, 3> & origin, const std::array<double, 3> & dims, const std::array<double, 3> & spacing, const std::array<double, 6> & extent) {
     const auto triCases = vtkMarchingCubesTriangleCases::GetCases();
 
     static const std::array<int, 8> CASE_MASK{1,2,4,8,16,32,64,128};
@@ -85,43 +84,50 @@ void marchingCubes(std::unordered_map<floatCoordinate, int> & points, std::vecto
                 pts[7][1] = ynext;
                 pts[7][2] = znext;
 
-                std::size_t index = 0;
+                std::unordered_map<U, std::uint64_t> obj2index;
                 // Build the case table
                 for (std::size_t pi = 0; pi < 8; ++pi) {
                     // for discrete marching cubes, we are looking for an
                     // exact match of a scalar at a vertex to a value
-                    if (values.find(cubeVals[pi]) != std::end(values)) {
-                        index |= CASE_MASK[pi];
-                    }
-                }
-                if (index == 0 || index == 255) {// no surface
-                    continue;
-                }
-
-                const auto & triCase = triCases[index];
-                for (auto edge = triCase.edges; edge[0] > -1; edge += 3) {
-                    std::array<std::size_t, 3> ptIds;
-                    for (std::size_t vi = 0; vi < 3; vi++) {// insert triangle
-                        const auto vert = edges[edge[vi]];
-                        // for discrete marching cubes, the interpolation point is always 0.5.
-                        const auto t = 0.5;
-                        const auto x1 = pts[vert[0]];
-                        const auto x2 = pts[vert[1]];
-                        std::array<float, 3> vertex;
-                        vertex[0] = x1[0] + t * (x2[0] - x1[0]);
-                        vertex[1] = x1[1] + t * (x2[1] - x1[1]);
-                        vertex[2] = x1[2] + t * (x2[2] - x1[2]);
-                        floatCoordinate coord{vertex[0], vertex[1], vertex[2]};
-                        if (points.find(coord) == std::end(points)) {
-                            points[coord] = ptIds[vi] = idCounter++;// add point
-                        } else {
-                            ptIds[vi] = points[coord];
+                    if (auto it = soid2oid.find(cubeVals[pi]); it != std::end(soid2oid)) {
+                        for (auto & oindex : Segmentation::singleton().subobjectFromId(it->first, {}).objects) {
+                            obj2index[Segmentation::singleton().objects[oindex].id] |= CASE_MASK[pi];
                         }
                     }
-                    if (ptIds[0] != ptIds[1] && ptIds[0] != ptIds[2] && ptIds[1] != ptIds[2] ) {// check for degenerate triangle
-                        faces.emplace_back(ptIds[0]);
-                        faces.emplace_back(ptIds[1]);
-                        faces.emplace_back(ptIds[2]);
+                }
+                for (auto pair : obj2index) {
+                    const auto oid = pair.first;
+                    auto & points = obj2points[oid];
+                    auto & faces = obj2faces[oid];
+                    auto & idCounter = obj2idCounter[oid];
+                    const auto index = pair.second;
+                    if (index != 0 && index != 255) {// no surface
+                        const auto & triCase = triCases[index];
+                        for (auto edge = triCase.edges; edge[0] > -1; edge += 3) {
+                            std::array<std::size_t, 3> ptIds;
+                            for (std::size_t vi = 0; vi < 3; vi++) {// insert triangle
+                                const auto vert = edges[edge[vi]];
+                                // for discrete marching cubes, the interpolation point is always 0.5.
+                                const auto t = 0.5;
+                                const auto x1 = pts[vert[0]];
+                                const auto x2 = pts[vert[1]];
+                                std::array<float, 3> vertex;
+                                vertex[0] = x1[0] + t * (x2[0] - x1[0]);
+                                vertex[1] = x1[1] + t * (x2[1] - x1[1]);
+                                vertex[2] = x1[2] + t * (x2[2] - x1[2]);
+                                floatCoordinate coord{vertex[0], vertex[1], vertex[2]};
+                                if (points.find(coord) == std::end(points)) {
+                                    points[coord] = ptIds[vi] = idCounter++;// add point
+                                } else {
+                                    ptIds[vi] = points[coord];
+                                }
+                            }
+                            if (ptIds[0] != ptIds[1] && ptIds[0] != ptIds[2] && ptIds[1] != ptIds[2] ) {// check for degenerate triangle
+                                faces.emplace_back(ptIds[0]);
+                                faces.emplace_back(ptIds[1]);
+                                faces.emplace_back(ptIds[2]);
+                            }
+                        }
                     }
                 }
             }
@@ -129,15 +135,15 @@ void marchingCubes(std::unordered_map<floatCoordinate, int> & points, std::vecto
     }
 }
 
-auto generateMeshForSubobjectID(const std::unordered_set<std::uint64_t> & values, const Loader::Worker::SnappyCache & cubes, QProgressDialog & progress) {
-    std::vector<std::unordered_map<floatCoordinate, int>> totalpoints(cubes.size());
-    std::vector<std::vector<unsigned int>> totalfaces(cubes.size());
+auto generateMeshForSubobjectID(const std::unordered_map<std::uint64_t, std::uint64_t> & soid2oid, const std::vector<std::uint64_t> & objects, const Loader::Worker::SnappyCache & cubes, QProgressDialog & progress) {
+    std::vector<std::unordered_map<std::uint64_t, std::unordered_map<floatCoordinate, int>>> obj2totalpoints(cubes.size());
+    std::vector<std::unordered_map<std::uint64_t, std::vector<unsigned int>>> obj2totalfaces(cubes.size());
     const auto processCube = [&](const auto & val){
         const auto id = val.first;
         const auto & pair = *val.second;
-        auto & points = totalpoints[id];
-        auto & faces = totalfaces[id];
-        std::size_t idCounter{0};
+        auto & obj2points = obj2totalpoints[id];
+        auto & obj2faces = obj2totalfaces[id];
+        std::unordered_map<std::uint64_t, std::size_t> obj2idCounter;
         const std::size_t cubeEdgeLen = Dataset::current().cubeEdgeLength;
         const std::size_t size = std::pow(cubeEdgeLen, 3);
 
@@ -167,7 +173,7 @@ auto generateMeshForSubobjectID(const std::unordered_set<std::uint64_t> & values
         const std::array<double, 3> spacing{{Dataset::current().scale.x, Dataset::current().scale.y, Dataset::current().scale.z}};
         const std::array<double, 6> extent{{0, dims[0], 0, dims[1], 0, dims[2]}};
 
-        marchingCubes(points, faces, idCounter, extractedCubeForCoord(pair.first), values, origin, dims, spacing, extent);
+        marchingCubes(obj2points, obj2faces, obj2idCounter, extractedCubeForCoord(pair.first), soid2oid, origin, dims, spacing, extent);
         for (std::size_t i = 0; i < 6; ++i) {
             const std::array<double, 3> dims{{i < 2 ? 2.0 : cubeEdgeLen + 2, i % 4 < 2 ? cubeEdgeLen + 2 : 2.0, i < 4 ? cubeEdgeLen + 2: 2.0}};
             const floatCoordinate unscaledOrigin(pair.first.cube2Global(cubeEdgeLen, 1) + floatCoordinate(i == 0 ? -1 : i == 1 ? 128 : -1, i == 2 ? -1 : i == 3 ? 128 : -1, i == 4 ? -1 : i == 5 ? 128 : -1));
@@ -192,7 +198,7 @@ auto generateMeshForSubobjectID(const std::unordered_set<std::uint64_t> & values
                 }
             }
             const auto scaledOrigin = Dataset::current().scale.componentMul(unscaledOrigin);
-            marchingCubes(points, faces, idCounter, data, values, {{scaledOrigin.x, scaledOrigin.y, scaledOrigin.z}}, dims, spacing, extent);
+            marchingCubes(obj2points, obj2faces, obj2idCounter, data, soid2oid, {{scaledOrigin.x, scaledOrigin.y, scaledOrigin.z}}, dims, spacing, extent);
         }
     };
     std::string s;
@@ -212,27 +218,38 @@ auto generateMeshForSubobjectID(const std::unordered_set<std::uint64_t> & values
     watcher.setFuture(QtConcurrent::map(threadids, processCube));
     pause.exec();
 
-    QVector<float> verts;
-    std::vector<std::size_t> offsets;
-    for (const auto & elempoints : totalpoints) {
-        const auto offset = verts.size();
-        offsets.emplace_back(offset / 3);
-        verts.resize(offset + 3 * elempoints.size());
-        for (const auto & pair : elempoints) {
-            verts[offset + 3 * pair.second    ] = pair.first.x;
-            verts[offset + 3 * pair.second + 1] = pair.first.y;
-            verts[offset + 3 * pair.second + 2] = pair.first.z;
+    for (const auto oid : objects) {
+        QVector<float> verts;
+        std::vector<std::size_t> offsets;
+        for (const auto & elempoints : obj2totalpoints) {
+            if (auto pit = elempoints.find(oid); pit != std::end(elempoints)) {
+                const auto offset = verts.size();
+                offsets.emplace_back(offset / 3);
+                verts.resize(offset + 3 * pit->second.size());
+                for (const auto & pair : pit->second) {
+                    verts[offset + 3 * pair.second    ] = pair.first.x;
+                    verts[offset + 3 * pair.second + 1] = pair.first.y;
+                    verts[offset + 3 * pair.second + 2] = pair.first.z;
+                }
+            }
         }
-    }
-    QVector<unsigned int> faces;
-    std::size_t offseti{0};
-    for (const auto & elemfaces : totalfaces) {
-        for (const auto & elem : elemfaces) {
-            faces.push_back(offsets[offseti] + elem);
+        QVector<unsigned int> faces;
+        std::size_t offseti{0};
+        for (const auto & elemfaces : obj2totalfaces) {
+            if (auto pit = elemfaces.find(oid); pit != std::end(elemfaces)) {
+                for (const auto & elem : pit->second) {
+                    faces.push_back(offsets[offseti] + elem);
+                }
+                ++offseti;
+            }
         }
-        ++offseti;
+
+        qDebug() << oid << ':' << (verts.size() / 3) << "→" << faces.size() / 3;
+
+        QVector<float> normals;
+        QVector<std::uint8_t> colors;
+        Skeletonizer::singleton().addMeshToTree(oid, verts, normals, faces, colors, GL_TRIANGLES);
     }
-    return std::make_tuple(verts, faces);
 }
 
 void generateMeshesForSubobjectsOfSelectedObjects() {
@@ -240,18 +257,15 @@ void generateMeshesForSubobjectsOfSelectedObjects() {
     QProgressDialog progress(QObject::tr("Generating Meshes …"), "Cancel", 0, Segmentation::singleton().selectedObjectsCount() * cubes[0].size(), QApplication::activeWindow());
     progress.setWindowModality(Qt::WindowModal);
     qDebug() << "Generating meshes for" << Segmentation::singleton().selectedObjectsCount() << "objects over" << cubes[0].size() << "cubes";
+
+    std::unordered_map<std::size_t, std::uint64_t> soids;
+    std::vector<std::uint64_t> oids;
     for (const auto objectIndex : Segmentation::singleton().selectedObjectIndices) {
         const auto oid = Segmentation::singleton().objects[objectIndex].id;
-        std::unordered_set<std::uint64_t> soids;
         for (const auto & elem : Segmentation::singleton().objects[objectIndex].subobjects) {
-            soids.emplace(elem.get().id);
+            soids.emplace(elem.get().id, oid);
         }
-        auto [verts, faces] = generateMeshForSubobjectID(soids, cubes[0], progress);
-
-        QVector<float> normals;
-        QVector<std::uint8_t> colors;
-        Skeletonizer::singleton().addMeshToTree(oid, verts, normals, faces, colors, GL_TRIANGLES);
-
-        qDebug() << oid << ':' << (verts.size() / 3) << "→" << faces.size() / 3;
+        oids.emplace_back(oid);
     }
+    generateMeshForSubobjectID(soids, oids, cubes[0], progress);
 }
