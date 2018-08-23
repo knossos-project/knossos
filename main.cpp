60437ca8164ecca0ea3fae37eca257c444f74a26
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2018-08-23 09:34:54 +0200

    testing code

--- main.cpp
+++ main.cpp
@@ -100,7 +100,15 @@ void debugMessageHandler(QtMsgType type, const QMessageLogContext &
 
 Q_DECLARE_METATYPE(std::string)
 
+#include "skeleton/NeuroLucida.h"
+
 int main(int argc, char *argv[]) {
+//    parseNeuroLucida(QFile{"C:/k_mesh_test/12042017 Slice 5 thinned.ASC"});
+//    parseNeuroLucida(QFile{"C:/k_mesh_test/12042017 Slice 5.ASC"});
+//    parseNeuroLucida(QFile{"C:/k_mesh_test/01232018 slice 6 thinned.ASC"});
+    parseNeuroLucida(QFile{"C:/k_mesh_test/01232018 slice 6.ASC"});
+    exit(0);
+
     QtConcurrent::run([](){ QSslSocket::supportsSsl(); });// workaround until https://bugreports.qt.io/browse/QTBUG-59750
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);// explicitly enable sharing for undocked viewports
 #ifdef Q_OS_OSX
