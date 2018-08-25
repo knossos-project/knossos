93b7681c1003b59c21b992e5fb674e011e870ecd
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2018-08-25 10:05:10 +0200

    elaborate error output for blockDownloadExtractData

--- network.cpp
+++ network.cpp
@@ -126,7 +126,7 @@ QPair<bool, QByteArray> blockDownloadExtractData(QNetworkReply & reply) {
     });
 
     if (reply.error() != QNetworkReply::NoError) {
-        qDebug() << reply.errorString();
+        qDebug() << reply.attribute(QNetworkRequest::HttpStatusCodeAttribute) << static_cast<int>(reply.error()) << reply.error() << reply.errorString();
     }
     emit Network::singleton().finishedNetworkRequest();
     reply.deleteLater();
