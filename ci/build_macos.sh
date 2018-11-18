9e34edfaa91458377f3f4f9bf3191f261afef7fa
Eroica <seba79@mailbox.org>
2018-11-18 14:09:23 +0100

    Add Mac CI build script

---
+++ ci/build_macos.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -x
+
+# Fix Python headers
+# https://bugs.python.org/review/10910/diff2/2561:8559/Include/pyport.h
+cd ..
+mkdir python2.7
+cp /usr/include/python2.7/* python2.7
+ed -s python2.7/pyport.h << EOF
+/#ifdef _PY_PORT_CTYPE_UTF8_ISSUE/
+a
+#ifndef __cplusplus
+.
++16
+a
+#endif
+.
+w
+EOF
+
+# Build PythonQt
+git clone https://github.com/knossos-project/PythonQt.git
+mkdir PythonQt-build && cd PythonQt-build
+cmake ../PythonQt -DCMAKE_PREFIX_PATH=/usr/local/opt/qt -DPYTHON_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/../python2.7
+make && make install
+
+# Fix QuaZip include directory name
+cd $TRAVIS_BUILD_DIR && cd ..
+mkdir -p quazip/quazip5
+QUAZIP_VERSION=`brew list --versions quazip | cut -d " " -f2`
+cp -R /usr/local/Cellar/quazip/${QUAZIP_VERSION}/include/quazip/* quazip/quazip5/
+
+# Build KNOSSOS
+mkdir knossos-build && cd knossos-build
+cmake ../knossos -DCMAKE_PREFIX_PATH=/usr/local/opt/qt -DCMAKE_CXX_FLAGS=-isystem\ ${TRAVIS_BUILD_DIR}/../quazip
+make
+
+# OS X housekeeping
+mv knossos.app KNOSSOS.app
+/usr/local/opt/qt/bin/macdeployqt KNOSSOS.app
+cp /usr/local/Cellar/quazip/${QUAZIP_VERSION}/lib/libquazip.1.0.0.dylib KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib
+
+# Deployment
+BRANCH_PREFIX=""
+if [[ $TRAVIS_BRANCH != "master" ]]; then
+	BRANCH_PREFIX=${TRAVIS_BRANCH}-
+fi
+zip -r ${TRAVIS_BUILD_DIR}/macos.${BRANCH_PREFIX}KNOSSOS.nightly.app.zip KNOSSOS.app
