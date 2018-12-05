1effe08f2718cfd1f874fcf8381678ac1b9da02a
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2018-12-05 12:17:34 +0100

    take over ids from file

--- skeleton/swc.cpp
+++ skeleton/swc.cpp
@@ -18,16 +18,16 @@ void parseSWC(QIODevice && file) {
     const auto node = [treeID](auto & ctx){
         using namespace boost::fusion;
         const auto & attr = _attr(ctx);
-        const auto pos = floatCoordinate(at_c<1>(attr), at_c<2>(attr), at_c<3>(attr)) * 1000.0 / Dataset::current().scale;
-        const auto radius = at_c<4>(attr) * 1000.0 / Dataset::current().scale.x;
-        auto node = Skeletonizer::singleton().addNode(boost::none, radius, treeID, pos, VIEWPORT_UNDEFINED, 1, boost::none, false);
-        if (const auto connectedNode = Skeletonizer::singleton().findNodeByNodeID(at_c<5>(attr))) {
+        const auto pos = floatCoordinate(at_c<2>(attr), at_c<3>(attr), at_c<4>(attr)) * 1000.0 / Dataset::current().scale;
+        const auto radius = at_c<5>(attr) * 1000.0 / Dataset::current().scale.x;
+        auto node = Skeletonizer::singleton().addNode(at_c<0>(attr), radius, treeID, pos, VIEWPORT_UNDEFINED, 1, boost::none, false);
+        if (const auto connectedNode = Skeletonizer::singleton().findNodeByNodeID(at_c<6>(attr))) {
             Skeletonizer::singleton().addSegment(*connectedNode, node.get());
         }
     };
     using namespace boost::spirit::x3;
     const auto comment_ = lexeme['#' >> *(char_ - eol) >> (eol|eoi)];
-    const auto node_ = int_ >> (int_ >> double_ >> double_ >> double_ >> double_ >> int_)[node];
+    const auto node_ = (int_ >> int_ >> double_ >> double_ >> double_ >> double_ >> int_)[node];
     const bool res = boost::spirit::x3::phrase_parse(first, std::end(stream), *(node_ | comment_), boost::spirit::x3::space);
 
     if (!res || first != std::end(stream)) {
