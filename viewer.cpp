knossos-project/knossos

commit 4dbee5750d71b968bddb56e72e5db4d7ae3779ae

per layer bias and range delta

viewer.cpp
@@ -251,7 +251,18 @@ void Viewer::setMagnificationLock(const bool locked) {
     emit magnificationLockChanged(locked);
 }
 
-void Viewer::dcSliceExtract(std::uint8_t * datacube, Coordinate cubePosInAbsPx, std::uint8_t * slice, ViewportOrtho & vp, bool useCustomLUT) {
+const auto datasetAdjustment = [](auto layerId, auto index){
+    if (state->viewerState->datasetColortableOn) {
+        return state->viewerState->datasetColortable[index];
+    } else {
+        const auto MAX_COLORVAL{std::numeric_limits<uint8_t>::max()};
+        int dynIndex = ((index - state->viewerState->layerRenderSettings[layerId].bias * 255) / (state->viewerState->layerRenderSettings[layerId].rangeDelta));
+        std::uint8_t val = std::min(static_cast<int>(MAX_COLORVAL), std::max(0, dynIndex));
+        return std::tuple(val, val, val);
+    }
+};
+
+void Viewer::dcSliceExtract(std::uint8_t * datacube, Coordinate cubePosInAbsPx, std::uint8_t * slice, ViewportOrtho & vp, std::size_t layerId) {
     const auto & session = Session::singleton();
     const Coordinate areaMinCoord = {session.movementAreaMin.x,
                                      session.movementAreaMin.y,
@@ -274,15 +285,12 @@ void Viewer::dcSliceExtract(std::uint8_t * datacube, Coordinate cubePosInAbsPx, 
 
     int offsetX = 0, offsetY = 0;
     const int coordsPerLine = cubeEdgeLen * Dataset::current().magnification;
+    const auto useCustomLUT = state->viewerState->layerRenderSettings[layerId].bias > 0 || state->viewerState->layerRenderSettings[layerId].rangeDelta < std::numeric_limits<uint8_t>::max();
     for (auto y = cubeEdgeLen; y != 0; --y) {
         for (auto x = cubeEdgeLen; x != 0; --x) {// x and y’s values are not used
             uint8_t r, g, b;
-            if(useCustomLUT) {
-                //extract data as unsigned number from the datacube
-                const uint8_t adjustIndex = datacube[0];
-                r = std::get<0>(state->viewerState->datasetAdjustmentTable[adjustIndex]);
-                g = std::get<1>(state->viewerState->datasetAdjustmentTable[adjustIndex]);
-                b = std::get<2>(state->viewerState->datasetAdjustmentTable[adjustIndex]);
+            if (useCustomLUT) {
+                std::tie(r, g, b) = datasetAdjustment(layerId, datacube[0]);
             } else {
                 r = g = b = datacube[0];
             }
@@ -318,7 +326,7 @@ void Viewer::dcSliceExtract(std::uint8_t * datacube, Coordinate cubePosInAbsPx, 
     }
 }
 
-void Viewer::dcSliceExtract(std::uint8_t * datacube, floatCoordinate *currentPxInDc_float, std::uint8_t * slice, int s, int *t, const floatCoordinate & v2, bool useCustomLUT, float usedSizeInCubePixels) {
+void Viewer::dcSliceExtract(std::uint8_t * datacube, floatCoordinate *currentPxInDc_float, std::uint8_t * slice, int s, int *t, const floatCoordinate & v2, std::size_t layerId, float usedSizeInCubePixels) {
     Coordinate currentPxInDc = {roundFloat(currentPxInDc_float->x), roundFloat(currentPxInDc_float->y), roundFloat(currentPxInDc_float->z)};
     const auto cubeEdgeLen = Dataset::current().cubeEdgeLength;
     if((currentPxInDc.x < 0) || (currentPxInDc.y < 0) || (currentPxInDc.z < 0) ||
@@ -346,12 +354,8 @@ void Viewer::dcSliceExtract(std::uint8_t * datacube, floatCoordinate *currentPxI
         if(datacube == nullptr) {
             slice[sliceIndex] = slice[sliceIndex + 1] = slice[sliceIndex + 2] = 0;
         } else {
-            if(useCustomLUT) {
-                //extract data as unsigned number from the datacube
-                const unsigned char adjustIndex = datacube[dcIndex];
-                slice[sliceIndex + 0] = std::get<0>(state->viewerState->datasetAdjustmentTable[adjustIndex]);
-                slice[sliceIndex + 1] = std::get<1>(state->viewerState->datasetAdjustmentTable[adjustIndex]);
-                slice[sliceIndex + 2] = std::get<2>(state->viewerState->datasetAdjustmentTable[adjustIndex]);
+            if (layerId) {
+                std::tie(slice[sliceIndex + 0], slice[sliceIndex + 1], slice[sliceIndex + 2]) = datasetAdjustment(layerId, datacube[dcIndex]);
             }
             else {
                 slice[sliceIndex] = slice[sliceIndex + 1] = slice[sliceIndex + 2] = datacube[dcIndex];
@@ -571,7 +575,7 @@ void Viewer::vpGenerateTexture(ViewportOrtho & vp, const std::size_t layerId) {
                     if (Dataset::datasets[layerId].isOverlay()) {
                         ocSliceExtract(reinterpret_cast<std::uint64_t *>(cube) + slicePositionWithinCube, cubePosInAbsPx, texData.data() + index, vp);
                     } else {
-                        dcSliceExtract(reinterpret_cast<std::uint8_t  *>(cube) + slicePositionWithinCube, cubePosInAbsPx, texData.data() + index, vp, state->viewerState->datasetAdjustmentOn);
+                        dcSliceExtract(reinterpret_cast<std::uint8_t  *>(cube) + slicePositionWithinCube, cubePosInAbsPx, texData.data() + index, vp, layerId);
                     }
                 } else {
                     for (int y = y_px; y < y_px + cubeEdgeLen; ++y) {
@@ -740,12 +744,7 @@ void Viewer::vpGenerateTexture(ViewportArb &vp, const std::size_t layerId) {
             currentPxInDc_float = currentPx_float - currentDc * Dataset::current().cubeEdgeLength;
             t_old = t;
 
-            dcSliceExtract(reinterpret_cast<std::uint8_t *>(datacube),
-                           &currentPxInDc_float,
-                           texData.data(),
-                           s, &t,
-                           vp.v2,
-                           state->viewerState->datasetAdjustmentOn, vp.texture.usedSizeInCubePixels);
+            dcSliceExtract(reinterpret_cast<std::uint8_t *>(datacube), &currentPxInDc_float, texData.data(), s, &t, vp.v2, layerId, vp.texture.usedSizeInCubePixels);
             currentPx_float = currentPx_float - vp.v2 * (t - t_old);
         }
         s++;
@@ -1200,27 +1199,12 @@ void Viewer::loadDatasetLUT(const QString & path) {
 }
 
 void Viewer::datasetColorAdjustmentsChanged() {
-    if (state->viewerState->datasetColortableOn) {
-        state->viewerState->datasetAdjustmentTable = state->viewerState->datasetColortable;
-    } else {
-        state->viewerState->datasetAdjustmentTable.resize(256);
-        for (std::size_t i = 0; i < state->viewerState->datasetAdjustmentTable.size(); ++i) {//identity adjustment
-            state->viewerState->datasetAdjustmentTable[i] = std::make_tuple(i, i, i);
-        }
+    for (std::size_t layerId{0}; layerId < Dataset::datasets.size(); ++layerId) {
+        viewerState.layerRenderSettings[layerId].rangeDelta = state->viewerState->luminanceRangeDelta;
+        viewerState.layerRenderSettings[layerId].bias = state->viewerState->luminanceBias;
     }
-    //Apply the dynamic range settings to the adjustment table
-    const uint MAX_COLORVAL{255};
-    if (state->viewerState->luminanceBias > 0 || state->viewerState->luminanceRangeDelta < MAX_COLORVAL) {
-        const auto originalAdjustment = state->viewerState->datasetAdjustmentTable;
-        for (std::size_t i = 0; i < 256; ++i) {
-            int dynIndex = ((i - state->viewerState->luminanceBias) / (state->viewerState->luminanceRangeDelta / static_cast<double>(MAX_COLORVAL)));
-            dynIndex = std::min(static_cast<int>(MAX_COLORVAL), std::max(0, dynIndex));
-            state->viewerState->datasetAdjustmentTable[i] = originalAdjustment[dynIndex];
-        }
-    }
-    state->viewerState->datasetAdjustmentOn = state->viewerState->datasetColortableOn || state->viewerState->luminanceBias > 0 || state->viewerState->luminanceRangeDelta < MAX_COLORVAL;
-
-    reslice_notify(0);// FIXME
+    reslice_notify();
+    emit layerSettingsChanged();
 }
 
 /** Global interfaces  */

viewer.h
@@ -91,9 +91,9 @@ struct GLBuffers {
 struct LayerRenderSettings {
     QString layerName{"unnamed"};
     bool visible{true};
-    float opacity{1.0f};
-    float rangeDelta{1.f};
-    float bias{0.f};
+    double opacity{1.0};
+    double rangeDelta{1.};
+    double bias{0.};
     QOpenGLTexture::Filter textureFilter;
 };
 
@@ -143,13 +143,11 @@ struct ViewerState {
     float outsideMovementAreaFactor{80.f};
 
     // dataset & segmentation rendering options
-    uint luminanceRangeDelta{255};
-    int luminanceBias{0};
+    double luminanceRangeDelta{1.};
+    double luminanceBias{0.};
     bool showOnlyRawData{false};
     std::vector<std::tuple<uint8_t, uint8_t, uint8_t>> datasetColortable;//user LUT
-    std::vector<std::tuple<uint8_t, uint8_t, uint8_t>> datasetAdjustmentTable;//final LUT used during slicing
     bool datasetColortableOn{false};
-    bool datasetAdjustmentOn{false};
     // skeleton rendering options
     float depthCutOff{5.f};
     QFlags<TreeDisplay> skeletonDisplayVP3D{TreeDisplay::ShowIn3DVP};
@@ -214,8 +212,8 @@ private:
 
     void vpGenerateTexture(ViewportArb & vp, const std::size_t layerId);
 
-    void dcSliceExtract(std::uint8_t * datacube, Coordinate cubePosInAbsPx, std::uint8_t * slice, ViewportOrtho & vp, bool useCustomLUT);
-    void dcSliceExtract(std::uint8_t * datacube, floatCoordinate *currentPxInDc_float, std::uint8_t * slice, int s, int *t, const floatCoordinate & v2, bool useCustomLUT, float usedSizeInCubePixels);
+    void dcSliceExtract(std::uint8_t * datacube, Coordinate cubePosInAbsPx, std::uint8_t * slice, ViewportOrtho & vp, std::size_t layerId);
+    void dcSliceExtract(std::uint8_t * datacube, floatCoordinate *currentPxInDc_float, std::uint8_t * slice, int s, int *t, const floatCoordinate & v2, std::size_t layerId, float usedSizeInCubePixels);
 
     void ocSliceExtract(std::uint64_t * datacube, Coordinate cubePosInAbsPx, std::uint8_t * slice, ViewportOrtho & vp);
 

widgets/datasetloadwidget.cpp
@@ -355,6 +355,7 @@ bool DatasetLoadWidget::loadDataset(const boost::optional<bool> loadOverlay, QUr
         state->viewer->setPosition({Dataset::current().boundary / 2});
     }
     state->viewer->applyTextureFilterSetting(state->viewerState->textureFilter);// set filter for all layers
+    state->viewer->datasetColorAdjustmentsChanged();// set range delta and bias for all layers
     state->viewer->updateDatasetMag();// clear vps and notify loader
 
     emit datasetChanged();

widgets/layerdialog.cpp
@@ -233,6 +233,7 @@ LayerDialogWidget::LayerDialogWidget(QWidget *parent) : DialogVisibilityNotify(P
             std::size_t ordered_index = itemModel.ordered_i(currentIndex.row());
             auto& layerSettings = state->viewerState->layerRenderSettings[ordered_index];
             layerSettings.rangeDelta = static_cast<float>(value) / rangeDeltaSlider.maximum();
+            state->viewer->reslice_notify(ordered_index);
         }
     });
 
@@ -242,6 +243,7 @@ LayerDialogWidget::LayerDialogWidget(QWidget *parent) : DialogVisibilityNotify(P
             std::size_t ordered_index = itemModel.ordered_i(currentIndex.row());
             auto& layerSettings = state->viewerState->layerRenderSettings[ordered_index];
             layerSettings.bias = static_cast<float>(value) / biasSlider.maximum();
+            state->viewer->reslice_notify(ordered_index);
         }
     });
 

widgets/preferences/datasetsegmentationtab.cpp
@@ -92,22 +92,22 @@ DatasetAndSegmentationTab::DatasetAndSegmentationTab(QWidget *parent) : QWidget(
     });
     QObject::connect(&useOwnDatasetColorsButton, &QPushButton::clicked, [this](){ useOwnDatasetColorsButtonClicked(); });
     QObject::connect(&biasSlider, &QSlider::valueChanged, [this](const int value) {
-        state->viewerState->luminanceBias = value;
+        state->viewerState->luminanceBias = static_cast<double>(value) / biasSpinBox.maximum();
         biasSpinBox.setValue(value);
         state->viewer->datasetColorAdjustmentsChanged();
     });
     QObject::connect(&biasSpinBox, static_cast<void(QSpinBox::*)(int)>(&QSpinBox::valueChanged), [this](const int value) {
-        state->viewerState->luminanceBias = value;
+        state->viewerState->luminanceBias = static_cast<double>(value) / biasSlider.maximum();
         biasSlider.setValue(value);
         state->viewer->datasetColorAdjustmentsChanged();
     });
     QObject::connect(&rangeDeltaSlider, &QSlider::valueChanged, [this](const int value) {
-        state->viewerState->luminanceRangeDelta = value;
+        state->viewerState->luminanceRangeDelta = static_cast<double>(value) / rangeDeltaSpinBox.maximum();
         rangeDeltaSpinBox.setValue(value);
         state->viewer->datasetColorAdjustmentsChanged();
     });
     QObject::connect(&rangeDeltaSpinBox, static_cast<void(QSpinBox::*)(int)>(&QSpinBox::valueChanged), [this](const int value) {
-        state->viewerState->luminanceRangeDelta = value;
+        state->viewerState->luminanceRangeDelta = static_cast<double>(value) / rangeDeltaSlider.maximum();
         rangeDeltaSlider.setValue(value);
         state->viewer->datasetColorAdjustmentsChanged();
     });

commit df0be8823074d4b9db5ed3565db8effc7cee302f

fix condition to enable data adjustment
it was always on
regression from 4dbee5750d71b968bddb56e72e5db4d7ae3779ae
range delta changed type

viewer.cpp
@@ -275,11 +275,11 @@ void Viewer::dcSliceExtract(std::uint8_t * datacube, Coordinate cubePosInAbsPx, 
     const std::size_t texNext = vp.viewportType == VIEWPORT_ZY ? state->viewerState->texEdgeLength * 4 : 4;// RGBA per pixel
     const std::size_t texNextLine = vp.viewportType == VIEWPORT_ZY ? 4 - 4 * cubeEdgeLen * state->viewerState->texEdgeLength : (state->viewerState->texEdgeLength - cubeEdgeLen) * 4;
 
-    const auto useCustomLUT = state->viewerState->layerRenderSettings[layerId].bias > 0 || state->viewerState->layerRenderSettings[layerId].rangeDelta < std::numeric_limits<uint8_t>::max();
+    const bool isDatasetAdjustment = state->viewerState->datasetColortableOn || state->viewerState->layerRenderSettings[layerId].bias > 0.0 || state->viewerState->layerRenderSettings[layerId].rangeDelta < 1.0;
     for (int yzz = 0; yzz < cubeEdgeLen; ++yzz) {
         for (int xxy = 0; xxy < cubeEdgeLen; ++xxy) {
             uint8_t r, g, b;
-            if (useCustomLUT) {
+            if (isDatasetAdjustment) {
                 std::tie(r, g, b) = datasetAdjustment(layerId, datacube[0]);
             } else {
                 r = g = b = datacube[0];