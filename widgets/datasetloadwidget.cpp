knossos-project/knossos

commit 0fe4652545996bf425e7b877da7f3639f668a623

Add dataset to DatasetLoadWidget after drag&drop

widgets/datasetloadwidget.cpp
@@ -102,6 +102,16 @@ DatasetLoadWidget::DatasetLoadWidget(QWidget *parent) : DialogVisibilityNotify(D
     };
     QObject::connect(this, &DatasetLoadWidget::rejected, []() { resetSettings(); });
     QObject::connect(&cancelButton, &QPushButton::clicked, [this]() { resetSettings(); hide(); });
+
+    QObject::connect(&state->viewer->mainWindow, &MainWindow::datasetDropped, [this](const QUrl & url) {
+        const auto existingEntries = tableWidget.findItems(url.url(), Qt::MatchFlag::MatchExactly);
+        if (existingEntries.size() == 0) {
+            insertDatasetRow(url.url(), tableWidget.rowCount() - 1);
+        } else {
+            tableWidget.selectRow(existingEntries.front()->row());
+        }
+        loadDataset(boost::none, url);
+    });
     resize(600, 600);//random default size, will be overriden by settings if present
 }
 
@@ -143,6 +153,7 @@ void DatasetLoadWidget::insertDatasetRow(const QString & dataset, const int row)
     tableWidget.setItem(row, 0, new QTableWidgetItem(dataset));
     tableWidget.setCellWidget(row, 1, addDatasetButton);
     tableWidget.setCellWidget(row, 2, removeDatasetButton);
+    tableWidget.selectRow(row);
 }
 
 void DatasetLoadWidget::datasetCellChanged(int row, int col) {

widgets/mainwindow.cpp
@@ -1308,7 +1308,7 @@ void MainWindow::dropEvent(QDropEvent *event) {
     for (auto && url : event->mimeData()->urls()) {
         const auto localPath = url.toLocalFile();
         if (localPath.endsWith(".conf")) {
-            widgetContainer.datasetLoadWidget.loadDataset(boost::none, url);
+            emit datasetDropped(url);
         } else {
             files.append(localPath);
         }

widgets/mainwindow.h
@@ -213,7 +213,7 @@ public:
     void loadSettings();
     void clearSettings();
 
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     template<typename Function> void forEachVPDo(Function func) {
         for (auto * vp : { static_cast<ViewportBase *>(viewportXY.get()), static_cast<ViewportBase *>(viewportXZ.get()), static_cast<ViewportBase *>(viewportZY.get()), static_cast<ViewportBase *>(viewportArb.get()), static_cast<ViewportBase *>(viewport3D.get()) }) {
             func(*vp);
@@ -241,6 +241,7 @@ public:
 
 signals:
     void aboutToSave();
+    void datasetDropped(const QUrl &);
     void overlayOpacityChanged();
 public slots:
     void refreshPluginMenu();

commit 2bb20053915fbda8db4a056123437bc1ec5e77aa

fix dataset selection on K startup
regression from 0fe4652545996bf425e7b877da7f3639f668a623

widgets/datasetloadwidget.cpp
@@ -107,6 +107,7 @@ DatasetLoadWidget::DatasetLoadWidget(QWidget *parent) : DialogVisibilityNotify(D
         const auto existingEntries = tableWidget.findItems(url.url(), Qt::MatchFlag::MatchExactly);
         if (existingEntries.size() == 0) {
             insertDatasetRow(url.url(), tableWidget.rowCount() - 1);
+            tableWidget.selectRow(tableWidget.rowCount() - 2);
         } else {
             tableWidget.selectRow(existingEntries.front()->row());
         }
@@ -153,7 +154,6 @@ void DatasetLoadWidget::insertDatasetRow(const QString & dataset, const int row)
     tableWidget.setItem(row, 0, new QTableWidgetItem(dataset));
     tableWidget.setCellWidget(row, 1, addDatasetButton);
     tableWidget.setCellWidget(row, 2, removeDatasetButton);
-    tableWidget.selectRow(row);
 }
 
 void DatasetLoadWidget::datasetCellChanged(int row, int col) {