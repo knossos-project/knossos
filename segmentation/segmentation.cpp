knossos-project/knossos

commit e8a902d22b9a942dd3908d2eba6f2205fc26d9c0

update selection speed workaround for proxy models
deltaBlockSelection was basically ineffective because mapSelectionFromSource was splitting it up into thousands of peaces again
regressions from 644dc7ae2b5f0542e8d6d2d744dcf96677c862e6 (segmentation) and bf34cf72916f48a0be6d1f1f63540d502e451108 (skeleton)

it was impossible to do operations on large amounts of elements (nodes, objects)

widgets/tools/segmentationview.cpp
@@ -554,7 +554,8 @@ void SegmentationView::selectionChanged(const QItemSelection & selected, const Q
 }
 
 void SegmentationView::updateTouchedObjSelection() {
-    const auto & selectedItems = blockSelection(touchedObjectModel, touchedObjectModel.objectCache);
+    const auto dataFromIndex = [&data = touchedObjectModel.objectCache](auto index){ return data[index]; };
+    const auto & selectedItems = deltaBlockSelection(touchedObjectModel, dataFromIndex);
 
     touchedObjectSelectionProtection = true;//using block signals prevents update of the tableview
     touchedObjsTable.selectionModel()->select(selectedItems, QItemSelectionModel::ClearAndSelect);
@@ -562,15 +563,16 @@ void SegmentationView::updateTouchedObjSelection() {
 }
 
 void SegmentationView::updateSelection() {
-    const auto & selectedItems = blockSelection(objectModel, Segmentation::singleton().objects);
-    const auto & proxySelection = objectProxyModelComment.mapSelectionFromSource(objectProxyModelCategory.mapSelectionFromSource(selectedItems));
+    const auto & proxySelection = deltaBlockSelection(objectProxyModelComment, [&](const auto rowIndex){
+        return Segmentation::singleton().objects[objectProxyModelCategory.mapToSource(objectProxyModelComment.mapToSource(objectProxyModelComment.index(rowIndex, 0))).row()];
+    });
 
     objectSelectionProtection = true;//using block signals prevents update of the tableview
     objectsTable.selectionModel()->select(proxySelection, QItemSelectionModel::ClearAndSelect);
     objectSelectionProtection = false;
 
-    if (!selectedItems.indexes().isEmpty()) {// scroll to first selected entry
-        objectsTable.scrollTo(proxySelection.indexes().front());
+    if (const auto indexes = proxySelection.indexes(); !indexes.isEmpty()) {// scroll to first selected entry
+        objectsTable.scrollTo(indexes.front());
     }
 }
 

widgets/tools/model_helper.h
@@ -37,15 +37,15 @@ template<typename Elem>
 Elem & getElem(const std::reference_wrapper<Elem> & elem) {
     return elem.get();
 }
-
-auto deltaBlockSelection = [](const auto & model, const auto & data, const auto isAlreadySelected, const bool inverter = false){
+template<typename M, typename D, typename S>
+auto deltaBlockSelection(const M & model, const D dataFromIndex, const S isAlreadySelected, const bool inverter = false){
     QItemSelection selectedItems;
 
     bool blockSelection{false};
-    std::size_t blockStartIndex{0};
+    int blockStartIndex{0};
 
-    std::size_t rowIndex{0};
-    for (auto & elem : data) {
+    for (int rowIndex{0}; rowIndex < model.rowCount(); ++rowIndex) {
+        const auto & elem = dataFromIndex(rowIndex);
         const auto alreadySelected = isAlreadySelected(rowIndex) ? !inverter : inverter;
         const auto selected = getElem(elem).selected ? !inverter : inverter;
         if (!blockSelection && selected && !alreadySelected) {// start block selection
@@ -56,17 +56,20 @@ auto deltaBlockSelection = [](const auto & model, const auto & data, const auto 
             selectedItems.select(model.index(blockStartIndex, 0), model.index(rowIndex-1, model.columnCount()-1));
             blockSelection = false;
         }
-        ++rowIndex;
     }
     // finish last blockselection – if any
     if (blockSelection) {
-        selectedItems.select(model.index(blockStartIndex, 0), model.index(rowIndex-1, model.columnCount()-1));
+        selectedItems.select(model.index(blockStartIndex, 0), model.index(model.rowCount()-1, model.columnCount()-1));
     }
 
     return selectedItems;
-};
+}
+template<typename M, typename D>
+auto deltaBlockSelection(const M & model, const D dataFromIndex){
+    return deltaBlockSelection(model, dataFromIndex, [](int){return false;});
+}
 auto blockSelection = [](const auto & model, const auto & data){
-    return deltaBlockSelection(model, data, [](int){return false;});
+    return deltaBlockSelection(model, [&data](auto index){return data[index];}, [](int){return false;});
 };
 
 auto threeWaySorting = [](auto & table, auto & sortIndex){// emulate ability for the user to disable sorting

widgets/tools/skeletonview.cpp
@@ -282,11 +282,12 @@ auto selectElems(View & view, Model & model, Proxy & proxy) {
 
 template<typename Model, typename Proxy>
 auto updateSelection(QTreeView & view, Model & model, Proxy & proxy) {
-    auto isSelectedFunc = [&model, &proxy, &selectionModel = *view.selectionModel()](const int rowIndex){
-        return selectionModel.isSelected(proxy.mapFromSource(model.index(rowIndex, 0)));
+    const auto isSelectedFunc = [&proxy, &view](const int rowIndex){
+        return view.selectionModel()->isSelected(proxy.index(rowIndex, 0));
     };
-    const auto selection = deltaBlockSelection(model, model.cache, isSelectedFunc);
-    const auto selectedIndices = proxy.mapSelectionFromSource(selection);
+    const auto selectedIndices = deltaBlockSelection(proxy, [&model, &proxy](const auto rowIndex){
+        return model.cache[proxy.mapToSource(proxy.index(rowIndex, 0)).row()];
+    }, isSelectedFunc);
 
     model.selectionProtection = true;
     if (!selectedIndices.isEmpty()) {// selecting an empty index range apparently clears

widgets/tools/skeletonview.h
@@ -53,7 +53,7 @@ public:
     virtual int columnCount(const QModelIndex & parent = QModelIndex()) const override;
     virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
     virtual Qt::ItemFlags flags(const QModelIndex & index) const override;
-    virtual int rowCount(const QModelIndex &) const override;
+    virtual int rowCount(const QModelIndex & = QModelIndex{}) const override;
 };
 
 class TreeModel : public AbstractSkeletonModel<TreeModel> {

commit fe4916a39584eb59bbc49bb51887c4e3d5358039

fix crash on clear segmentation when objects were selected
a table view reset also resets the selection model (and doesn’t emit signals)
segmentation should never get invalid indices passed and therefore doesn’t need a range check
regression from e8a902d22b9a942dd3908d2eba6f2205fc26d9c0

segmentation/segmentation.cpp
@@ -394,9 +394,7 @@ void Segmentation::selectObject(Object & object) {
 }
 
 void Segmentation::unselectObject(const uint64_t & objectIndex) {
-    if (objectIndex < objects.size()) {
-        unselectObject(objects[objectIndex]);
-    }
+    unselectObject(objects[objectIndex]);
 }
 
 void Segmentation::unselectObject(Object & object) {

widgets/tools/segmentationview.cpp
@@ -385,9 +385,7 @@ SegmentationView::SegmentationView(QWidget * const parent) : QWidget(parent), ca
         }
     });
     QObject::connect(&Segmentation::singleton(), &Segmentation::resetData, [this](){
-        touchedObjsTable.clearSelection();
         touchedObjectModel.recreate();
-        objectsTable.clearSelection();
         objectModel.recreate();
         updateSelection();
         updateTouchedObjSelection();