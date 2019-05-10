1c79123d652d4240f7dca03e6a21d57a6e7053fd
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2019-05-10 18:21:11 +0200

    upload new pythonqt build

--- ci/build_macos.sh
+++ ci/build_macos.sh
@@ -18,30 +18,10 @@ a
 w
 EOF
 
-# Download and install PythonQt
-time curl -JLO https://github.com/knossos-project/knossos/releases/download/nightly-dev/macOS-PythonQt.zip
-time unzip -d / macOS-PythonQt.zip
+# Build PythonQt
+time git clone https://github.com/knossos-project/PythonQt.git
+mkdir PythonQt-build && cd PythonQt-build
+time cmake -G Ninja ../PythonQt -DCMAKE_PREFIX_PATH=/usr/local/opt/qt -DPYTHON_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/../python2.7
+time ninja install
 
-# Fix QuaZip include directory name
-cd $TRAVIS_BUILD_DIR && cd ..
-mkdir -p quazip/quazip5
-QUAZIP_VERSION=`brew list --versions quazip | cut -d " " -f2`
-time cp -R /usr/local/Cellar/quazip/${QUAZIP_VERSION}/include/quazip/* quazip/quazip5/
-
-# Build KNOSSOS
-mkdir knossos-build && cd knossos-build
-time cmake -G Ninja ../knossos -DCMAKE_PREFIX_PATH=/usr/local/opt/qt -DCMAKE_CXX_FLAGS=-isystem\ ${TRAVIS_BUILD_DIR}/../quazip
-time ninja
-
-# OS X housekeeping
-mv knossos.app KNOSSOS.app
-time /usr/local/opt/qt/bin/macdeployqt KNOSSOS.app
-cp /usr/local/Cellar/quazip/${QUAZIP_VERSION}/lib/libquazip.1.0.0.dylib KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib
-install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore
-
-# Deployment
-BRANCH_PREFIX=""
-if [[ $TRAVIS_BRANCH != "master" ]]; then
-	BRANCH_PREFIX=${TRAVIS_BRANCH}-
-fi
-time zip -r ${TRAVIS_BUILD_DIR}/macos.${BRANCH_PREFIX}KNOSSOS.nightly.app.zip KNOSSOS.app
+time zip -r ${TRAVIS_BUILD_DIR}/macOS-PythonQt.zip /usr/local/lib/libQt5Python27.a /usr/local/lib/libQt5Python27_QtAll.a /usr/local/include/Qt5Python27 /usr/local/lib/cmake/Qt5Python27 /usr/local/lib/cmake/Qt5Python27_QtAll
