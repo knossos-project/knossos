knossos-project/knossos

commit 140bcdc767179452e26ec499db8395751258f3a8

pass vp normal directly to remote
rotation is inferred from the presence of the arg

remote.cpp
@@ -40,13 +40,15 @@ Remote::Remote() {
     QObject::connect(&timer, &QTimer::timeout, [this](){remoteWalk();});
 }
 
-void Remote::process(const Coordinate & pos) {
+void Remote::process(const Coordinate & pos, boost::optional<floatCoordinate> normal) {
     //distance vector
     floatCoordinate deltaPos = pos - state->viewerState->currentPosition;
     const float jumpThreshold = 0.5f * state->cubeEdgeLength * state->M * state->magnification;//approximately inside sc
     if (deltaPos.length() > jumpThreshold) {
         state->viewer->setPosition(pos);
     } else if (pos != state->viewerState->currentPosition) {
+        rotate = normal.is_initialized();
+        normal = normal.get_value_or({});
         targetPos = pos;
         recenteringOffset = pos - state->viewerState->currentPosition;
         elapsed.restart();
@@ -130,7 +132,6 @@ void Remote::remoteWalk() {
 
             const auto target = state->viewerState->currentPosition + recenteringOffset;
             floatCoordinate delta = target - avg;
-            const auto normal = state->viewer->window->viewportOrtho(activeVP)->n;
             quaternion = QQuaternion::rotationTo({normal.x, normal.y, normal.z}, {delta.x, delta.y, delta.z});
         }
     }

remote.h
@@ -24,28 +24,30 @@
 #define REMOTE_H
 
 #include "coordinate.h"
-#include "widgets/viewport.h"
+
 #include <QElapsedTimer>
 #include <QTimer>
+
+#include <boost/optional.hpp>
 
 #include <deque>
 
 class Remote {
-public:
     Coordinate targetPos;
     floatCoordinate recenteringOffset;
     bool rotate{false};
-    ViewportType activeVP{VIEWPORT_UNDEFINED};
+    floatCoordinate normal;
     QTimer timer;
     QElapsedTimer elapsed;
     static const qint64 ms;
     static const float goodEnough;
 
-    Remote();
-
-    void process(const Coordinate & pos);
     std::deque<floatCoordinate> getLastNodes();
     void remoteWalk();
+
+public:
+    Remote();
+    void process(const Coordinate & pos, boost::optional<floatCoordinate> normal = boost::none);
 };
 
 #endif // REMOTE_H

viewer.cpp
@@ -936,14 +936,11 @@ void Viewer::setPosition(const floatCoordinate & pos, UserMoveType userMoveType,
 }
 
 void Viewer::setPositionWithRecentering(const Coordinate &pos) {
-    remote.rotate = false;
     remote.process(pos);
 }
 
-void Viewer::setPositionWithRecenteringAndRotation(const Coordinate &pos, const ViewportType vpType) {
-    remote.rotate = true;
-    remote.activeVP = vpType;
-    remote.process(pos);
+void Viewer::setPositionWithRecenteringAndRotation(const Coordinate &pos, const floatCoordinate normal) {
+    remote.process(pos, normal);
 }
 
 void Viewer::userMoveVoxels(const Coordinate & step, UserMoveType userMoveType, const Coordinate & viewportNormal) {

viewer.h
@@ -225,7 +225,7 @@ public slots:
     bool updateDatasetMag(const uint mag = 0);
     void setPosition(const floatCoordinate & pos, UserMoveType userMoveType = USERMOVE_NEUTRAL, const Coordinate & viewportNormal = {0, 0, 0});
     void setPositionWithRecentering(const Coordinate &pos);
-    void setPositionWithRecenteringAndRotation(const Coordinate &pos, const ViewportType vpType);
+    void setPositionWithRecenteringAndRotation(const Coordinate &pos, const floatCoordinate normal);
     void userMoveVoxels(const Coordinate &step, UserMoveType userMoveType, const Coordinate & viewportNormal);
     void userMove(const floatCoordinate & floatStep, UserMoveType userMoveType = USERMOVE_NEUTRAL, const Coordinate & viewportNormal = {0, 0, 0});
     void userMoveRound(UserMoveType userMoveType = USERMOVE_NEUTRAL, const Coordinate & viewportNormal = {0, 0, 0});

widgets/viewportevents.cpp
@@ -251,7 +251,7 @@ void ViewportOrtho::handleMouseButtonRight(const QMouseEvent *event) {
         // Move to the new node position
         if (state->viewerState->autoTracingMode != Recentering::Off) {
             if (viewportType == VIEWPORT_ARBITRARY) {
-                state->viewer->setPositionWithRecenteringAndRotation(clickedCoordinate, viewportType);
+                state->viewer->setPositionWithRecenteringAndRotation(clickedCoordinate, n);
             } else {
                 state->viewer->setPositionWithRecentering(clickedCoordinate);
             }

commit e37b39fd9a4ff7f720aafd76c8b8d8130721d1bf

fix arb vp rotation
broken since 140bcdc767179452e26ec499db8395751258f3a8

remote.cpp
@@ -42,15 +42,14 @@ Remote::Remote() {
     QObject::connect(&timer, &QTimer::timeout, [this](){remoteWalk();});
 }
 
-void Remote::process(const Coordinate & pos, boost::optional<floatCoordinate> normal) {
+void Remote::process(const Coordinate & pos, bool rotate) {
     //distance vector
     floatCoordinate deltaPos = pos - state->viewerState->currentPosition;
     const float jumpThreshold = 0.5f * Dataset::current().cubeEdgeLength * state->M * Dataset::current().magnification;//approximately inside sc
     if (deltaPos.length() > jumpThreshold) {
         state->viewer->setPosition(pos);
     } else if (pos != state->viewerState->currentPosition) {
-        rotate = normal.is_initialized();
-        normal = normal.get_value_or({});
+        this->rotate = rotate;
         targetPos = pos;
         recenteringOffset = pos - state->viewerState->currentPosition;
         elapsed.restart();
@@ -134,6 +133,7 @@ void Remote::remoteWalk() {
 
             const auto target = state->viewerState->currentPosition + recenteringOffset;
             floatCoordinate delta = target - avg;
+            const auto normal = state->viewer->window->viewportArb->n;
             quaternion = QQuaternion::rotationTo({normal.x, normal.y, normal.z}, {delta.x, delta.y, delta.z});
         }
     }

remote.h
@@ -36,7 +36,6 @@ class Remote {
     Coordinate targetPos;
     floatCoordinate recenteringOffset;
     bool rotate{false};
-    floatCoordinate normal;
     QTimer timer;
     QElapsedTimer elapsed;
     static const qint64 ms;
@@ -47,7 +46,7 @@ class Remote {
 
 public:
     Remote();
-    void process(const Coordinate & pos, boost::optional<floatCoordinate> normal = boost::none);
+    void process(const Coordinate & pos, bool rotate = false);
 };
 
 #endif // REMOTE_H

viewer.cpp
@@ -974,8 +974,8 @@ void Viewer::setPositionWithRecentering(const Coordinate &pos) {
     remote.process(pos);
 }
 
-void Viewer::setPositionWithRecenteringAndRotation(const Coordinate &pos, const floatCoordinate normal) {
-    remote.process(pos, normal);
+void Viewer::setPositionWithRecenteringAndRotation(const Coordinate &pos) {
+    remote.process(pos, true);
 }
 
 void Viewer::userMoveVoxels(const Coordinate & step, UserMoveType userMoveType, const floatCoordinate & viewportNormal) {

viewer.h
@@ -265,7 +265,7 @@ public slots:
     bool updateDatasetMag(const int mag = 0);
     void setPosition(const floatCoordinate & pos, UserMoveType userMoveType = USERMOVE_NEUTRAL, const Coordinate & viewportNormal = {0, 0, 0});
     void setPositionWithRecentering(const Coordinate &pos);
-    void setPositionWithRecenteringAndRotation(const Coordinate &pos, const floatCoordinate normal);
+    void setPositionWithRecenteringAndRotation(const Coordinate &pos);
     void userMoveVoxels(const Coordinate &step, UserMoveType userMoveType, const floatCoordinate & viewportNormal);
     void userMove(const floatCoordinate & floatStep, UserMoveType userMoveType = USERMOVE_NEUTRAL, const floatCoordinate & viewportNormal = {0, 0, 0});
     void userMoveRound(UserMoveType userMoveType = USERMOVE_NEUTRAL, const floatCoordinate & viewportNormal = {0, 0, 0});

widgets/viewports/viewportevents.cpp
@@ -257,7 +257,7 @@ void ViewportOrtho::handleMouseButtonRight(const QMouseEvent *event) {
         // Move to the new node position
         if (state->viewerState->autoTracingMode != Recentering::Off) {
             if (viewportType == VIEWPORT_ARBITRARY) {
-                state->viewer->setPositionWithRecenteringAndRotation(clickedCoordinate, n);
+                state->viewer->setPositionWithRecenteringAndRotation(clickedCoordinate);
             } else {
                 state->viewer->setPositionWithRecentering(clickedCoordinate);
             }