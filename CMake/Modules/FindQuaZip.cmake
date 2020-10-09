f3bfcd80dc582248a59b857609a796d133bb1fa5
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2020-10-09 22:44:45 +0200

    accomodate QuaZip 1.0

--- CMake/Modules/FindQuaZip.cmake
+++ CMake/Modules/FindQuaZip.cmake
@@ -22,28 +22,31 @@
 
 # provides an imported target for the quazip library
 
-find_package(ZLIB REQUIRED)
+find_package(QuaZip-Qt5)
 
-find_library(QUAZIP_LIBRARY NAMES quazip quazip5 PATH_SUFFIXES "QuaZip")
-find_path(QUAZIP_INCLUDE_DIR quazip.h PATH_SUFFIXES quazip quazip5)
+if (NOT TARGET QuaZip::QuaZip)
+    find_package(ZLIB REQUIRED)
+    find_library(QUAZIP_LIBRARY NAMES quazip quazip5 PATH_SUFFIXES "QuaZip")
+    find_path(QUAZIP_INCLUDE_DIR quazip.h PATH_SUFFIXES quazip quazip5)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(QuaZip
-    REQUIRED_VARS QUAZIP_LIBRARY QUAZIP_INCLUDE_DIR
-)
-
-if(QUAZIP_FOUND)
-    add_library(QuaZip::QuaZip UNKNOWN IMPORTED)
-    set_target_properties(QuaZip::QuaZip PROPERTIES
-        IMPORTED_LOCATION ${QUAZIP_LIBRARY}
-        # workaround conflicting crypt.h header
-        # use #include <quazip5/*>
-#        INTERFACE_INCLUDE_DIRECTORIES ${QUAZIP_INCLUDE_DIR}
-        INTERFACE_LINK_LIBRARIES ZLIB::ZLIB
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(QuaZip
+        REQUIRED_VARS QUAZIP_LIBRARY QUAZIP_INCLUDE_DIR
     )
-    if(NOT ${QUAZIP_LIBRARY} MATCHES ".so" AND NOT ${QUAZIP_LIBRARY} MATCHES ".dll")
-    set_target_properties(QuaZip::QuaZip PROPERTIES
-        INTERFACE_COMPILE_DEFINITIONS "QUAZIP_STATIC"
-    )
+
+    if (QUAZIP_FOUND)
+        add_library(QuaZip::QuaZip UNKNOWN IMPORTED)
+        set_target_properties(QuaZip::QuaZip PROPERTIES
+            IMPORTED_LOCATION ${QUAZIP_LIBRARY}
+            # workaround conflicting crypt.h header
+            # use #include <quazip5/*>
+            INTERFACE_INCLUDE_DIRECTORIES ${QUAZIP_INCLUDE_DIR}
+            INTERFACE_LINK_LIBRARIES ZLIB::ZLIB
+        )
+        if(NOT ${QUAZIP_LIBRARY} MATCHES ".so" AND NOT ${QUAZIP_LIBRARY} MATCHES ".dll")
+        set_target_properties(QuaZip::QuaZip PROPERTIES
+            INTERFACE_COMPILE_DEFINITIONS "QUAZIP_STATIC"
+        )
+        endif()
     endif()
-endif(QUAZIP_FOUND)
+endif()
