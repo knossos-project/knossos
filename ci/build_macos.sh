66c89c048c4af949fc6f82dcab8e41ba9e7a1fee
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2020-11-23 22:10:39 +0100

    determine python version

--- ci/build_macos.sh
+++ ci/build_macos.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
+PYTHON_VERSION=$(brew list --versions python | cut -d " " -f2)
+PYTHON_MAJOR=$(echo ${PYTHON_VERSION} | cut -d '.' -f1)
+PYTHON_MINOR=$(echo ${PYTHON_VERSION} | cut -d '.' -f2)
+
 # Build PythonQt
 time git clone --single-branch --branch new https://github.com/knossos-project/PythonQt.git
 mkdir PythonQt-build && cd PythonQt-build
@@ -16,14 +20,14 @@ QUAZIP_VERSION=$(brew list --versions quazip | cut -d " " -f2)
 cd $TRAVIS_BUILD_DIR && cd ..
 # Build KNOSSOS
 mkdir knossos-build && cd knossos-build
-time cmake -G Ninja ../knossos -DCMAKE_PREFIX_PATH=/usr/local/opt/qt
+time cmake -G Ninja ../knossos -Dpythonqt=Qt5Python${PYTHON_MAJOR}${PYTHON_MINOR} -DCMAKE_PREFIX_PATH=/usr/local/opt/qt
 time ninja
 
 # OS X housekeeping
 mv knossos.app KNOSSOS.app
 time /usr/local/opt/qt/bin/macdeployqt KNOSSOS.app
 cp -v /usr/local/Cellar/quazip/${QUAZIP_VERSION}/lib/libquazip.1.0.0.dylib KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib
-cp -v /usr/local/Frameworks/Python.framework/Versions/3.9/lib/libpython3.9.dylib KNOSSOS.app/Contents/Frameworks/
+cp -v /usr/local/Frameworks/Python.framework/Versions/${PYTHON_MAJOR}.${PYTHON_MINOR}/lib/libpython${PYTHON_MAJOR}.${PYTHON_MINOR}.dylib KNOSSOS.app/Contents/Frameworks/
 install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore
 
 # Deployment
