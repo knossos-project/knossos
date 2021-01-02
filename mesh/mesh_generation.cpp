3297f9b2344efe9ab64925dd961efdd1b9f35326
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2021-01-02 21:40:47 +0100

    try3

--- mesh/mesh_generation.cpp
+++ mesh/mesh_generation.cpp
@@ -177,7 +177,7 @@ auto generateMeshForSubobjectID(const std::unordered_map<std::uint64_t, std::uin
 
         marchingCubes(obj2points, obj2faces, obj2idCounter, extractedCubeForCoord(pair.first), soid2oid, origin, dims, spacing, extent);
         for (std::size_t i = 1; i < 6; i+=2) {
-            const std::array<double, 3> dims{{i < 2 ? 2.0 : cubeEdgeLen + 1, i % 4 < 2 ? cubeEdgeLen + 1 : 2.0, i < 4 ? cubeEdgeLen + 1 : 2.0}};
+            const std::array<double, 3> dims{{i < 2 ? 2.0 : cubeEdgeLen, i % 4 < 2 ? cubeEdgeLen + (i==1) : 2.0, i < 4 ? cubeEdgeLen + 1 : 2.0}};
             const floatCoordinate unscaledOrigin(inMagCoord + floatCoordinate(i == 1 ? 127 : 0, i == 3 ? 127 : 0, i == 5 ? 127 : 0));
             const std::array<double, 6> extent{{0, dims[0], 0, dims[1], 0, dims[2]}};
 
