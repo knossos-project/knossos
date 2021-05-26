80f65872b79418d99649835ea00dc259699f5b1c
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2021-05-27 01:24:23 +0200

    Revert "manual homebrew"
    This reverts commit 7c6266fe62d4c3eccc7b3b2e7ff96bc4c06c2954.

--- ci/build_macos.sh
+++ ci/build_macos.sh
@@ -10,22 +10,22 @@ QUAZIP_VERSION=$(brew list --versions quazip | tr " " "\n" | tail -1)
 cd $TRAVIS_BUILD_DIR && cd ..
 # Build KNOSSOS
 mkdir knossos-build && cd knossos-build
-time cmake -G Ninja ../knossos -Dpythonqt=Qt5Python${PYTHON_MAJOR}${PYTHON_MINOR} -DCMAKE_PREFIX_PATH=~/homebrew/opt/qt@5/
+time cmake -G Ninja ../knossos -Dpythonqt=Qt5Python${PYTHON_MAJOR}${PYTHON_MINOR} -DCMAKE_PREFIX_PATH=/usr/local/opt/qt@5/
 time ninja
 
 # OS X housekeeping
 mv knossos.app KNOSSOS.app
-time ~/homebrew/opt/qt@5/bin/macdeployqt KNOSSOS.app
+time /usr/local/opt/qt@5/bin/macdeployqt KNOSSOS.app
 cp -v /usr/local/Cellar/quazip/${QUAZIP_VERSION}/lib/libquazip.1.0.0.dylib KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib &&
-  install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib -change ~/homebrew/opt/qt@5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore || 
+  install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib -change /usr/local/opt/qt@5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore || 
   true
 cp -v /usr/local/lib/libquazip1-qt5.${QUAZIP_VERSION}.dylib KNOSSOS.app/Contents/Frameworks/libquazip1-qt5.1.dylib &&
-  install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip1-qt5.1.dylib -change ~/homebrew/opt/qt@5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ||
+  install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip1-qt5.1.dylib -change /usr/local/opt/qt@5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ||
   true
 
 cp -r /usr/local/Frameworks/Python.framework KNOSSOS.app/Contents/Frameworks/
 rm -rf KNOSSOS.app/Contents/Frameworks/Python.framework/Versions/2.7
-install_name_tool KNOSSOS.app/Contents/MacOS/knossos -change ~/homebrew/opt/python@${PYTHON_MAJOR}.${PYTHON_MINOR}/Frameworks/Python.framework/Versions/${PYTHON_MAJOR}.${PYTHON_MINOR}/Python @executable_path/../Frameworks/Python.framework/Versions/${PYTHON_MAJOR}.${PYTHON_MINOR}/Python
+install_name_tool KNOSSOS.app/Contents/MacOS/knossos -change /usr/local/opt/python@${PYTHON_MAJOR}.${PYTHON_MINOR}/Frameworks/Python.framework/Versions/${PYTHON_MAJOR}.${PYTHON_MINOR}/Python @executable_path/../Frameworks/Python.framework/Versions/${PYTHON_MAJOR}.${PYTHON_MINOR}/Python
 
 # Deployment
 time zip -r ${TRAVIS_BUILD_DIR}/macos.KNOSSOS.nightly.app.zip KNOSSOS.app
