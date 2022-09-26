2b04cccc06e3fd79a2c4202212714ee854c9660b
My-Tien Nguyen <my-tien.nguyen@ariadne.ai>
2022-09-26 15:48:14 +0200

    Segmentation Cell Painting Cheatsheet

--- widgets/cheatsheet.cpp
+++ widgets/cheatsheet.cpp
@@ -14,6 +14,7 @@ void Cheatsheet::load(const AnnotationMode mode) {
                    (mode == AnnotationMode::Mode_MergeTracing) ? "mergetracingmode" :
                    (mode == AnnotationMode::Mode_Paint) ? "paintmode" :
                    (mode == AnnotationMode::Mode_OverPaint) ? "overpaintmode" :
+                   (mode == AnnotationMode::Mode_CellSegmentation) ? "segmentationcellpaintingmode" :
                    (mode == AnnotationMode::Mode_Tracing) ? "tracingmode" :
                    (mode == AnnotationMode::Mode_TracingAdvanced) ? "tracingadvancedmode" :
                    /*mode == AnnotationMode::Mode_Selection:*/ "reviewmode"));
