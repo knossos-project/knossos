78ef760f8771702c0c4eea5154c6718092fdacad
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2023-02-01 04:19:47 +0100

    *wip 2d*

--- widgets/layerdialog.cpp
+++ widgets/layerdialog.cpp
@@ -58,7 +58,7 @@ QVariant LayerItemModel::data(const QModelIndex &index, int role) const {
             case 2: return data.compressionString();
             case 3: return data.apiString();
             case 4: return data.magnification;
-            case 5: return data.cubeEdgeLength;
+            case 5: return QString{"%1, %2, %3"}.arg(data.cubeShape.x).arg(data.cubeShape.y).arg(data.cubeShape.z);
             case 6: return data.experimentname;
             case 7: return data.description;
             case 8: return layerSettings.color;
@@ -231,7 +231,7 @@ LayerDialogWidget::LayerDialogWidget(QWidget *parent) : DialogVisibilityNotify(P
                     ++state->viewerState->layerOrder[i];
                 }
             }
-            state->viewer->resizeTexEdgeLength(Dataset::current().cubeEdgeLength, state->M, Dataset::datasets.size());// update layerRenderSettings and textures
+            state->viewer->resizeTexEdgeLength(Dataset::current().cubeShape.x, state->M, Dataset::datasets.size());// update layerRenderSettings and textures//FIXME
             if (Segmentation::singleton().layerId >= layeri) {
                 ++Segmentation::singleton().layerId;
             }
@@ -252,7 +252,7 @@ LayerDialogWidget::LayerDialogWidget(QWidget *parent) : DialogVisibilityNotify(P
                     --state->viewerState->layerOrder[i];
                 }
             }
-            state->viewer->resizeTexEdgeLength(Dataset::current().cubeEdgeLength, state->M, Dataset::datasets.size());// update layerRenderSettings and textures
+            state->viewer->resizeTexEdgeLength(Dataset::current().cubeShape.x, state->M, Dataset::datasets.size());// update layerRenderSettings and textures//FIXME
             if (Segmentation::singleton().layerId >= layeri) {
                 --Segmentation::singleton().layerId;
             }
@@ -428,7 +428,7 @@ void LayerDialogWidget::updateLayerProperties() {
         combineSlicesXyOnlyCheck.setEnabled(layerSettings.combineSlicesEnabled);
 
         combineSlicesType.setCurrentIndex(layerSettings.combineSlicesType);
-        combineSlicesSpin.setRange(0, Dataset::datasets[ordered_index].cubeEdgeLength * 0.5 * (state->M - 1));
+        combineSlicesSpin.setRange(0, Dataset::datasets[ordered_index].cubeShape.x * 0.5 * (state->M - 1));//FIXME
         combineSlicesSpin.setValue(layerSettings.combineSlices);
         combineSlicesXyOnlyCheck.setChecked(layerSettings.combineSlicesXyOnly);
     }
