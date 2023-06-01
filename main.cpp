knossos-project/knossos

commit 869f20c27529c0255e8e6786ebb7cb8f5a1a7dc8

register debug output handler earlier

main.cpp
@@ -111,6 +111,7 @@ int main(int argc, char *argv[]) try {
 #ifdef _GLIBCXX_DEBUG_PEDANTIC
     std::cerr << "_GLIBCXX_DEBUG_PEDANTIC set" << std::endl;
 #endif
+    qInstallMessageHandler(debugMessageHandler);
     QtConcurrent::run([](){ QSslSocket::supportsSsl(); });// workaround until https://bugreports.qt.io/browse/QTBUG-59750
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);// explicitly enable sharing for undocked viewports
 
@@ -150,7 +151,6 @@ int main(int argc, char *argv[]) try {
         QApplication::setStyle(QStyleFactory::create("Fusion"));
     }
 #endif
-    qInstallMessageHandler(debugMessageHandler);
 #ifdef NDEBUG
     Splash splash(qApp->primaryScreen()->devicePixelRatio() == 1.0 ? ":/resources/splash.png" : ":/resources/splash@2x.png");
 #endif

commit 3b7a0b0e53b715cfa7da2b5f72309e74124f989f

fix log file location (regression from 869f20c2)

main.cpp
@@ -144,7 +144,15 @@ int main(int argc, char *argv[]) try {
 #ifdef _GLIBCXX_DEBUG_PEDANTIC
     std::cerr << "_GLIBCXX_DEBUG_PEDANTIC set" << std::endl;
 #endif
-    qInstallMessageHandler(debugMessageHandler);
+    QCoreApplication::setOrganizationDomain("knossos.app");
+    QCoreApplication::setOrganizationName("MPIN");
+    QCoreApplication::setApplicationName("KNOSSOS");
+    QCoreApplication::setApplicationVersion(KREVISION);
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+
+    QFile::copy(QSettings{QSettings::IniFormat, QSettings::UserScope, "MPIN", "KNOSSOS 5.0"}.fileName(), QSettings{}.fileName());// doesn’t overwrite
+
+    qInstallMessageHandler(debugMessageHandler);// needs application to be set for log path
     qDebug() << QDateTime::currentDateTimeUtc().toString(Qt::ISODate).toUtf8().constData();
     qDebug() << KREVISION << " " << KREVISIONDATE;
     qDebug() << QSysInfo::prettyProductName() << QSysInfo::productType() << QSysInfo::productVersion();
@@ -196,14 +204,6 @@ int main(int argc, char *argv[]) try {
 #ifdef NDEBUG
     Splash splash(qApp->primaryScreen()->devicePixelRatio() == 1.0 ? ":/resources/splash.png" : ":/resources/splash@2x.png");
 #endif
-    QCoreApplication::setOrganizationDomain("knossos.app");
-    QCoreApplication::setOrganizationName("MPIN");
-    QCoreApplication::setApplicationName("KNOSSOS");
-    QCoreApplication::setApplicationVersion(KREVISION);
-    QSettings::setDefaultFormat(QSettings::IniFormat);
-
-    QFile::copy(QSettings{QSettings::IniFormat, QSettings::UserScope, "MPIN", "KNOSSOS 5.0"}.fileName(), QSettings{}.fileName());// doesn’t overwrite
-
     qRegisterMetaType<std::string>();
     qRegisterMetaType<Coordinate>();
     qRegisterMetaType<CoordOfCube>();