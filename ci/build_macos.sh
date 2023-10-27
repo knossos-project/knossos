5e4e7170bb4dbbf4b9c99a86cb45688e7d209652
Norbert Pfeiler <>
2023-10-27 05:56:17 +0200

    fix plugins on macOS
    bundle python requests (requires handling openssl)

--- ci/build_macos.sh
+++ ci/build_macos.sh
@@ -12,17 +12,19 @@ time ninja
 # OS X housekeeping
 mv knossos.app KNOSSOS.app
 time /usr/local/opt/qt@5/bin/macdeployqt KNOSSOS.app
-cp -v /usr/local/Cellar/quazip/${QUAZIP_VERSION}/lib/libquazip.1.0.0.dylib KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib &&
-  install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip.1.dylib -change /usr/local/opt/qt@5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ||
-  true
-cp -v /usr/local/lib/libquazip1-qt5.${QUAZIP_VERSION}.dylib KNOSSOS.app/Contents/Frameworks/libquazip1-qt5.1.dylib &&
-  install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip1-qt5.1.dylib -change /usr/local/opt/qt@5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ||
-  true
+cp -v /usr/local/lib/libquazip1-qt5.${QUAZIP_VERSION}.dylib KNOSSOS.app/Contents/Frameworks/libquazip*.dylib
+install_name_tool KNOSSOS.app/Contents/Frameworks/libquazip*.dylib -change /usr/local/opt/qt@5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore
 
-PY_VER=$(cd /usr/local/Frameworks/Python.framework/Versions/ && echo * | tr ' ' '\n' | grep -v Current | sort -V | tail -1)
+PYTHON_VERSION=$(cd /usr/local/Frameworks/Python.framework/Versions/ && echo * | tr ' ' '\n' | grep -v Current | sort -V | tail -1)
 mkdir -p KNOSSOS.app/Contents/Frameworks/Python.framework/Versions
-cp -r /usr/local/Frameworks/Python.framework/Versions/${PY_VER} KNOSSOS.app/Contents/Frameworks/Python.framework/Versions
-install_name_tool KNOSSOS.app/Contents/MacOS/knossos -change /usr/local/opt/python@${PY_VER}/Frameworks/Python.framework/Versions/${PY_VER}/Python @executable_path/../Frameworks/Python.framework/Versions/${PY_VER}/Python
+/usr/local/Frameworks/Python.framework/Versions/${PYTHON_VERSION}/bin/pip3 install requests
+cp -r /usr/local/Frameworks/Python.framework/Versions/${PYTHON_VERSION} KNOSSOS.app/Contents/Frameworks/Python.framework/Versions
+install_name_tool KNOSSOS.app/Contents/MacOS/knossos -change /usr/local/opt/python@${PYTHON_VERSION}/Frameworks/Python.framework/Versions/${PYTHON_VERSION}/Python @executable_path/../Frameworks/Python.framework/Versions/${PYTHON_VERSION}/Python
+
+cp -v /usr/local/opt/openssl/lib/lib{ssl,crypto}*.dylib KNOSSOS.app/Contents/Frameworks/
+install_name_tool KNOSSOS.app/Contents/Frameworks/Python.framework/Versions/*/lib/python*/lib-dynload/_ssl.cpython-*-darwin.so -change /usr/local/opt/openssl@3/lib/libssl.3.dylib @executable_path/../Frameworks/libssl.3.dylib
+install_name_tool KNOSSOS.app/Contents/Frameworks/Python.framework/Versions/*/lib/python*/lib-dynload/_ssl.cpython-*-darwin.so -change /usr/local/opt/openssl@3/lib/libcrypto.*.dylib @executable_path/../Frameworks/libcrypto.3.dylib
+install_name_tool KNOSSOS.app/Contents/Frameworks/libssl.3.dylib -change /usr/local/Cellar/openssl@3/*/lib/libcrypto.3.dylib @executable_path/../Frameworks/libcrypto.3.dylib
 
 # Deployment
 time zip -rq ${APPVEYOR_BUILD_FOLDER}/macos.KNOSSOS.nightly.app.zip KNOSSOS.app
