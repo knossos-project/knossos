knossos-project/knossos

commit 7d330783833fb21b39e6f6b0079ead3ed4c76875

adjacent bucket fill

widgets/viewports/viewportevents.cpp
@@ -446,6 +446,13 @@ void ViewportOrtho::handleMouseReleaseMiddle(const QMouseEvent *event) {
             areaMin = areaMin.capped(Annotation::singleton().movementAreaMin, Annotation::singleton().movementAreaMax);
             areaMax = areaMax.capped(Annotation::singleton().movementAreaMin, Annotation::singleton().movementAreaMax) + 1;
 
+            if (!brush_copy.inverse && brush_copy.mode == brush_t::mode_t::two_dim && readVoxel(clickedCoordinate) != seg.getBackgroundId()) {
+                auto brush_copy2 = brush_copy;
+                brush_copy2.inverse = true;
+                subobjectBucketFill(clickedCoordinate, seg.getBackgroundId(), brush_copy2, areaMin, areaMax);
+
+                brush_copy.mode = brush_t::mode_t::adjacent;
+            }
             subobjectBucketFill(clickedCoordinate, soid, brush_copy, areaMin, areaMax);
         }
     }

segmentation/segmentationsplit.cpp
@@ -54,16 +54,17 @@ void subobjectBucketFill(const Coordinate & seed, const uint64_t fillsoid, const
         const auto pos = work.back();
         work.pop_back();
 
-        const auto walk = [&work, &cubeCoords, clickedsoid, fillsoid](const Coordinate coord){
-            if (readVoxel(coord) == clickedsoid) {
+        const auto posDec = (pos - voxelSpacing).capped(areaMin, areaMax);
+        const auto posInc = (pos + voxelSpacing).capped(areaMin, areaMax);
+
+        const auto walk = [&work, &cubeCoords, clickedsoid, fillsoid, &brush](const Coordinate coord){
+            const auto adjacent = brush.mode == brush_t::mode_t::adjacent && (readVoxel(coord + brush.n) == fillsoid || readVoxel(coord - brush.n) == fillsoid) && readVoxel(coord) != fillsoid;
+            if (adjacent || brush.mode != brush_t::mode_t::adjacent && readVoxel(coord) == clickedsoid) {
                 writeVoxel(coord, fillsoid, false);
                 work.emplace_back(coord);
                 cubeCoords.insert(Dataset::datasets[Segmentation::singleton().layerId].global2cube(coord));
             }
         };
-
-        const auto posDec = (pos - voxelSpacing).capped(areaMin, areaMax);
-        const auto posInc = (pos + voxelSpacing).capped(areaMin, areaMax);
 
         if (brush.view != brush_t::view_t::xy || brush.mode == brush_t::mode_t::three_dim) {
             walk({pos.x, pos.y, posInc.z});

segmentation/segmentationsplit.h
@@ -31,7 +31,7 @@
 class brush_t {
 public:
     enum class mode_t {
-        two_dim, three_dim
+        two_dim, three_dim, adjacent
     };
     enum class view_t {
         xy, xz, zy, arb

commit d53694497cd2ec014017fd39b8ba44d2722d81d4

restrict adjacent brush (i.e. for OverPaint)
only enable if invoked on top of the selection
add in cheat sheet

regression from 7d330783833fb21b39e6f6b0079ead3ed4c76875
bucket fill in OverPaint did only erase

resources/cheatsheet/paintmode.html.in
@@ -22,6 +22,7 @@
 			</tr>
 		</thead>
 		<tr><td><span class="sh"><img src=":/resources/cheatsheet/middleclick.png" alt="Middle click"/> click</span></td><td>Bucket fill</td></tr>
+		<tr><td><span class="sh"><img src=":/resources/cheatsheet/middleclick.png" alt="Middle click on selection"/> on selection</span></td><td>bounded by adjacent frame fills</td></tr>
 		<tr><td><span class="sh">Shift +  <img src=":/resources/cheatsheet/middleclick.png" alt="Middle click"/> scroll</span></td><td>Change brush size</td></tr>
 		<tr><td><span class="sh"><img src=":/resources/cheatsheet/rightclick.png" alt="Right click"/></span></td><td>Draw</td></tr>
 		<tr><td><span class="sh">Shift + <img src=":/resources/cheatsheet/rightclick.png" alt="Right click"/></span></td><td>Erase</td></tr>

widgets/viewports/viewportevents.cpp
@@ -446,12 +446,14 @@ void ViewportOrtho::handleMouseReleaseMiddle(const QMouseEvent *event) {
             areaMin = areaMin.capped(Annotation::singleton().movementAreaMin, Annotation::singleton().movementAreaMax);
             areaMax = areaMax.capped(Annotation::singleton().movementAreaMin, Annotation::singleton().movementAreaMax) + 1;
 
-            if (!brush_copy.inverse && brush_copy.mode == brush_t::mode_t::two_dim && readVoxel(clickedCoordinate) != seg.getBackgroundId()) {
-                auto brush_copy2 = brush_copy;
-                brush_copy2.inverse = true;
-                subobjectBucketFill(clickedCoordinate, seg.getBackgroundId(), brush_copy2, areaMin, areaMax);
+            if (!Annotation::singleton().annotationMode.testFlag(AnnotationMode::Mode_OverPaint)) {
+                if (!brush_copy.inverse && brush_copy.mode == brush_t::mode_t::two_dim && seg.isSelected(seg.subobjectFromId(readVoxel(clickedCoordinate), clickedCoordinate))) {
+                    auto brush_copy2 = brush_copy;
+                    brush_copy2.inverse = true;
+                    subobjectBucketFill(clickedCoordinate, seg.getBackgroundId(), brush_copy2, areaMin, areaMax);
 
-                brush_copy.mode = brush_t::mode_t::adjacent;
+                    brush_copy.mode = brush_t::mode_t::adjacent;
+                }
             }
             subobjectBucketFill(clickedCoordinate, soid, brush_copy, areaMin, areaMax);
         }